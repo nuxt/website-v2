8086507dea050bfb58e95a7d002752ed442c242a
Alexandre Chopin <wikeo65@gmail.com>
2016-11-18 11:25:27 +0100

    Home page and routes

---
+++ pages/guide/release-notes.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="Guide">
+    <nuxt-header></nuxt-header>
+    Release Notes
+  </div>
+</template>
+
+<script>
+import NuxtHeader from '~components/Header.vue'
+
+export default {
+  components: {
+    NuxtHeader
+  }
+}
+</script>

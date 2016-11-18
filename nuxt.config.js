db5d83b2c51ff8cf96aff6bb74da82809687cd5b
Sébastien Chopin <seb@chopin.io>
2016-11-18 11:38:27 +0100

    Add  meta description

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,8 @@ module.exports = {
     title: 'Nuxt.js - A minimal framework for server-renderer Vue.js applications',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', content: 'Nuxt.js is a minimal framework for creating Vue.js applications with server side rendering, code-splitting, hot-reloading, static generation and more!' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' }

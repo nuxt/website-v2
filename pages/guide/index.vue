a6279f6de2502396a49a9ec53f869e4ca9983b54
Alexandre Chopin <wikeo65@gmail.com>
2016-11-18 11:25:27 +0100

    Home page and routes to v3

---
+++ pages/guide/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="Guide">
+    <nuxt-header></nuxt-header>
+    What's Nuxt.js ?
+  </div>
+</template>
+
+<script>
+import NuxtHeader from '~components/Header.vue'
+
+export default {
+  components: {
+    NuxtHeader
+  }
+}
+</script>

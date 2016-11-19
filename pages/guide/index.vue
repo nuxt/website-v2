8721535041b3a9a513fc91c44131d06ad4d7e7eb
Alexandre Chopin <wikeo65@gmail.com>
2016-11-20 00:18:21 +0100

    Create markdown structure

--- pages/guide/index.vue
+++ pages/guide/index.vue
@@ -1,7 +1,28 @@
 <template>
   <div class="Guide">
     <nuxt-header></nuxt-header>
+<<<<<<< HEAD
     What's Nuxt.js ?
+=======
+    <div class="container">
+      <nuxt-affix></nuxt-affix>
+      <nuxt-content>
+        <nuxt-title>What's Nuxt.js ?</nuxt-title>
+        <nuxt-paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <a href="/">link</a>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </nuxt-paragraph>
+        <hr>
+        <nuxt-subtitle>Quick demo</nuxt-subtitle>
+        <nuxt-code title="nuxt.config.js">
+          module.exports = {
+            css: ['bulma']
+          }
+        </nuxt-code>
+      </nuxt-content>
+    </div>
+>>>>>>> ea2a176f (Create markdown structure)
   </div>
 </template>
 

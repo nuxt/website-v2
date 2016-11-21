81d1728bb9b704d8a9140ceb4601b24f5f3160ea
Alexandre Chopin <wikeo65@gmail.com>
2016-11-21 22:40:56 +0100

    supercharges app

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,42 @@
 <template>
-  <div class="Landscape">
-    <nuxt-header></nuxt-header>
-    <div class="Landscape__Content">
-      <div class="container content">
-        <img src="~static/logo_noir.png" alt="Logo nuxt" class="Landscape__Content__Image"/>
-        <h2 class="Landscape__Content__Title">
-          A minimalistic framework <br>for server-rendered Vue.js applications
-        </h2>
-        <ul class="Landscape__Content__List">
-          <li class="Landscape__Content__List__Item">
-            <router-link class="Landscape__Content__List__Item__Link" to="/guide">
-              GUIDE
-            </router-link>
-          </li>
-          <li class="Landscape__Content__List__Item">
-            <router-link class="Landscape__Content__List__Item__Link" to="/examples">
-              EXAMPLES
-            </router-link>
-          </li>
-          <li class="Landscape__Content__List__Item">
-            <a class="Landscape__Content__List__Item__Link" href="https://github.com/nuxt/nuxt.js" target="_blank">
-              GITHUB
-            </a>
-          </li>
-        </ul>
+  <div>
+    <section class="Landscape">
+      <div class="Landscape__Content">
+        <div class="container content">
+          <img src="~static/logo_noir.png" alt="Logo nuxt" class="Landscape__Content__Image"/>
+          <h2 class="Landscape__Content__Title">
+            A minimalistic framework <br>for server-rendered Vue.js applications
+          </h2>
+          <ul class="Landscape__Content__List">
+            <li class="Landscape__Content__List__Item">
+              <router-link class="Landscape__Content__List__Item__Link" to="/guide">
+                GUIDE
+              </router-link>
+            </li>
+            <li class="Landscape__Content__List__Item">
+              <router-link class="Landscape__Content__List__Item__Link" to="/examples">
+                EXAMPLES
+              </router-link>
+            </li>
+            <li class="Landscape__Content__List__Item">
+              <a class="Landscape__Content__List__Item__Link" href="https://github.com/nuxt/nuxt.js" target="_blank">
+                GITHUB
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </section>
+    <footbar></footbar>
   </div>
 </template>
 
 <script>
-import NuxtHeader from '~components/Header.vue'
+import Footbar from '~components/Footer.vue'
 
 export default {
   components: {
-    NuxtHeader
+    Footbar
   }
 }
 </script>
@@ -65,7 +67,6 @@ export default {
     }
     &__List
     {
-      // float: right;
       list-style: none;
       margin: 0;
       padding: 0;

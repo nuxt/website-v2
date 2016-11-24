d1aea9e3fc84fe17de44ac3d334a6d9d7b70f5d1
Sébastien Chopin <seb@chopin.io>
2016-11-24 17:16:09 +0100

    Add html-parser for link reactivity

---
+++ components/HtmlParser.vue
@@ -0,0 +1,29 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  mounted () {
+    const links = this.$el.getElementsByTagName('a')
+    for (let i = 0; i < links.length; i++) {
+      links[i].addEventListener('click', this.navigate, false)
+    }
+  },
+  beforeDestroy () {
+    const links = this.$el.getElementsByTagName('a')
+    for (let i = 0; i < links.length; i++) {
+      links[i].removeEventListener('click', this.navigate, false)
+    }
+  },
+  methods: {
+    navigate (event) {
+      const href = event.target.getAttribute('href')
+      if (href && href[0] === '/') {
+        event.preventDefault()
+        this.$router.push(href)
+      }
+    }
+  }
+}
+</script>

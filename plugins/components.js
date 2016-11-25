cc8150a2d030f4cb35910bee5e78a95d571934a3
Alexandre Chopin <wikeo65@gmail.com>
2016-11-25 04:25:53 +0100

    files tree list recursive

---
+++ plugins/components.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import RecursiveList from '~components/RecursiveList.vue'
+
+Vue.component('recursive-list', RecursiveList)
--- store/index.js
+++ store/index.js
@@ -1,19 +1,41 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
     visibleHeader: false,
-    visibleAffix: false
+    visibleAffix: false,
+    currentFile: null,
+    currentFileContent: null
   },
   mutations: {
     toggle (state, key) {
       state[key] = !state[key]
+    },
+    updateFile (state, value) {
+      state.currentFile = value
+    },
+    updateFileContent (state, value) {
+      state.currentFileContent = value
     }
   },
   actions: {
+    updateFileDatas ({ commit, state }, file) {
+      commit('updateFile', file)
+      // return axios({
+      //   url: file.download_url,
+      //   headers: {
+      //     'Authorization': 'token 4aa6bcf919d238504e7db59a66d32e78281c0ad3'
+      //   }
+      // })
+      // .then((res) => {
+      //   console.log(res)
+      //   commit('updateFileContent', res.data)
+      // })
+    },
     nuxtServerInit ({ state }, { req }) {
       if (!process.BROWSER) {
         state.guideMenu = require('json-loader!static/docs/guide/menu.json')

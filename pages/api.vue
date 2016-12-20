ce1c24811fae333dd6c1f5fdaedf43680173ee85
Alexandre Chopin <wikeo65@gmail.com>
2016-12-20 12:48:59 +0100

    error page

--- pages/api.vue
+++ pages/api.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="API">
     <nuxt-affix :list="$store.state.apiMenu" menu="/api"></nuxt-affix>
-    <!-- Children -->
     <div class="API__Content" :class="{'API__Content--hidden': visible}">
-      <html-parser class="Content" :content="body"></html-parser>
+      <nuxt-child></nuxt-child>
       <p class="API__Contribute">Caught a mistake or want to contribute to the documentation?
         <a :href="'https://github.com/nuxt/docs/blob/master/api/'+path+'.md'" target="_blank">
           Edit this page on Github!
@@ -17,62 +16,17 @@
 </template>
 
 <script>
-import marked, { Renderer } from 'marked'
-import highlightjs from 'highlight.js'
-import fm from 'front-matter'
-
 import NuxtAffix from '~components/Affix.vue'
 import NuxtFooter from '~components/Footer.vue'
-import HtmlParser from '~components/HtmlParser.vue'
 
 export default {
-  components: {
-    NuxtAffix,
-    NuxtFooter,
-    HtmlParser
-  },
-  data ({ route }, callback) {
-    // Default data
-    let data = {
-      content: '',
-      path: route.params.slug || 'index'
-    }
-    const path = '/docs/api/' + data.path + '.md'
-    if (process.BROWSER_BUILD) {
-      fetch(path)
-      .then((response) => {
-        const contenType = response.headers.get('content-type') || ''
-        const requestOK = (response.status >= 200 && response.status < 300)
-        if (!requestOK || contenType.indexOf('text/x-markdown') === -1) {
-          throw new Error('API page not found')
-        }
-        return response.text()
-      })
-      .then((content) => {
-        data.content = content
-        callback(null, data)
-      })
-      .catch((e) => {
-        callback({ statusCode: 404, message: 'API page not found' }, data)
-      })
-    } else {
-      require('fs').readFile('static' + path, 'utf8', function (err, content) {
-        if (err) return callback({ statusCode: 404, message: 'Documentation page not found' })
-        data.content = content
-        callback(null, data)
-      })
-    }
-  },
   computed: {
     visible () { return this.$store.state.visibleAffix },
-    page () { return fm(this.content) },
-    body () { return marked(this.page.body) }
+    path () { return this.$route.params.slug || 'index' }
   },
-  head () {
-    return {
-      title: this.page.attributes.title || 'No title',
-      titleTemplate: 'API: %s - Nuxt.js'
-    }
+  components: {
+    NuxtAffix,
+    NuxtFooter
   }
 }
 </script>

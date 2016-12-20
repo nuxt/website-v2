3d1d3d02616283b3001e638b44c8b99a2ad4a23f
Alexandre Chopin <wikeo65@gmail.com>
2016-12-20 14:02:37 +0100

    new architecture

---
+++ pages/_category/index.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <html-parser class="Content" :content="body"></html-parser>
+    <p class="Contribute">Caught a mistake or want to contribute to the documentation?
+      <a :href="'https://github.com/nuxt/docs/blob/master/api/index.md'" target="_blank">
+        Edit this page on Github!
+      </a>
+    </p>
+  </div>
+</template>
+
+<script>
+import marked, { Renderer } from 'marked'
+import highlightjs from 'highlight.js'
+import fm from 'front-matter'
+
+import HtmlParser from '~components/HtmlParser.vue'
+
+export default {
+  data ({ route, store }, callback) {
+    // Default data
+    let data = {
+      content: ''
+    }
+    const path = '/docs/' + store.state.lang.iso + '/' + route.params.category + '/index.md'
+    if (process.BROWSER_BUILD) {
+      fetch(path)
+      .then((response) => {
+        const contenType = response.headers.get('content-type') || ''
+        const requestOK = (response.status >= 200 && response.status < 300)
+        if (!requestOK || contenType.indexOf('text/x-markdown') === -1) {
+          throw new Error('API page not found')
+        }
+        return response.text()
+      })
+      .then((content) => {
+        data.content = content
+        callback(null, data)
+      })
+      .catch((e) => {
+        callback({ statusCode: 404, message: 'API page not found' }, data)
+      })
+    } else {
+      require('fs').readFile('static' + path, 'utf8', function (err, content) {
+        if (err) return callback({ statusCode: 404, message: 'Documentation page not found' })
+        data.content = content
+        callback(null, data)
+      })
+    }
+  },
+  computed: {
+    page () { return fm(this.content) },
+    body () { return marked(this.page.body) }
+  },
+  head () {
+    return {
+      title: this.page.attributes.title || 'No title',
+      titleTemplate: 'API: %s - Nuxt.js'
+    }
+  },
+  components: {
+    HtmlParser
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.Contribute
+{
+  border-top: 1px #dbdfe1 solid;
+  padding-top: 20px;
+  color: #7f8c8d;
+}
+</style>

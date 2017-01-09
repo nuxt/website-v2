928fe9bec7db2c2aaeadbcc3b7f9b42e7885ae00
Sébastien Chopin <seb@chopin.io>
2017-01-09 16:55:45 +0100

    Fix https

--- pages/index.vue
+++ pages/index.vue
@@ -37,13 +37,13 @@ export default {
       { name: 'og:description', content: 'Nuxt.js is a minimal framework for creating Vue.js applications with server side rendering, code-splitting, hot-reloading, static generation and more!' },
       { name: 'og:type', content: 'website' },
       { name: 'og:url', content: 'https://nuxtjs.org' },
-      { name: 'og:image', content: 'http://i.imgur.com/PzEm5j2.png' },
+      { name: 'og:image', content: 'https://i.imgur.com/PzEm5j2.png' },
       // Twitter Card
       { name: 'twitter:card', content: 'summary' },
       { name: 'twitter:site', content: '@nuxt_js' },
       { name: 'twitter:title', content: 'Nuxt.js - Universal Vue.js Applications' },
       { name: 'twitter:description', content: 'Nuxt.js is a minimal framework for creating Vue.js applications with server side rendering, code-splitting, hot-reloading, static generation and more!' },
-      { name: 'twitter:image', content: 'http://i.imgur.com/PzEm5j2.png' },
+      { name: 'twitter:image', content: 'https://i.imgur.com/PzEm5j2.png' },
       { name: 'twitter:image:alt', content: 'Nuxt.js Logo' }
     ]
   }

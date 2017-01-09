76a24fcb749eeb245362d8e9cf5b0ee5844d5e92
Sébastien Chopin <seb@chopin.io>
2017-01-09 18:54:57 +0100

    Use lang file in docs

--- pages/index.vue
+++ pages/index.vue
@@ -30,22 +30,26 @@ export default {
   components: {
     NuxtFooter
   },
-  head: {
-    meta: [
-      // Open Grapg
-      { name: 'og:title', content: 'Nuxt.js - Universal Vue.js Applications' },
-      { name: 'og:description', content: 'Nuxt.js is a minimal framework for creating Vue.js applications with server side rendering, code-splitting, hot-reloading, static generation and more!' },
-      { name: 'og:type', content: 'website' },
-      { name: 'og:url', content: 'https://nuxtjs.org' },
-      { name: 'og:image', content: 'https://i.imgur.com/PzEm5j2.png' },
-      // Twitter Card
-      { name: 'twitter:card', content: 'summary' },
-      { name: 'twitter:site', content: '@nuxt_js' },
-      { name: 'twitter:title', content: 'Nuxt.js - Universal Vue.js Applications' },
-      { name: 'twitter:description', content: 'Nuxt.js is a minimal framework for creating Vue.js applications with server side rendering, code-splitting, hot-reloading, static generation and more!' },
-      { name: 'twitter:image', content: 'https://i.imgur.com/PzEm5j2.png' },
-      { name: 'twitter:image:alt', content: 'Nuxt.js Logo' }
-    ]
+  head () {
+    return {
+      title: this.$store.state.lang.homepage.meta.title,
+      meta: [
+        { name: 'description', hid: 'description', content: this.$store.state.lang.homepage.meta.description },
+        // Open Grapg
+        { name: 'og:title', content: this.$store.state.lang.homepage.meta.title },
+        { name: 'og:description', content: this.$store.state.lang.homepage.meta.description },
+        { name: 'og:type', content: 'website' },
+        { name: 'og:url', content: 'https://nuxtjs.org' },
+        { name: 'og:image', content: 'https://i.imgur.com/PzEm5j2.png' },
+        // Twitter Card
+        { name: 'twitter:card', content: 'summary' },
+        { name: 'twitter:site', content: '@nuxt_js' },
+        { name: 'twitter:title', content: this.$store.state.lang.homepage.meta.title },
+        { name: 'twitter:description', content: this.$store.state.lang.homepage.meta.description },
+        { name: 'twitter:image', content: 'https://i.imgur.com/PzEm5j2.png' },
+        { name: 'twitter:image:alt', content: 'Nuxt.js Logo' }
+      ]
+    }
   }
 }
 </script>

8e0c5293e5da7442dba87b76ca6e53821970bec4
Alexandre Chopin <wikeo65@gmail.com>
2017-01-13 09:45:02 +0100

    add lang support

--- components/Affix.vue
+++ components/Affix.vue
@@ -4,7 +4,7 @@
       <div class="icon more-vertical"></div>
     </div>
     <h2 class="Affix__Version">
-      Version {{ $store.state.version }}
+      {{ $store.state.lang.links.version }} {{ $store.state.version }}
       <div class="Affix__Version__Toggler" @click="toggle">
         <div class="icon close"></div>
       </div>

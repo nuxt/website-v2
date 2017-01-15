c14f4a67ee3157ac34551042bec477d4f5e84de0
Sébastien Chopin <seb@chopin.io>
2017-01-15 17:58:05 +0100

    Update nuxt.js version

--- store/index.js
+++ store/index.js
@@ -6,7 +6,7 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
-    version: '0.9.5',
+    version: '0.9.6',
     visibleHeader: false,
     visibleAffix: false,
     apiURI: 'https://docs.api.nuxtjs.org',

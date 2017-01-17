f0fba0cc4e64c2713b3c974b4025b1a4909ef06c
Sébastien Chopin <seb@chopin.io>
2017-01-17 11:36:30 +0100

    Add canonical url

--- layouts/default.vue
+++ layouts/default.vue
@@ -24,9 +24,17 @@ export default {
     }
   },
   head () {
+    let canonical = `https://nuxtjs.org${this.$route.path}`
+    if (this.$store.state._lang !== 'en') {
+      canonical = `https://${this.$store.state._lang}.nuxtjs.org${this.$route.path}`
+    }
+    if (canonical.slice(-1) !== '/') {
+      canonical = canonical + '/'
+    }
     return {
       htmlAttrs: { lang: this.$store.state._lang },
       link: [
+        { rel: 'canonical', href: canonical },
         { rel: 'alternate', hreflang: 'en', href: `https://nuxtjs.org${this.$route.path}` },
         { rel: 'alternate', hreflang: 'cn', href: `https://cn.nuxtjs.org${this.$route.path}` },
         { rel: 'alternate', hreflang: 'ru', href: `https://ru.nuxtjs.org${this.$route.path}` }

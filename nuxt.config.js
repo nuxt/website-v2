06140b3540aa83a5adff02d99644d1572fcdf145
Sebastien Chopin <seb@chopin.io>
2017-08-21 16:32:06 +0200

    No more generate

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,3 @@
-const axios = require('axios')
-const _ = require('lodash')
-
 module.exports = {
   head: {
     meta: [
@@ -27,30 +24,5 @@ module.exports = {
     githubToken: '4aa6bcf919d238504e7db59a66d32e78281c0ad3',
     docSearchApiKey: 'ff80fbf046ce827f64f06e16f82f1401'
   },
-  loading: { color: '#41B883' },
-  generate: {
-    routes () {
-      const lang = 'en'
-      return Promise.all(
-        ['guide', 'api', 'examples', 'faq']
-        .map((category) => {
-          return axios.get(`https://docs.api.nuxtjs.org/menu/${lang}/${category}`)
-          .then((res) => res.data || [])
-          .then((menu) => {
-            return _(menu)
-            .map('links')
-            .flatten()
-            .map((m) => m.to.slice(1))
-            .compact()
-            .map((slug) => {
-              return `/${category}/${slug}`
-            })
-            .value()
-            .concat(`/${category}`)
-          })
-        })
-      )
-      .then((routes) => _(routes).flatten().uniq().value())
-    }
-  }
+  loading: { color: '#41B883' }
 }

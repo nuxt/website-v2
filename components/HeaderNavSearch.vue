911ded4f282fb2f5add5f0afb47a885e679afde7
Sebastien Chopin <seb@chopin.io>
2017-10-05 16:53:57 +0200

    Improve perfs by loading CSS dynamically

--- components/HeaderNavSearch.vue
+++ components/HeaderNavSearch.vue
@@ -14,11 +14,18 @@ export default {
   mounted () {
     onScriptLoaded(() => this.addInstantSearch())
     if (scriptInjected) return
+    // Load JS
     const script = document.createElement('script')
     script.setAttribute('type', 'text/javascript')
     script.setAttribute('src', '//cdn.jsdelivr.net/docsearch.js/2/docsearch.min.js')
     document.getElementsByTagName('body')[0].appendChild(script)
     script.onload = scriptLoaded
+    // Load CSS
+    var link = document.createElement('link')
+    link.setAttribute('rel', 'stylesheet')
+    link.setAttribute('type', 'text/css')
+    link.setAttribute('href', 'https://cdn.jsdelivr.net/docsearch.js/2/docsearch.min.css')
+    document.getElementsByTagName('body')[0].appendChild(link)
     scriptInjected = true
   },
   methods: {

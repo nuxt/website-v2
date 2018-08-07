3c794568806f57a141aeb5402fda93d6b6abf47e
Alex <alex@orion.sh>
2018-08-08 00:05:32 +0200

    add scss variables

---
+++ components/lib/Nav.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="Nav">
+    <slot/>
+  </nav>
+</template>
+
+<style lang="scss">
+$nav-height-lg: 72px;
+$nav-height-sm: 64px;
+$nav-height-xs: 56px;
+
+.Nav {
+  height: $nav-height-xs;
+  color: $color-white;
+}
+</style>

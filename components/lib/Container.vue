b953deae9b263d0de8d031c7fca9b197ad9a9583
Alex <alex@orion.sh>
2018-08-08 00:05:32 +0200

    add scss variables

---
+++ components/lib/Container.vue
@@ -0,0 +1,19 @@
+<style lang="scss">
+.Container {
+  width: 100%;
+  padding: 0 15px;
+  margin: 0 auto;
+  @media (min-width: 576px) {
+    max-width: 540px;
+  }
+  @media (min-width: 768px) {
+    max-width: 720px;
+  }
+  @media (min-width: 992px) {
+    max-width: 960px;
+  }
+  @media (min-width: 1200px) {
+    max-width: 1140px;
+  }
+}
+</style>

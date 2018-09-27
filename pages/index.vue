286436a0beaf21bdee3480088d70ea1ac40c6cac
Sébastien Chopin <seb@chopin.io>
2018-09-28 00:47:38 +0200

    feat: Add CodeSandBox

--- pages/index.vue
+++ pages/index.vue
@@ -16,10 +16,19 @@
         </li>
         <li class="Landscape__List__Item">
           <a class="button button--grey" href="https://github.com/nuxt/nuxt.js" target="_blank">
-            {{ $store.state.lang.links.github }} ({{ $store.state.ghVersion }})
+            {{ $store.state.lang.links.github }} <span class="version">{{ $store.state.ghVersion }}</span>
           </a>
         </li>
       </ul>
+    </section>
+    <section class="Codesandbox">
+      <h3>Play with Nuxt.js online</h3>
+      <p>
+        <a class="Codesandbox__Link" href="https://codesandbox.io/s/github/nuxt/codesandbox-nuxt/tree/master/?autoresize=1&hidenavigation=1&view=preview" target="_blank" rel="noopener"><svg class="Codesandbox__Link__Icon" x="0px" y="0px" width="1em" height="1em" viewBox="0 0 1024 1024"><g id="Layer_1"><polyline class="fill" points="719.001,851 719.001,639.848 902,533.802 902,745.267 719.001,851"></polyline><polyline class="fill" points="302.082,643.438 122.167,539.135 122.167,747.741 302.082,852.573 302.082,643.438"></polyline><polyline class="fill" points="511.982,275.795 694.939,169.633 512.06,63 328.436,169.987 511.982,275.795"></polyline></g><g id="Layer_2"><polyline fill="none" class="stroke" stroke-width="80" stroke-miterlimit="10" points="899,287.833 509,513 509,963"></polyline><line fill="none" class="stroke" stroke-width="80" stroke-miterlimit="10" x1="122.167" y1="289" x2="511.5" y2="513"></line><polygon fill="none" class="stroke" stroke-width="80" stroke-miterlimit="10" points="121,739.083 510.917,963.042 901,738.333 901,288 511,62 121,289"></polygon></g></svg> Open on CodeSandBox.io</a>
+        <br>
+        <nuxt-link class="Codesandbox__Examples" to="/examples">See more examples</nuxt-link>
+      </p>
+      <iframe src="https://codesandbox.io/s/github/nuxt/codesandbox-nuxt/tree/master/?autoresize=1&hidenavigation=1&view=preview" sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"></iframe>
     </section>
     <nuxt-footer/>
   </div>
@@ -70,7 +79,7 @@ export default {
   text-align: center;
   @media (min-width: 992px)
   {
-    padding: 140px 30px;
+    padding: 100px 30px;
     padding-top: 200px;
   }
   &__Logo
@@ -116,7 +125,69 @@ export default {
       display: inline-block;
       padding: 15px;
       padding-bottom: 0;
+      span {
+        font-weight: 400;
+      }
     }
+  }
+}
+.Codesandbox {
+  background: #141618;
+  padding: 30px 0 50px 0;
+  text-align: center;
+  h3 {
+    color: #EEE;
+    font-weight: 600;
+  }
+  &__Link {
+    border: 1px solid #41B883;
+    padding: 10px 20px;
+    font-size: 20px;
+    position: relative;
+    right: 0;
+    margin: 10px 0;
+    text-decoration: none;
+    display: inline-flex;
+    align-self: center;
+    &:hover {
+      background: #41B883;
+      color: #141618;
+      .fill {
+        fill: #141618;
+      }
+      .stroke {
+        stroke: #141618;
+      }
+    }
+    &__Icon {
+      margin-right: 8px;
+      .fill {
+        fill: #41B883;
+      }
+      .stroke {
+        stroke:#41B883;
+      }
+    }
+  }
+  &__Examples {
+    color: #999;
+    text-decoration: underline;
+    display: inline-block;
+    margin-bottom: 10px;
+  }
+  iframe {
+    width:100%;
+    max-width: 1400px;
+    height:700px;
+    border:0;
+    border-radius: 4px;
+    overflow:hidden;
+    margin: 0 auto;
+  }
+}
+@media (max-width: 991px) {
+  .Codesandbox iframe {
+    display: none;
   }
 }
 </style>

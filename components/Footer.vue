a946075385f9bf3a4996b0ee374e5b304a34cf74
Sébastien Chopin <seb@chopin.io>
2018-09-27 23:51:32 +0200

    home: Improve sponsors

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,14 +1,22 @@
 <template>
   <footer class="Footer">
-    <!-- <div class="Footer__Links container">
-      Links
-    </div> -->
-
-    <div align="center" style="padding-bottom: 10px;">
-      <a href="https://www.shuttleworthfoundation.org">
-        <img src="/shuttleworth-funded.jpg" width="200" alt="shuttleworth funded"/>
+    <div class="Footer__Sponsors">
+      <h3>Sponsors</h3>
+      <a class="Footer__Sponsors__Link" href="https://t.co/sUZfRy6ZxS" target="_blank" rel="noopener">
+        <img src="https://user-images.githubusercontent.com/1016365/34124854-48fafa06-e3e9-11e7-8c04-251055feebee.png" alt="Tipe.io" />
+      </a>
+      <a class="Footer__Sponsors__Link" href="https://www.storyblok.com/?ref=nuxt" target="_blank" rel="noopener">
+        <img src="https://a.storyblok.com/f/39898/x/aa0dc897ff/storyblok-logo.svg" alt="Storyblok.com">
+      </a>
+      <br/>
+      <a class="Footer__Sponsors__Link" href="https://www.shuttleworthfoundation.org">
+        <img src="/shuttleworth-funded.png" width="200" alt="shuttleworth funded"/>
       </a>
     </div>
+    <p class="Footer__Sponsors__Become">
+      Support us and
+      <a href="https://opencollective.com/nuxtjs" target="_blank" rel="noopener">become a partner</a>.
+    </p>
     <div class="Footer__Author container">
       {{ $store.state.lang.footer.authors }}:
       <a href="https://github.com/Atinux" target="_blank">@Atinux</a>
@@ -21,6 +29,40 @@
 <style lang="scss" scoped>
 .Footer
 {
+  text-align: center;
+  background: #f6f6f6;
+  padding-top: 30px;
+  &__Sponsors {
+    max-width: 700px;
+    padding-bottom: 10px;
+    margin: 0px auto;
+    h3 {
+      color: #999;
+      font-weight: 600;
+      margin: 0 0 10px;
+    }
+    &__Link {
+      display: inline-block;
+      position: relative;
+      margin: 20px 15px 0;
+      height: 60px;
+      img {
+        display: inline-block;
+        vertical-align: middle;
+        height: 100%;
+        transition: all 0.3s ease;
+        filter: grayscale(100%);
+        opacity: 0.66;
+        &:hover, :focus {
+          filter: none;
+          opacity: 1;
+        }
+      }
+    }
+    &__Become {
+      padding-bottom: 10px;
+    }
+  }
   &__Links
   {
     text-align: center;

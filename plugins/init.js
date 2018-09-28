1ee1998ac0e86b3e92dc5e18acbb5ec2077ba7e5
Sébastien Chopin <seb@chopin.io>
2018-09-28 11:13:29 +0200

    feat: Add SPA support by using plugin

---
+++ plugins/init.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+
+export default async function ({ isDev, env, req, store: { commit, state }, redirect }) {
+  // If state filled on server-side (to support spa fallback)
+  if (state.filled) {
+    return
+  }
+  if (isDev) {
+    commit('setApiURI', 'http://localhost:4000')
+  }
+  // If SSR
+  if (req) {
+    const hostParts = (req.headers.host || '').replace('.org', '').split('.')
+    // If url like ja.nuxtjs.org
+    if (hostParts.length === 2) {
+      if (hostParts[0] === 'www') return redirect(301, 'https://nuxtjs.org' + req.url)
+    }
+  } else {
+    // Used with nuxt generate
+    commit('setLocale', env.locale)
+  }
+  try {
+    const resReleases = await axios(state.apiURI + '/releases')
+    commit('setGhVersion', resReleases.data[0].name)
+    const resLang = await axios(state.apiURI + '/lang/' + state.locale)
+    commit('setLang', resLang.data)
+    commit('setDocVersion', resLang.data.docVersion)
+    const resMenu = await axios(state.apiURI + '/menu/' + state.locale)
+    commit('setMenu', resMenu.data)
+    commit('setFilled')
+  } catch (e) {
+    console.error('Error on filling the store, please run the docs server.') // eslint-disable-line no-console
+    console.error(e) // eslint-disable-line no-console
+  }
+}

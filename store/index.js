5e315c382c2ecdc29400857f9201a1213333366e
Sébastien Chopin <seb@chopin.io>
2019-01-09 17:29:40 +0100

    app: Support ad blockers, upgrade Nuxt

--- store/index.js
+++ store/index.js
@@ -1,43 +1,44 @@
 import Vuex from 'vuex'
 
-const store = () => new Vuex.Store({
-  state: {
-    filled: false,
-    docVersion: '',
-    ghVersion: '',
-    visibleHeader: false,
-    visibleAffix: false,
-    apiURI: 'https://docs.api.nuxtjs.org',
-    locale: 'en',
-    lang: {},
-    menu: {}
-  },
-  mutations: {
-    toggle(state, key) {
-      state[key] = !state[key]
-    },
-    setApiURI(state, apiURI) {
-      state.apiURI = apiURI
-    },
-    setDocVersion(state, docVersion) {
-      state.docVersion = docVersion
-    },
-    setGhVersion(state, ghVersion) {
-      state.ghVersion = ghVersion
-    },
-    setLocale(state, locale) {
-      state.locale = locale
-    },
-    setLang(state, lang) {
-      state.lang = lang
-    },
-    setMenu(state, menu) {
-      state.menu = menu
-    },
-    setFilled(state) {
-      state.filled = true
-    }
-  }
+export const state = () => ({
+  filled: false,
+  docVersion: '',
+  ghVersion: '',
+  visibleHeader: false,
+  visibleAffix: false,
+  apiURI: 'https://docs.api.nuxtjs.org',
+  locale: 'en',
+  lang: {},
+  menu: {},
+  adBlocked: false
 })
 
-export default store
+export const mutations = {
+  toggle(state, key) {
+    state[key] = !state[key]
+  },
+  setApiURI(state, apiURI) {
+    state.apiURI = apiURI
+  },
+  setDocVersion(state, docVersion) {
+    state.docVersion = docVersion
+  },
+  setGhVersion(state, ghVersion) {
+    state.ghVersion = ghVersion
+  },
+  setLocale(state, locale) {
+    state.locale = locale
+  },
+  setLang(state, lang) {
+    state.lang = lang
+  },
+  setMenu(state, menu) {
+    state.menu = menu
+  },
+  setFilled(state) {
+    state.filled = true
+  },
+  setAdBlocked(state, value) {
+    state.adBlocked = value
+  }
+}

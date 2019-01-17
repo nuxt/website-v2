214dc1c8b68d968e4dec682d0c6787f63df5fd07
Alexander Alemayhu <alexander@alemayhu.com>
2019-01-17 09:31:32 +0100

    minor reword

--- partials/home/modes.vue
+++ partials/home/modes.vue
@@ -36,7 +36,7 @@
             <p class="nModes_Content_Description">
               Don't need SSR or Static Site Generation but still want to profit from the benefits that Nuxt provides?
               Are you slowly transitioning your app and want to start lightweight? Then the traditional SPA mode will
-              likely your choice.
+              likely be your choice.
               The outcome will be a typical Vue SPA as you know it but influenced by your Nuxt configuration and the
               framework itself.
             </p>

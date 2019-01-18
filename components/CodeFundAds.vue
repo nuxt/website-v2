24ea4690dca4680702798a79cc311ce3b1db60c6
Alexander Lichter <manniL@gmx.net>
2019-01-18 11:00:20 +0000

    chore: catch possibly unavailable codefund error

--- components/CodeFundAds.vue
+++ components/CodeFundAds.vue
@@ -23,7 +23,12 @@ export default {
       script.setAttribute('type', 'text/javascript')
       script.setAttribute('src', '//codefund.io/scripts/7a55aa99-7866-418d-9720-8b1342303656/embed.js?template=vertical')
       script.setAttribute('id', '_codefund_ad_js')
-      this.$refs.codefundads.appendChild(script)
+      try {
+        this.$refs.codefundads.appendChild(script)
+      }catch(e){
+        // In case codefund is *whyever* not available, the page will return a 500 if we don't catch the error
+        console.error(e)
+      }
     }
   },
   beforeDestroy() {

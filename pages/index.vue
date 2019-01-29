0797ac69af6eca1e81841105999ee52cfc99523b
Alex <alex@orion.sh>
2019-01-29 15:29:00 +0100

    change image for social network

--- pages/index.vue
+++ pages/index.vue
@@ -121,13 +121,13 @@ export default {
         { name: 'og:description', content: this.$store.state.lang.homepage.meta.description },
         { name: 'og:type', content: 'website' },
         { name: 'og:url', content: 'https://nuxtjs.org' },
-        { name: 'og:image', content: 'https://i.imgur.com/PzEm5j2.png' },
+        { name: 'og:image', content: 'https://nuxtjs.org/meta_640.png' },
         // Twitter Card
         { name: 'twitter:card', content: 'summary' },
         { name: 'twitter:site', content: '@nuxt_js' },
         { name: 'twitter:title', content: this.$store.state.lang.homepage.meta.title },
         { name: 'twitter:description', content: this.$store.state.lang.homepage.meta.description },
-        { name: 'twitter:image', content: 'https://i.imgur.com/PzEm5j2.png' },
+        { name: 'twitter:image', content: 'https://nuxtjs.org/meta_640.png' },
         { name: 'twitter:image:alt', content: 'Nuxt.js Logo' }
       ]
     }

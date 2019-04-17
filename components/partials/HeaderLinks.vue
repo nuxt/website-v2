b94507057c828caf6af0fe1a5bbb01c5893b103c
Alex <alex@orion.sh>
2019-04-17 15:11:56 +0200

    navbar

---
+++ components/partials/HeaderLinks.vue
@@ -0,0 +1,50 @@
+<template>
+  <ul class="nui-headerLinks">
+    <li v-for="link in links" :key="link">
+      <nuxt-link class="nui-headerLinks-link" :to="'/' + link">
+        {{ $store.state.lang.links[link] || 'modules' }}
+      </nuxt-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      links: ['guide', 'api', 'examples', 'faq', 'modules']
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+$grey_blue: #2F495E;
+$light_green: #00C58E;
+
+.nui-headerLinks {
+  list-style: none;
+  display: flex;
+  margin: 0;
+  padding: 0;
+  li {
+    padding: 1rem 0.5rem;
+  }
+  &-link {
+    font-size: 1rem;
+    font-weight: 700;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+    color: $grey_blue;
+    transition: color 0.25s;
+    &:hover {
+      transition: color 0.25s;
+      text-decoration: none;
+      color: $light_green;
+    }
+  }
+  .nuxt-link-active {
+    color: $light_green;
+  }
+}
+</style>

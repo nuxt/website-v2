741a6c3fdc3aae2adf51476bd49f4c8c62a3556a
Alex <alex@orion.sh>
2019-05-02 17:01:46 +0200

    add footer

--- components/ui/Hero.vue
+++ components/ui/Hero.vue
@@ -8,6 +8,6 @@
 .nui-hero {
   background-color: #fff;
   padding: 4rem 0;
-  box-shadow: 0 3px 4px 0 rgba(10, 31, 68, 0.08);
+  box-shadow: 0 2px 4px 0 rgba(10, 31, 68, 0.08);
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,14 @@
 <template>
   <div class="Home">
-    <nui-hero class="Home__Welcome">
-      <nui-container>
-        <nui-row>
-          <div class="Home__Welcome__Text">
-            <h1>The Vue.js Framework for <br><span>Universals Applications</span></h1>
-            <h3>An open-source project under MIT license that make your Vue.js Application development enjoyable.</h3>
-            <div class="Home__Welcome__Text__Buttons">
-              <nui-button to="/" green>
-                <nui-svg-play/>
-                get started
-              </nui-button>
-              <nui-button to="/">
-                <nui-svg-gh/>
-                19K+ github stars
-              </nui-button>
-            </div>
-          </div>
-          <figure class="Home__Welcome__Media">
-            <nui-media src="https://player.vimeo.com/video/311756540" style="margin: 0;"/>
-            <p v-html="$store.state.homepage.welcome_figure.body"></p>
-          </figure>
-        </nui-row>
-      </nui-container>
-    </nui-hero>
-    <s-welcome/>
-    <s-why/>
-    <s-modes/>
+    <home-welcome/>
+    <section>
+      <br><br><br><br><br>Wtf<br><br><br><br><br>
+    </section>
+    <!-- <s-why/> -->
+    <!-- <s-modes/>
     <s-codesandbox/>
-    <s-backers/>
-    <nuxt-footer/>
+    <s-backers/> -->
+    <!-- <nuxt-footer/> -->
   </div>
 </template>
 
@@ -42,7 +21,7 @@ import nuiButton from '@/components/ui/Button.vue'
 import nuiSvgPlay from '@/components/svg/Play.vue'
 import nuiSvgGh from '@/components/svg/Github.vue'
 
-import sWelcome from '@/partials/home/welcome'
+import homeWelcome from '@/partials/home/welcome'
 import sWhy from '@/partials/home/why'
 
 export default {
@@ -54,7 +33,7 @@ export default {
     nuiButton,
     nuiSvgPlay,
     nuiSvgGh,
-    sWelcome,
+    homeWelcome,
     sWhy,
     sModes: () => import('@/partials/home/modes'),
     sCodesandbox: () => import('@/partials/home/codesandbox'),
@@ -89,48 +68,9 @@ export default {
 $grey_blue: #2F495E;
 $light_green: #00C58E;
 $grey: #606F7B;
+$light_grey: #F1F5F8;
 
 .Home {
-  &__Welcome {
-    padding-bottom: 6rem;
-    &__Text {
-      width: 40%;
-      h1 {
-        color: $grey_blue;
-        font-size: 2.25rem;
-        line-height: 3.25rem;
-        font-weight: 700;
-        margin: 0;
-        span {
-          color: $light_green;
-        }
-      }
-      h3 {
-        color: $grey;
-        font-size: 1.15rem;
-        line-height: 1.8rem;
-        font-weight: 700;
-        margin: 1.25rem 0;
-      }
-      &__Buttons {
-        padding: 1.25rem 0;
-        .nui-button {
-          margin-right: 1rem;
-          .nui-svg-gh {
-            height: 20px;
-          }
-        }
-      }
-    }
-    &__Media {
-      width: 40%;
-      margin: 0;
-      p {
-        color: $grey;
-        font-size: 0.9rem;
-        text-align: center;
-      }
-    }
-  }
+  background-color: $light_grey;
 }
 </style>

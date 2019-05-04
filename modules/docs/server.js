d3aae89d841cd2f806ef59e22d83b4cb797ed725
pooya parsa <pooya@pi0.ir>
2019-05-05 01:40:56 +0430

    refactor: improve docs module

--- modules/docs/server.js
+++ modules/docs/server.js
@@ -1,372 +1,358 @@
 const { promisify } = require('util')
 const { resolve } = require('path')
 const fs = require('fs')
-
 const micro = require('micro')
 const { send } = require('micro')
-const marked = require('marked')
-const highlightjs = require('highlight.js')
 const fm = require('front-matter')
-const logger = require('consola').withScope('docs/server')
-const octicon = require('octicons')
 const fetch = require('node-fetch')
-
 const glob = promisify(require('glob'))
+const chokidar = require('chokidar')
+const degit = require('degit')
+const logger = require('consola').withScope('docs/server')
+const { marked, partialRenderer } = require('./renderer')
 const readFile = promisify(fs.readFile)
 
-const defaultLang = 'en'
+class DocsServer {
+  constructor(options) {
+    this.docsDir = options.docsDir || process.cwd()
+    this.defaultLang = options.defaultLang || 'en'
+    this.port = options.port || 3001
+    this.repo = options.repo
+    this.watch = options.watch
 
-// Use highlight.js for code blocks
-const renderer = new marked.Renderer()
-renderer.code = (code, language) => {
-  const validLang = !!(language && highlightjs.getLanguage(language))
-  const highlighted = validLang ? highlightjs.highlight(language, code).value : code
-  return `<pre><code class="hljs ${language}">${highlighted}</code></pre>`
-}
-renderer.heading = (text, level) => {
-  const patt = /\s?{([^}]+)}$/
-  let link = patt.exec(text)
+    this.releases = []
+    this.docsFiles = {}
+    this.menu = {}
+    this.langs = {}
+    this.homepage = {}
 
-  if (link && link.length && link[1]) {
-    text = text.replace(patt, '')
-    link = link[1]
-  } else {
-    link = text.toLowerCase().replace(/[^\wА-яіІїЇєЄ\u4e00-\u9eff一-龠ぁ-ゔァ-ヴー々〆〤\u3130-\u318F\uAC00-\uD7AF]+/gi, '-')
+    this.server = null
   }
-  return '<h' + level + '>' +
-    '<a id="' + link + '" class="anchor" aria-hidden="true" href="#' + link + '">' +
-      octicon.link.toSVG() +
-    '</a>' + text + '</h' + level + '>'
-}
-marked.setOptions({ renderer })
 
-// Partial renderer for homepage object
-const partialRenderer = new marked.Renderer()
-partialRenderer.paragraph = (text) => {
-  return text + '<br>'
-}
+  // Initialize
+  async init() {
+    await Promise.all([
+      this.getFiles(),
+      this.getReleases()
+    ])
 
-// Fetch releases
-let RELEASES = []
-
-const getReleases = async () => {
-  logger.info('Fetching releases...')
-  const options = {}
-  if (process.env.GITHUB_TOKEN) {
-    options.headers = { 'Authorization': `token ${process.env.GITHUB_TOKEN}` }
+    if (this.watch) {
+      this.watchFiles()
+    }
   }
-  try {
-    const data = await fetch('https://api.github.com/repos/nuxt/nuxt.js/releases', options).then(res => res.json())
-    RELEASES = data.filter(r => !r.draft).map((release) => {
-      return {
-        name: release.name || release.tag_name,
-        date: release.published_at,
-        body: marked(release.body)
+
+  // Start server
+  async listen() {
+    if (this.server) {
+      return
+    }
+
+    this.server = micro(async (req, res) => {
+      res.setHeader('Access-Control-Allow-Origin', '*')
+      res.setHeader('Access-Control-Allow-Methods', 'GET')
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+      try {
+        const data = this.get(req.url)
+        send(res, 200, data)
+      } catch (err) {
+        send(res, 404, err.message)
       }
     })
-  } catch (e) {
-    logger.error('Could not fetch nuxt.js release notes:', e.message)
+
+    this.server.listen = promisify(this.server.listen)
+    this.server.close = promisify(this.server.close)
+
+    await this.server.listen(this.port)
+
+    this.server.url = `http://localhost:${this.port}`
+    logger.success(`Docs server listening on \`${this.server.url}\``)
   }
-  const getMajorVersion = r => r.name && Number(r.name.substring(1, 2))
-  RELEASES.sort((a, b) => {
-    const aMajorVersion = getMajorVersion(a)
-    const bMajorVersion = getMajorVersion(b)
-    if (aMajorVersion !== bMajorVersion) {
-      return bMajorVersion - aMajorVersion
-    }
-    return new Date(b.date) - new Date(a.date)
-  })
-}
 
-// Fetch doc and menu files
-let _DOC_FILES_ = {}
-
-async function getFiles(cwd) {
-  logger.info('Building files...')
-  cwd = cwd || process.cwd()
-  const docPaths = await glob('*/**/*.md', {
-    cwd: cwd,
-    ignore: 'node_modules/**/*',
-    nodir: true
-  })
-
-  const promises = []
-  const tmpDocFiles = {}
-  docPaths.forEach((path) => {
-    const promise = getDocFile(path, cwd)
-    promise.then((file) => {
-      tmpDocFiles[path] = file
-    })
-    promises.push(promise)
-  })
-
-  await Promise.all(promises)
-  _DOC_FILES_ = tmpDocFiles
-  // Construct the doc menu
-  await getMenu(cwd)
-  // Construct the lang object
-  await getLanguages(cwd)
-  // Construct the homepage object
-  await getHomepage(cwd)
-}
-
-// Get doc file and sent back it's attributes and html body
-async function getDocFile(path, cwd, parseOptions) {
-  cwd = cwd || process.cwd()
-  let file = await readFile(resolve(cwd, path), 'utf-8')
-  // transform markdown to html
-  file = fm(file)
-  _DOC_FILES_[path] = {
-    attrs: file.attributes,
-    body: marked(file.body, parseOptions)
-  }
-  return _DOC_FILES_[path]
-}
-
-// Get menu files and create the doc menu
-let _MENU_ = {}
-
-async function getMenu(cwd) {
-  logger.info('Building menu...')
-  cwd = cwd || process.cwd()
-  const menuPaths = await glob('*/**/menu.json', {
-    cwd: cwd,
-    ignore: 'node_modules/**/*',
-    nodir: true
-  })
-  const tmpMenu = {}
-  const promises = []
-  menuPaths.forEach((path) => {
-    let menu = tmpMenu
-    const keys = path.split('/').slice(0, -1)
-    keys.forEach((key, i) => {
-      if ((i + 1) === keys.length) {
-        const promise = readFile(resolve(cwd, path), 'utf-8')
-        promise.then((fileContent) => {
-          menu[key] = JSON.parse(fileContent)
-        }).catch((e) => { logger.error(e, path) })
-        promises.push(promise)
-        return
-      }
-      menu[key] = menu[key] || {}
-      menu = menu[key]
-    })
-  })
-  await Promise.all(promises)
-  _MENU_ = tmpMenu
-}
-
-// Get lang files and create the lang object
-let _LANG_ = {}
-
-async function getLanguages(cwd) {
-  logger.info('Building languages...')
-  cwd = cwd || process.cwd()
-  const langPaths = await glob('*/lang.json', {
-    cwd: cwd,
-    ignore: 'node_modules/**/*',
-    nodir: true
-  })
-  const tmpLang = {}
-  const promises = []
-  langPaths.forEach((path) => {
-    const lang = path.split('/')[0]
-    const promise = readFile(resolve(cwd, path), 'utf-8')
-    promise.then((fileContent) => {
-      tmpLang[lang] = JSON.parse(fileContent)
-    })
-    promises.push(promise)
-  })
-  await Promise.all(promises)
-  _LANG_ = tmpLang
-}
-
-// Get homepage files and create the homepage object
-let _HOMEPAGE_ = {}
-
-async function getHomepage(cwd) {
-  logger.info('Building homepage object...')
-  cwd = cwd || process.cwd()
-  const homepagePaths = await glob('*/homepage/*.md', {
-    cwd: cwd,
-    ignore: 'node_modules/**/*',
-    nodir: true
-  })
-  const tmpHomepage = {}
-  const promises = []
-  homepagePaths.forEach((path) => {
-    const lang = path.split('/')[0]
-    const homepage = path.split('/')[1]
-    const part = path.split('/')[2].replace(/.md$/, '')
-    const parseOptions = {
-      renderer: partialRenderer
-    }
-    const promise = getDocFile(path, cwd, parseOptions)
-    promise.then((file) => {
-      if (!tmpHomepage[lang]) {
-        tmpHomepage[lang] = {}
-      }
-      tmpHomepage[lang][part] = file
-    })
-    promises.push(promise)
-  })
-  await Promise.all(promises)
-
-  // Copy fallback resource from defaultLang: en
-  // Target check uses _LANG_, */lang.json
-  Object.keys(_LANG_).map((lang) => {
-    Object.keys(tmpHomepage[defaultLang]).map((part) => {
-      if (!tmpHomepage[lang]) {
-        tmpHomepage[lang] = {}
-      }
-      if (!tmpHomepage[lang][part]) {
-        tmpHomepage[lang][part] = tmpHomepage[defaultLang][part]
-        tmpHomepage[lang][part]['attrs']['fallback'] = true
-      }
-    })
-  })
-
-  _HOMEPAGE_ = tmpHomepage
-}
-
-// watch file changes
-function watchFiles(cwd) {
-  logger.info('Watch files changes...')
-  const options = {
-    cwd: cwd,
-    ignoreInitial: true,
-    ignored: 'node_modules/**/*'
-  }
-  const chokidar = require('chokidar')
-  // Doc Pages
-  chokidar.watch('*/**/*.md', options)
-    .on('add', path => getDocFile(path, cwd))
-    .on('change', path => getDocFile(path, cwd))
-    .on('unlink', path => delete _DOC_FILES_[path])
-  // Menu
-  chokidar.watch('*/**/menu.json', options)
-    .on('add', () => getMenu(cwd))
-    .on('change', () => getMenu(cwd))
-    .on('unlink', () => getMenu(cwd))
-  // Lang
-  chokidar.watch('*/lang.json', options)
-    .on('add', () => getLanguages(cwd))
-    .on('change', () => getLanguages(cwd))
-    .on('unlink', () => getLanguages(cwd))
-  // Homepage
-  chokidar.watch('*/homepage/*.md', options)
-    .on('add', () => getHomepage(cwd))
-    .on('change', () => getHomepage(cwd))
-    .on('unlink', () => getHomepage(cwd))
-}
-
-// Server handle request method
-function get(url) {
-  // Releases
-  if (url === '/releases') {
-    return RELEASES
-  }
-  // Menu
-  if (url.indexOf('/menu') === 0) {
-    const lang = url.split('/')[2]
-    const category = url.split('/')[3]
-    if (lang && category && _MENU_[lang] && _MENU_[lang][category]) {
-      return _MENU_[lang][category]
+  // Stop server
+  async close() {
+    if (!this.server) {
+      return
     }
 
-    if (lang && _MENU_[lang]) {
-      return _MENU_[lang]
-    }
-
-    if (lang) {
-      throw new Error('Language not found')
-    }
-
-    return _MENU_
-  }
-  // Lang
-  if (url.indexOf('/lang') === 0) {
-    const lang = url.split('/')[2]
-    if (lang && _LANG_[lang]) {
-      return _LANG_[lang]
-    }
-
-    if (lang) {
-      throw new Error('Language not found')
-    }
-
-    return _LANG_
-  }
-  // Homepgae
-  if (url.indexOf('/homepage') === 0) {
-    const lang = url.split('/')[2]
-    if (lang && _HOMEPAGE_[lang]) {
-      return _HOMEPAGE_[lang]
-    }
-
-    if (lang) {
-      throw new Error('Language not found')
-    }
-
-    return _HOMEPAGE_
+    logger.info(`Stopping docs server...`)
+    await this.server.close()
+    delete this.server
   }
 
-  // remove first /
-  const path = url.slice(1) + '.md'
-  // Check if path exists
-
-  if (!_DOC_FILES_[path]) {
-    throw new Error('File not found')
-  }
-  // Send back doc content
-  return _DOC_FILES_[path]
-}
-
-let server
-
-// Start server
-exports.start = async function start(cwd, port) {
-  if (server) {
-    return
-  }
-
-  await Promise.all([
-    getFiles(cwd),
-    getReleases()
-  ])
-
-  if (process.env.NODE_ENV !== 'production') {
-    watchFiles(cwd)
-  }
-
-  server = micro(async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET')
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  // Fetch releases
+  async getReleases() {
+    logger.info('Fetching releases...')
+    const options = {}
+    if (process.env.GITHUB_TOKEN) {
+      options.headers = { 'Authorization': `token ${process.env.GITHUB_TOKEN}` }
+    }
     try {
-      const data = get(req.url)
-      send(res, 200, data)
-    } catch (err) {
-      send(res, 404, err.message)
+      const data = await fetch('https://api.github.com/repos/nuxt/nuxt.js/releases', options).then(res => res.json())
+      this.releases = data.filter(r => !r.draft).map((release) => {
+        return {
+          name: release.name || release.tag_name,
+          date: release.published_at,
+          body: marked(release.body)
+        }
+      })
+    } catch (e) {
+      logger.error('Could not fetch nuxt.js release notes:', e.message)
     }
-  })
-  server.listen = promisify(server.listen)
-  server.close = promisify(server.close)
-
-  await server.listen(port)
-
-  server.url = `http://localhost:${port}`
-
-  logger.success(`Docs server listening on ${server.url}`)
-}
-
-// Stop server
-exports.stop = async function stop() {
-  if (!server) {
-    return
+    const getMajorVersion = r => r.name && Number(r.name.substring(1, 2))
+    this.releases.sort((a, b) => {
+      const aMajorVersion = getMajorVersion(a)
+      const bMajorVersion = getMajorVersion(b)
+      if (aMajorVersion !== bMajorVersion) {
+        return bMajorVersion - aMajorVersion
+      }
+      return new Date(b.date) - new Date(a.date)
+    })
   }
 
-  logger.info(`Stopping docs server...`)
-  await server.close()
-  server = null
+  // Fetch doc and menu files
+  async getFiles() {
+    logger.info('Building files...')
+
+    // Get all docs files
+    await this.getDocFiles()
+
+    // Construct the doc menu
+    await this.getMenu()
+
+    // Construct the lang object
+    await this.getLanguages()
+
+    // Construct the homepage object
+    await this.getHomepage()
+  }
+
+  // Get all docs files
+  async getDocFiles() {
+    const docPaths = await this.glob('*/**/*.md')
+    await Promise.all(docPaths.map((path) => this.getDocFile(path)))
+  }
+
+  // Get doc file and sent back it's attributes and html body
+  async getDocFile(path, parseOptions, useCache = true) {
+    if (this.docsFiles[path] && useCache) {
+      return this.docsFiles[path]
+    }
+
+    // Read file
+    let file = await readFile(resolve(this.docsDir, path), 'utf-8')
+
+    // Transform markdown to html
+    file = fm(file)
+
+    this.docsFiles[path] = {
+      attrs: file.attributes,
+      body: marked(file.body, parseOptions)
+    }
+
+    return this.docsFiles[path]
+  }
+
+  // Get menu files and create the doc menu
+  async getMenu() {
+    logger.info('Building menu...')
+
+    const menuPaths = await this.glob('*/**/menu.json')
+
+    const menu = {}
+
+    await Promise.all(menuPaths.map(async path => {
+      const keys = path.split('/').slice(0, -1)
+      const lastKey = keys.pop()
+
+      let pathMenu = menu
+      for (const key of keys) {
+        pathMenu[key] = pathMenu[key] || {}
+        pathMenu = pathMenu[key]
+      }
+
+      const fileContent = await readFile(resolve(this.docsDir, path), 'utf-8')
+      pathMenu[lastKey] = JSON.parse(fileContent)
+    }))
+
+    this.menu = menu
+  }
+
+  // Get lang files and create the lang object
+  async getLanguages() {
+    logger.info('Building languages...')
+
+    const langPaths = await this.glob('*/lang.json')
+
+    const langs = {}
+
+    await Promise.all(langPaths.map(async path => {
+      const lang = path.split('/')[0]
+      const fileContent = await readFile(resolve(this.docsDir, path), 'utf-8')
+      langs[lang] = JSON.parse(fileContent)
+    }))
+
+    this.langs = langs
+  }
+
+  // Get homepage files and create the homepage object
+  async getHomepage() {
+    logger.info('Building homepage...')
+
+    const homepagePaths = await this.glob('*/homepage/*.md')
+
+    const homepage = {}
+
+    await Promise.all(homepagePaths.map(async (path) => {
+      const lang = path.split('/')[0]
+      // const homepage = path.split('/')[1]
+      const part = path.split('/')[2].replace(/.md$/, '')
+      const file = await this.getDocFile(path, { renderer: partialRenderer })
+      if (!homepage[lang]) {
+        homepage[lang] = {}
+      }
+      homepage[lang][part] = file
+    }))
+
+    // Copy fallback resource from defaultLang: en
+    // Target check uses this.langs, */lang.json
+    for (const lang in this.langs) {
+      if (lang === this.defaultLang) {
+        continue
+      }
+      for (const part in homepage[this.defaultLang]) {
+        if (!homepage[lang]) {
+          homepage[lang] = {}
+        }
+        if (!homepage[lang][part]) {
+          homepage[lang][part] = homepage[this.defaultLang][part]
+          homepage[lang][part]['attrs']['fallback'] = true
+        }
+      }
+    }
+
+    this.homepage = homepage
+  }
+
+  // watch file changes
+  watchFiles(cwd) {
+    logger.info('Watching files changes...')
+
+    const options = {
+      cwd: cwd,
+      ignoreInitial: true,
+      ignored: 'node_modules/**/*'
+    }
+
+    // Doc Pages
+    chokidar.watch('*/**/*.md', options)
+      .on('add', path => this.getDocFile(path, null, false))
+      .on('change', path => this.getDocFile(path, null, false))
+      .on('unlink', path => delete this.docsFiles[path])
+
+    // Menu
+    chokidar.watch('*/**/menu.json', options)
+      .on('add', () => this.getMenu())
+      .on('change', () => this.getMenu())
+      .on('unlink', () => this.getMenu())
+
+    // Lang
+    chokidar.watch('*/lang.json', options)
+      .on('add', () => this.getLanguages())
+      .on('change', () => this.getLanguages())
+      .on('unlink', () => this.getLanguages())
+
+    // Homepage
+    chokidar.watch('*/homepage/*.md', options)
+      .on('add', () => this.getHomepage())
+      .on('change', () => this.getHomepage())
+      .on('unlink', () => this.getHomepage())
+  }
+
+  // Server handle request method
+  get(url) {
+    // Releases
+    if (url === '/releases') {
+      return this.releases
+    }
+
+    // Menu
+    if (url.indexOf('/menu') === 0) {
+      const lang = url.split('/')[2]
+      const category = url.split('/')[3]
+
+      if (lang && category && this.menu[lang] && this.menu[lang][category]) {
+        return this.menu[lang][category]
+      }
+
+      if (lang && this.menu[lang]) {
+        return this.menu[lang]
+      }
+
+      if (lang) {
+        throw new Error('Language not found')
+      }
+
+      return this.menu
+    }
+
+    // Lang
+    if (url.indexOf('/lang') === 0) {
+      const lang = url.split('/')[2]
+
+      if (lang && this.langs[lang]) {
+        return this.langs[lang]
+      }
+
+      if (lang) {
+        throw new Error('Language not found')
+      }
+
+      return this.langs
+    }
+    // Homepgae
+    if (url.indexOf('/homepage') === 0) {
+      const lang = url.split('/')[2]
+
+      if (lang && this.homepage[lang]) {
+        return this.homepage[lang]
+      }
+
+      if (lang) {
+        throw new Error('Language not found')
+      }
+
+      return this.homepage
+    }
+
+    // Remove first /
+    const path = url.slice(1) + '.md'
+
+    // Check if path exists
+    if (!this.docsFiles[path]) {
+      throw new Error('File not found')
+    }
+
+    // Send back doc content
+    return this.docsFiles[path]
+  }
+
+  // Glob util
+  glob(pattern) {
+    return glob(pattern, {
+      cwd: this.docsDir,
+      ignore: 'node_modules/**/*',
+      nodir: true
+    })
+  }
+
+  // Clone repository
+  async cloneRepo() {
+    if (fs.existsSync(this.docsDir)) {
+      return
+    }
+    logger.info(`Cloning ${this.repo} into ${this.docsDir}`)
+    await degit(this.repo, { force: true, cache: false }).clone(this.docsDir)
+  }
 }
+
+module.exports = DocsServer

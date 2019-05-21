bfeb046cce5beeb97c4f784720534bfaa80462d7
Alex <alex@orion.sh>
2019-05-21 15:08:14 +0200

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    fill: theme => ({
+      'red': theme('colors.red.500'),
+      'green': theme('colors.green.500'),
+      'blue': theme('colors.blue.500'),
+    })
+  },
+  variants: {},
+  plugins: []
+}

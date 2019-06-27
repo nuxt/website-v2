be2403bbfe65fdecddd22edc2bd5be7c7368831c
Alex <alex@orion.sh>
2019-06-27 13:17:27 +0200

    chore (add): responsive header and align footer

--- components/nui/partials/Footer.vue
+++ components/nui/partials/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="bg-white shadow z-10 relative">
     <nui-container class="flex items-center content-center justify-between py-10">
-      <nav v-for="(l, title, index) in links" :key="title" :class="{'text-center': index === 1, 'text-right': index === 2}">
+      <nav v-for="(l, title, index) in links" :key="title" class="flex-1" :class="{'text-center': index === 1, 'text-right': index === 2}">
         <h3 class="font-bold uppercase text-lg pb-4 text-nuxt-gray">{{ title }}</h3>
         <ul>
           <li v-for="(link, i) in l" :key="i" class="py-2">
@@ -17,20 +17,26 @@
     </nui-container>
     <div class="border-t border-gray-300 py-4">
       <nui-container class="flex items-center content-center justify-between">
-        <nui-select v-model="themes.current" :options="themes.options">
-          <template v-slot:icon>
-            <component :is="currentThemeIcon"/>
-          </template>
-        </nui-select>
-        <a class="block" :href="localePath('index')" @click.prevent="$router.push(localePath('index'))" @click.right.stop.prevent="$router.push(localePath('resources-design'))">
-          <h1 class="m-0 h-0 w-0 overflow-hidden">NUXTJS</h1>
-          <nui-logo class="h-6 lg:h-auto"/>
-        </a>
-        <nui-select v-model="currentLang" :options="$i18n.locales">
-          <template v-slot:icon>
-            <nui-globe/>
-          </template>
-        </nui-select>
+        <div class="flex-1">
+          <nui-select v-model="themes.current" :options="themes.options">
+            <template v-slot:icon>
+              <component :is="currentThemeIcon"/>
+            </template>
+          </nui-select>
+        </div>
+        <div class="flex-1 text-center">
+          <a class="block" :href="localePath('index')" @click.prevent="$router.push(localePath('index'))" @click.right.stop.prevent="$router.push(localePath('resources-design'))">
+            <h1 class="m-0 h-0 w-0 overflow-hidden">NUXTJS</h1>
+            <nui-logo class="h-6 lg:h-auto"/>
+          </a>
+        </div>
+        <div class="flex-1 text-right">
+          <nui-select v-model="currentLang" :options="$i18n.locales">
+            <template v-slot:icon>
+              <nui-globe/>
+            </template>
+          </nui-select>
+        </div>
       </nui-container>
     </div>
   </footer>

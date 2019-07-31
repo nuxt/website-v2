62f51aaaf8660f79b0483e3d0b7162964bfc9501
Alex <alex@orion.sh>
2019-07-31 18:02:45 +0200

    [error] add svg for animations and color changes

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,101 +1,40 @@
 <template>
-  <div>
-    <section class="Landscape">
-      <img src="~static/triangles.png" alt="Logo nuxt" class="Landscape__Image"/>
-      <h1 class="Landscape__Title">
-        {{ error.statusCode }}
-      </h1>
-      <h2 class="Landscape__SubTitle">
-        {{ error.message }}
-      </h2>
-      <ul class="Landscape__List">
-        <li class="Landscape__List__Item">
-          <nuxt-link v-if="error.statusCode === 404" class="button button--green" to="/">
-            {{ $store.state.lang.links.homepage }}
-          </nuxt-link>
-        </li>
-      </ul>
-    </section>
-    <nuxt-footer></nuxt-footer>
+  <div class="relative py-24">
+    <nui-container>
+      <div class="flex justify-between">
+        <div class="lg:w-1/2 xl:w-6/12 text-center lg:text-left">
+          <h1 class="text-6xl font-medium leading-normal text-nuxt-lightgreen">
+            {{ error.statusCode }}
+          </h1>
+          <h3 class="text-3xl text-nuxt-gray font-medium leading-relaxed mb-6">
+            {{ error.message }}
+          </h3>
+          <!-- <div class="py-4">
+            <nui-button to="/" green class="mr-4">
+              <nui-svg-play/>
+              get started
+            </nui-button>
+            <nui-button to="/">
+              <nui-svg-gh/>
+              20K+ github stars
+            </nui-button>
+          </div> -->
+        </div>
+        <div class="hidden lg:block lg:w-5/12">
+          <mountains-globe/>
+        </div>
+      </div>
+    </nui-container>
   </div>
 </template>
 
 <script>
-import NuxtFooter from '@/components/partials/Footer.vue'
+import mountainsGlobe from '@/components/svg/streamline/montains-globe'
 
 export default {
   props: ['error'],
   components: {
-    NuxtFooter
+    mountainsGlobe
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.Landscape
-{
-  $footer-height: 50px;
-
-  min-height: calc(100vh - #{$footer-height});
-  background-color: #fff;
-  padding: 50px 15px;
-  padding-top: 120px;
-  text-align: center;
-  @media (min-width: 991px)
-  {
-    padding: 100px 30px;
-    padding-top: 180px;
-  }
-  &__Image
-  {
-    &--mobile
-    {
-      display: inline-block;
-      @media (min-width: 991px)
-      {
-        display: none;
-      }
-    }
-    &--desktop
-    {
-      display: none;
-      @media (min-width: 991px)
-      {
-        display: inline-block;
-      }
-    }
-  }
-  &__Title
-  {
-    font-weight: 300;
-    letter-spacing: 1px;
-    margin: 20px 0;
-    color: #35495e;
-    font-size: 128px;
-    line-height: 100px;
-  }
-  &__SubTitle
-  {
-    letter-spacing: 1px;
-    line-height: normal;
-    margin: 20px 0;
-    color: #35495e;
-  }
-  &__List
-  {
-    list-style: none;
-    margin: 0;
-    padding: 0;
-    &__Item
-    {
-      display: inline-block;
-      padding: 20px 15px;
-      padding-bottom: 0;
-      .button
-      {
-        font-size: 16px;
-      }
-    }
-  }
-}
-</style>

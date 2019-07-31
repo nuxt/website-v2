1a0cf61f45624e9a9950f9cf65b11e413f804fa6
Alex <alex@orion.sh>
2019-07-31 18:23:37 +0200

    [WIP] 404 page design

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,26 +1,21 @@
 <template>
-  <div class="relative py-24">
+  <div class="relative">
     <nui-container>
-      <div class="flex justify-between">
-        <div class="lg:w-1/2 xl:w-6/12 text-center lg:text-left">
+      <div class="flex items-center justify-between">
+        <div class="lg:w-1/2 text-center lg:text-left">
           <h1 class="text-6xl font-medium leading-normal text-nuxt-lightgreen">
             {{ error.statusCode }}
           </h1>
-          <h3 class="text-3xl text-nuxt-gray font-medium leading-relaxed mb-6">
+          <h3 class="text-4xl text-nuxt-gray font-medium leading-relaxed mb-6">
             {{ error.message }}
           </h3>
-          <!-- <div class="py-4">
+          <div class="inline-block py-4">
             <nui-button to="/" green class="mr-4">
-              <nui-svg-play/>
-              get started
+              Go back home...
             </nui-button>
-            <nui-button to="/">
-              <nui-svg-gh/>
-              20K+ github stars
-            </nui-button>
-          </div> -->
+          </div>
         </div>
-        <div class="hidden lg:block lg:w-5/12">
+        <div class="hidden lg:block lg:w-5/12 xl:w-4/12">
           <mountains-globe/>
         </div>
       </div>

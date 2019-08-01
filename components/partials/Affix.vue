afc5f73549ab9f0366a988107a28451b0b26ef21
Alex <alex@orion.sh>
2019-08-01 19:15:04 +0200

    add footer links pages and fix design affix

--- components/partials/Affix.vue
+++ components/partials/Affix.vue
@@ -2,10 +2,10 @@
   <div class="affix fixed text-center hidden md:block overflow-y-scroll bottom-0 py-8 pl-4 w-40">
     <div class="h-auto pb-8">
       <p class="text-sm mb-2 text-center text-gray-600">Platinum Sponsors</p>
-      <a v-for="sponsor in sponsors" :key="sponsor.name" :href="sponsor.link" class="inline-block w-32 mb-4">
+      <a v-for="sponsor in sponsors" :key="sponsor.name" :href="sponsor.link" class="inline-block mb-4">
         <img :src="sponsor.img" :alt="sponsor.name">
       </a>
-      <nui-button :to="localePath('index')" class="text-center">Sponsorship</nui-button>
+      <nui-button :to="localePath('support-nuxtjs')" class="justify-center">Support Us</nui-button>
     </div>
     <slot/>
   </div>
---
+++ pages/discover/events.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    events
+  </div>
+</template>

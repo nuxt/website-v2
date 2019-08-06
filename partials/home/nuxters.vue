71a21a0c07049af68a9f65b940b4ddef5a678c77
Alex <alex@orion.sh>
2019-08-06 17:58:00 +0200

    clean homepage and reorder sections

--- partials/home/nuxters.vue
+++ partials/home/nuxters.vue
@@ -1,33 +1,36 @@
 <template>
-  <nui-hero class="Home__Showcase">
-    <nui-container>
-      <div>
-        <h1>
-          Who's using nuxt<span>js</span>
-        </h1>
-      </div>
+  <nui-container class="py-12">
+    <section>
+      <h1 class="text-3xl uppercase text-center pb-8">
+        Nuxters
+      </h1>
+      <p>
+        Nuxt.js is an MIT licensed open source project and completely free to use.
+        However, the amount of effort needed to maintain and develop new features for the project is not sustainable without proper financial backing.
+        If you run a business and are using Nuxt in a revenue-generating product, it makes business sense to sponsor Nuxt development: it ensures the project that your product relies on stays healthy and actively maintained. It can also help your exposure in the Vue/Nuxt community and makes it easier to attract Vue/Nuxt developers.
+        If you are an individual user and have enjoyed the productivity of using Nuxt, consider donating as a sign of appreciation.
+      </p>
+      <p>TODO ! - opencollective API pour recuperer les images des sponsors uniquement + section and XXX Nuxters </p>
       <div>
         <a class="Home__Showcase__link" v-for="(partner, i) in partners" :key="i" :href="partner.url" target="_blank" rel="noopener">
           <img :src="'/img/partners/' + partner.img" :alt="partner.name" />
         </a>
       </div>
       <div>
-        <nui-button to="/support-us" green>
-          <nui-svg-play/>
+        <nui-button :to="localePath({ name: 'learn-section-slug', params: { section: 'guide', slug: 'installation'}})" class="mr-4 py-3 px-6 text-base">
+          <nui-svg-play slot="icon" class="h-4 -mt-1 mr-1"/>
           discover more showcase
         </nui-button>
       </div>
-    </nui-container>
-  </nui-hero>
+    </section>
+  </nui-container>
 </template>
 
 <script>
-// import nuiTitle from '@/components/commons/Title'
 import nuiSvgPlay from '@/components/svg/Play.vue'
 
 export default {
   components: {
-    // nuiTitle,
     nuiSvgPlay
   },
   data () {

1d3770b88e762aab1afc01b5f2dd633c80bad2ca
Alex <alex@orion.sh>
2019-08-08 11:36:16 +0200

    sponsors and flat routes with header links

--- components/partials/Header.vue
+++ components/partials/Header.vue
@@ -1,19 +1,19 @@
 <template>
   <header class="header fixed top-0 right-0 left-0 z-10">
-    <nui-container>
-      <div class="flex py-6 lg:pt-8 items-end lg:items-center justify-between border-b border-gray-300">
+    <nui-container class="border-b border-gray-300 lg:border-0">
+      <div class="flex py-6 lg:pt-8 items-end lg:items-center justify-between lg:border-b lg:border-gray-300">
         <a href="#" @click.prevent="$emit('toggle', !mobileNav)" class="flex p-2 -m-2 items-center justify-center text-nuxt-gray hover:text-nuxt-lightgreen z-10 lg:hidden">
           <nui-times-icon v-if="mobileNav" class="block h-5 fill-current"/>
           <nui-bars-icon v-else class="block h-5 fill-current"/>
         </a>
-        <a class="block h-7 lg:h-10 lg:pt-1 z-10" :href="localePath('index')" @click.prevent="$router.push(localePath('index'))" @click.right.stop.prevent="$router.push(localePath('resources-design'))">
+        <a class="block h-7 lg:h-10 lg:pt-1 z-10" :href="localePath('index')" @click.prevent="$router.push(localePath('index'))" @click.right.stop.prevent="$router.push(localePath('design'))">
           <h1 class="m-0 h-0 w-0 overflow-hidden">NUXTJS</h1>
           <nui-logo class="h-6 lg:h-auto"/>
         </a>
         <nav class="header_nav lg:block lg:relative lg:pt-0 fixed top-0 bottom-0 left-0 right-0 pt-24 overflow-y-auto" :class="{'hidden': !mobileNav}">
           <ul class="lg:flex lg:pt-1 xl:pt-0 container mx-auto text-center">
             <li v-for="link in links" :key="link" class="xl:px-4 lg:py-0 lg:px-2 py-2">
-              <nuxt-link class="header_nav_link block p-2 font-medium uppercase hover:no-underline hover:text-nuxt-lightgreen" :to="localePath({ name: 'learn-section-slug', params: { section: link } })">
+              <nuxt-link class="header_nav_link block p-2 font-medium uppercase hover:no-underline hover:text-nuxt-lightgreen" :to="localePath(link + '-slug')">
                 {{ $store.state.lang.links[link] || link}}
               </nuxt-link>
             </li>
--- partials/home/targets.vue
+++ partials/home/targets.vue
@@ -4,31 +4,22 @@
       <h1 class="text-3xl uppercase text-center pb-8">
         Nuxt<span class="text-nuxt-lightgreen">JS</span> targets
       </h1>
-      <p> Explain the new mode with steps:</p>
-      <p> 1) Create or migrate your Vue.js Application on NuxtJS</p>
-      <p> 2) Do you need to render every page?</p>
-      <p> YES - Image + SSR mode / NO - Image + SPA mode description</p>
-      <p> 3) Do you want to host your Application on a server or a CDN?</p>
-      <p> SERVER - Image + production ready / SERVERLESS - Image + static files generated
       <div class="flex items-end lg:items-center justify-between">
-        <i-performant/>
         <div class="Home__Modes__Section">
-          <h4 class="Home__Modes__Section__Title">{{ $store.state.homepage.modes_server_side_rendering.attrs.content_title }}</h4>
-          <p class="Home__Modes__Section__Description" v-html="$store.state.homepage.modes_server_side_rendering.body"></p>
+          <h4 class="Home__Modes__Section__Title">Server</h4>
+          <p class="Home__Modes__Section__Description">Heroku, Digital Ocean, OVH...</p>
         </div>
       </div>
       <div>
-        <i-modular/>
         <div class="Home__Modes__Section">
-          <h4 class="Home__Modes__Section__Title">{{ $store.state.homepage.modes_statically_generated.attrs.content_title }}</h4>
-          <p class="Home__Modes__Section__Description" v-html="$store.state.homepage.modes_statically_generated.body"></p>
+          <h4 class="Home__Modes__Section__Title">Serverless</h4>
+          <p class="Home__Modes__Section__Description">Zeit, Amazon Lambda, Google Cloud Function, Azure...</p>
         </div>
       </div>
       <div>
-        <i-enjoyable/>
         <div class="Home__Modes__Section">
-          <h4 class="Home__Modes__Section__Title">{{ $store.state.homepage.modes_single_page_app.attrs.content_title }}</h4>
-          <p class="Home__Modes__Section__Description" v-html="$store.state.homepage.modes_single_page_app.body"></p>
+          <h4 class="Home__Modes__Section__Title">Static (JAM Stack ready)</h4>
+          <p class="Home__Modes__Section__Description">Netlify, GitHub Pages, Surge...</p>
         </div>
       </div>
     </section>
@@ -36,16 +27,7 @@
 </template>
 
 <script>
-import iPerformant from '@/components/svg/Rocket'
-import iModular from '@/components/svg/Puzzle'
-import iEnjoyable from '@/components/svg/Dancers'
-
 export default {
-  components: {
-    iPerformant,
-    iModular,
-    iEnjoyable
-  }
 }
 </script>
 

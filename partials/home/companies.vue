9018aa0413578ee14fbbff714bc299473554b856
Alex <alex@orion.sh>
2019-08-08 17:49:44 +0200

    companies gray with hover transition

--- partials/home/companies.vue
+++ partials/home/companies.vue
@@ -6,7 +6,7 @@
       </h1>
       <div class="flex items-center justify-between">
         <div v-for="(company, i) in companies[$i18n.locale]" :key="i" class="p-8 text-center">
-          <a :href="company.url" target="_blank" rel="noopener">
+          <a :href="company.url" target="_blank" rel="noopener" class="company">
             <img :src="'/img/companies/' + $i18n.locale + '/' + company.img" :alt="company.name" :class="company.class">
           </a>
         </div>
@@ -21,32 +21,72 @@ export default {
     return {
       companies: {
         en: [
-
+          { name: 'Ubisoft', img: 'ubisoft-logo.png', url: 'https://assassinscreed.ubisoft.com/story-creator-mode', class: 'h-16' },
+          { name: 'Maison du monde', img: 'maison-du-monde.png', url: 'https://www.maisonsdumonde.com/', class: 'h-16' },
+          { name: 'BackMarket', img: 'backmarket.png', url: 'https://www.backmarket.fr', class: 'h-10' },
+          { name: 'Faire', img: 'faire.png', url: 'https://www.faire.fr', class: 'h-16' },
+          { name: 'Roland Garros', img: 'roland-garros.png', url: 'https://www.rolandgarros.com', class: 'h-16' },
         ],
         fr: [
-          { name: 'Ubisoft', img: 'ubisoft-logo.png', url: 'https://assassinscreed.ubisoft.com/story-creator-mode', class: 'h-20' },
-          { name: 'Maison du monde', img: 'maison-du-monde.png', url: 'https://www.maisonsdumonde.com/', class: 'h-20' },
-          { name: 'BackMarket', img: 'backmarket.png', url: 'https://www.backmarket.fr', class: 'h-12' },
+          { name: 'Ubisoft', img: 'ubisoft-logo.png', url: 'https://assassinscreed.ubisoft.com/story-creator-mode', class: 'h-16' },
+          { name: 'Maison du monde', img: 'maison-du-monde.png', url: 'https://www.maisonsdumonde.com/', class: 'h-16' },
+          { name: 'BackMarket', img: 'backmarket.png', url: 'https://www.backmarket.fr', class: 'h-10' },
           { name: 'Faire', img: 'faire.png', url: 'https://www.faire.fr', class: 'h-16' },
-          { name: 'Roland Garros', img: 'roland-garros.png', url: 'https://www.rolandgarros.com', class: 'h-20' },
+          { name: 'Roland Garros', img: 'roland-garros.png', url: 'https://www.rolandgarros.com', class: 'h-16' },
         ],
         zh: [
-
+          { name: 'Ubisoft', img: 'ubisoft-logo.png', url: 'https://assassinscreed.ubisoft.com/story-creator-mode', class: 'h-16' },
+          { name: 'Maison du monde', img: 'maison-du-monde.png', url: 'https://www.maisonsdumonde.com/', class: 'h-16' },
+          { name: 'BackMarket', img: 'backmarket.png', url: 'https://www.backmarket.fr', class: 'h-10' },
+          { name: 'Faire', img: 'faire.png', url: 'https://www.faire.fr', class: 'h-16' },
+          { name: 'Roland Garros', img: 'roland-garros.png', url: 'https://www.rolandgarros.com', class: 'h-16' },
         ],
         ja: [
-
+          { name: 'Ubisoft', img: 'ubisoft-logo.png', url: 'https://assassinscreed.ubisoft.com/story-creator-mode', class: 'h-16' },
+          { name: 'Maison du monde', img: 'maison-du-monde.png', url: 'https://www.maisonsdumonde.com/', class: 'h-16' },
+          { name: 'BackMarket', img: 'backmarket.png', url: 'https://www.backmarket.fr', class: 'h-10' },
+          { name: 'Faire', img: 'faire.png', url: 'https://www.faire.fr', class: 'h-16' },
+          { name: 'Roland Garros', img: 'roland-garros.png', url: 'https://www.rolandgarros.com', class: 'h-16' },
         ],
         ko: [
-
+          { name: 'Ubisoft', img: 'ubisoft-logo.png', url: 'https://assassinscreed.ubisoft.com/story-creator-mode', class: 'h-16' },
+          { name: 'Maison du monde', img: 'maison-du-monde.png', url: 'https://www.maisonsdumonde.com/', class: 'h-16' },
+          { name: 'BackMarket', img: 'backmarket.png', url: 'https://www.backmarket.fr', class: 'h-10' },
+          { name: 'Faire', img: 'faire.png', url: 'https://www.faire.fr', class: 'h-16' },
+          { name: 'Roland Garros', img: 'roland-garros.png', url: 'https://www.rolandgarros.com', class: 'h-16' },
         ],
         ru: [
-
+          { name: 'Ubisoft', img: 'ubisoft-logo.png', url: 'https://assassinscreed.ubisoft.com/story-creator-mode', class: 'h-16' },
+          { name: 'Maison du monde', img: 'maison-du-monde.png', url: 'https://www.maisonsdumonde.com/', class: 'h-16' },
+          { name: 'BackMarket', img: 'backmarket.png', url: 'https://www.backmarket.fr', class: 'h-10' },
+          { name: 'Faire', img: 'faire.png', url: 'https://www.faire.fr', class: 'h-16' },
+          { name: 'Roland Garros', img: 'roland-garros.png', url: 'https://www.rolandgarros.com', class: 'h-16' },
         ],
         id: [
-
+          { name: 'Ubisoft', img: 'ubisoft-logo.png', url: 'https://assassinscreed.ubisoft.com/story-creator-mode', class: 'h-16' },
+          { name: 'Maison du monde', img: 'maison-du-monde.png', url: 'https://www.maisonsdumonde.com/', class: 'h-16' },
+          { name: 'BackMarket', img: 'backmarket.png', url: 'https://www.backmarket.fr', class: 'h-10' },
+          { name: 'Faire', img: 'faire.png', url: 'https://www.faire.fr', class: 'h-16' },
+          { name: 'Roland Garros', img: 'roland-garros.png', url: 'https://www.rolandgarros.com', class: 'h-16' },
         ]
       }
     }
   }
 }
 </script>
+
+<style>
+.company {
+  & img {
+    opacity: 0.75;
+    filter: grayscale(100%);
+    transition: all 0.5s;
+  }
+  &:hover {
+    & img {
+      opacity: 1;
+      filter: grayscale(0%);
+    }
+  }
+}
+</style>

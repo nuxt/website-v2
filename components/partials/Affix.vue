6881f36a9e826d9fde30dd5cb98de4143aa011b6
Alex <alex@orion.sh>
2019-08-21 17:32:55 +0200

    responsive affix

--- components/partials/Affix.vue
+++ components/partials/Affix.vue
@@ -1,17 +1,45 @@
 <template>
-  <div class="hidden fixed inset-0 pt-16 h-full bg-white z-90 w-full border-b lg:-mb-0 lg:static lg:h-auto lg:overflow-y-visible lg:border-b-0 lg:pt-0 lg:w-1/4 lg:block lg:border-0">
-    <div class="h-full overflow-y-auto scrolling-touch lg:h-auto lg:block lg:relative lg:sticky lg:top-24 bg-white lg:bg-transparent">
-      <div class="px-6 pt-6 overflow-y-auto text-center lg:pt-8 lg:pr-0 lg:pl-8 sticky?lg:h-(screen-24)">
-        <p class="text-sm text-gray-600">
-          Platinum Sponsors
-        </p>
-        <a v-for="sponsor in sponsors" :key="sponsor.name" :href="sponsor.link" class="inline-block m-4">
-          <img :src="sponsor.img" :alt="sponsor.name">
-        </a>
-        <nui-button :to="localePath('support-nuxtjs')" class="justify-center mb-4">
-          Support Us
-        </nui-button>
-        <slot />
+  <!-- <div class="lg:fixed lg:inset-0 lg:pt-16 lg:h-full lg:z-90 w-full lg:-mb-0 lg:static lg:h-auto lg:overflow-y-visible lg:pt-0 lg:w-1/4 lg:block">
+    <div class="lg:h-full lg:overflow-y-auto lg:scrolling-touch lg:h-auto lg:block lg:relative lg:sticky lg:top-24 lg:bg-transparent">
+      <div class="flex justify-between px-4 pt-8 lg:flex-col lg:overflow-y-auto lg:text-center lg:px-6 lg:pr-0 lg:pl-8 sticky?lg:h-(screen-24)">
+        <div>
+          <p class="text-sm text-gray-600">
+            Platinum Sponsors
+          </p>
+          <a v-for="sponsor in sponsors" :key="sponsor.name" :href="sponsor.link" class="inline-block lg:m-4">
+            <img :src="sponsor.img" :alt="sponsor.name" class="h-12">
+          </a>
+          <p>
+            <nui-button :to="localePath('support-nuxtjs')" class="justify-center mb-4">
+              Support Us
+            </nui-button>
+          </p>
+        </div>
+        <div class="pl-8">
+          <slot/>
+        </div>
+      </div>
+    </div>
+  </div> -->
+  <div class="lg:inset-0 z-90 lg:-mb-0 lg:static lg:h-auto lg:overflow-y-visible lg:pt-0 lg:w-1/4 lg:block">
+    <div class="lg:overflow-y-auto lg:scrolling-touch lg:h-auto lg:block lg:relative lg:sticky lg:top-24">
+      <div class="flex px-4 pt-8 justify-between lg:justify-start lg:flex-col lg:overflow-y-auto lg:pr-0 lg:pl-8 sticky?lg:h-(screen-24)">
+        <div class="lg:text-center">
+          <p class="lg:text-sm text-gray-600">
+            Platinum Sponsors
+          </p>
+          <a v-for="sponsor in sponsors" :key="sponsor.name" :href="sponsor.link" class="inline-block my-4">
+            <img :src="sponsor.img" :alt="sponsor.name" class="h-16">
+          </a>
+          <p>
+            <nui-button :to="localePath('support-nuxtjs')" class="justify-center mb-4">
+              Support Us
+            </nui-button>
+          </p>
+        </div>
+        <div class="-m-4 lg:m-0">
+          <slot />
+        </div>
       </div>
     </div>
   </div>

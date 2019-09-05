d1fcea9e9eda801c953bbd3f8d5f2ffcc860e1bb
Sébastien Chopin <seb@nuxtjs.com>
2019-09-05 10:44:52 +0200

    fix: minor fixes

--- components/partials/ads/CarbonText.vue
+++ components/partials/ads/CarbonText.vue
@@ -13,6 +13,9 @@ export default {
   },
   methods: {
     loadScript () {
+      if (window._bsa) {
+        return this.scriptLoaded()
+      }
       // Order is important (https://stackoverflow.com/questions/16230886/trying-to-fire-the-onload-event-on-script-tag)
       const script = document.createElement('script')
       script.setAttribute('type', 'text/javascript')
@@ -21,7 +24,7 @@ export default {
       script.setAttribute('src', '//m.servedby-buysellads.com/monetization.js')
     },
     scriptLoaded () {
-      if (typeof _bsa === 'undefined') { return console.warn('Could not load Carbon Ads Text') } // eslint-disable-line no-console
+      if (typeof window._bsa === 'undefined') { return console.warn('Could not load Carbon Ads Text') } // eslint-disable-line no-console
       window._bsa.init('custom', 'CKYD62QW', 'placement:nuxtjsorg', {
         target: '#bsa-native',
         template: '<a class="native-box" href="##statlink##"><div class="native-sponsor">Sponsor</div><div class="native-text"><strong>##company##</strong> — ##description##</div></a>'

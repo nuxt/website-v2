ee284a7f00b2514985200eb041e1e6e57179c524
Alex <alex@orion.sh>
2019-09-20 18:58:43 +0200

    New Responsive Header mobile first

--- components/partials/Header.vue
+++ components/partials/Header.vue
@@ -1,58 +1,80 @@
 <template>
-  <header class="header fixed top-0 right-0 left-0 z-20">
-    <nui-container class="border-b border-gray-300 lg:border-0">
-      <div class="flex py-6 lg:pt-8 items-end lg:items-center justify-between lg:border-b lg:border-gray-300">
-        <a href="#" class="flex p-2 -m-2 items-center justify-center z-10 lg:hidden hover:text-nuxt-lightgreen" @click.prevent="$emit('toggle', !mobileNav)">
-          <nui-times-icon v-if="mobileNav" class="block h-5 fill-current text-white" />
-          <nui-bars-icon v-else class="block h-5 fill-current text-nuxt-gray" />
-        </a>
-        <a class="header_logo lg:text-nuxt-gray block h-7 lg:h-10 lg:pt-1 z-10" :class="mobileNav ? 'text-white' : 'text-nuxt-gray'" :href="localePath('index')" @click.prevent="$router.push(localePath('index'))" @click.right.stop.prevent="$router.push(localePath('design'))">
-          <h1 class="m-0 h-0 w-0 overflow-hidden">NUXTJS</h1>
-          <nui-logo class="h-6 lg:h-auto" />
-        </a>
-        <nav class="header_nav lg:block lg:relative lg:pt-0 fixed top-0 bottom-0 left-0 right-0 pt-24 overflow-y-auto" :class="{'header_nav--open': mobileNav}">
-          <!-- Desktop Navigation -->
-          <ul class="hidden lg:flex lg:pt-1 xl:pt-0 container mx-auto text-center">
+  <div class="absolute">
+    <!-- Common Header -->
+    <header class="header border-b border-gray-300 lg:border-0" :class="action === 'search' ? 'py-4' : 'py-6 lg:py-0'">
+      <template v-if="action === ''">
+        <nui-container class="flex items-center justify-between lg:py-6">
+          <!-- Left Title -->
+          <a class="block text-nuxt-gray h-7 lg:h-10 lg:pt-1 z-10" :href="localePath('index')" @click.prevent="$router.push(localePath('index'))" @click.right.stop.prevent="$router.push(localePath('design'))">
+            <h1 class="m-0 h-0 w-0 overflow-hidden">NUXTJS</h1>
+            <nui-logo class="h-6 lg:h-auto" />
+          </a>
+          <!-- Center Navigation -->
+          <ul class="hidden lg:flex lg:pt-1 xl:pt-0 text-center">
             <li v-for="link in links" :key="link" class="header_nav_link xl:px-4 lg:py-0 lg:px-2 py-2">
               <nuxt-link class="block p-2 font-medium uppercase hover:no-underline hover:text-nuxt-lightgreen" :to="localePath({ name: 'section-slug', params: { section: link }})">
                 {{ $store.state.lang.links[link] || link }}
               </nuxt-link>
             </li>
           </ul>
-          <!-- Mobile Navigation -->
-          <ul class="lg:hidden container mx-auto text-center">
-            <li v-for="link in links" :key="link" class="header_nav_link xl:px-4 lg:py-0 lg:px-2 py-2" :class="{'header_nav_link--close': currentSection !== '' && currentSection !== link}">
-              <a class="text-lg block p-4 font-medium uppercase hover:no-underline" :class="{'bg-nuxt-lightgreen text-nuxt-gray': currentSection === link, 'hover:text-nuxt-lightgreen': currentSection !== link}" :href="localePath({ name: 'section-slug', params: { section: link }})" @click.prevent="currentSection = (currentSection === link ? '' : link)">
-                <nui-arrow-left v-show="currentSection === link" class="absolute float-left h-6"/>
-                {{ $store.state.lang.links[link] || link }}
-              </a>
-              <div v-for="(group, index) in $store.state.menu[link]" :key="index" class="header_subnav" :class="{'header_subnav--open': currentSection === link}">
-                <h3 :key="`title-${index}`" class="uppercase text-gray-500 pb-2">
-                  {{ group.title }}
-                </h3>
-                <ul :key="`list-${index}`">
-                  <li v-for="link in group.links" :key="link.to" class="py-2">
-                    <nuxt-link class="text-gray-200 hover:text-nuxt-lightgreen" :class="{'text-nuxt-lightgreen': path === menu + link.to}" :to="menu + link.to" exact>
-                      {{ link.name }}
-                    </nuxt-link>
-                  </li>
-                </ul>
-              </div>
+          <!-- Right Action -->
+          <a href="#" class="block md:hidden flex p-2 -m-2 items-center justify-center text-nuxt-gray hover:text-nuxt-lightgreen z-10 lg:hidden" @click.prevent="$emit('change', 'search')">
+            <nui-search-icon class="block h-5 fill-current" />
+          </a>
+          <nui-search class="hidden md:block -my-2"/>
+        </nui-container>
+        <nui-container class="hidden lg:block">
+          <hr class="border-b border-t-0 border-gray-300 h-0"/>
+        </nui-container>
+      </template>
+      <nui-container v-else class="flex justify-between lg:hidden">
+        <!-- Left Title -->
+        <nui-search v-if="action === 'search'" class="w-full pr-4"/>
+        <div v-else class="flex items-end">
+          <component :is="'nui-' + action + '-icon'" class="block h-6 text-nuxt-lightgreen fill-current" />
+          <span class="block text-lg font-medium uppercase text-nuxt-gray pl-4 h-6">{{ $store.state.lang.links[action] || action }}</span>
+        </div>
+        <!-- Right Action -->
+        <a href="#" class="block flex p-2 -m-2 items-center justify-center text-nuxt-gray hover:text-nuxt-lightgreen z-10 lg:hidden" :class="action === 'search' ? 'pt-3' : ''" @click.prevent="$emit('change', '')">
+          <nui-times-icon class="block h-5 fill-current"/>
+        </a>
+      </nui-container>
+    </header>
+    <!-- Mobile Main Navigation -->
+    <nav class="header_mobile_nav block lg:hidden">
+      <nui-container class="flex justify-between">
+        <a v-for="link in links" :key="link" class="block md:flex p-2 md:p-4 text-nuxt-gray hover:no-underline hover:text-nuxt-lightgreen text-center visited:text-nuxt-gray" :href="localePath({ name: 'section-slug', params: { section: action }})" @click.prevent="$emit('change', action === link ? '' : link)">
+          <component :is="'nui-' + link + '-icon'" class="inline-block h-5 fill-current mb-1" :class="{'text-nuxt-lightgreen': action === link}"/>
+          <span class="block text-xs md:text-base md:pl-3 font-medium text-nuxt-gray">{{ $store.state.lang.links[link] || link }}</span>
+        </a>
+      </nui-container>
+    </nav>
+    <!-- Mobile Aside Navigation -->
+    <div class="header_mobile_aside block lg:hidden" :class="{'header_mobile_aside--open': sublinks.length}">
+      <nui-container>
+        <transition-group v-for="(group, index) in sublinks" :key="index" tag="div" name="list" class="header_mobile_aside_group">
+          <h3 :key="`title-${index}`" class="uppercase text-gray-500 pb-2">
+            {{ group.title }}
+          </h3>
+          <ul :key="`list-${index}`" class="pb-6">
+            <li v-for="l in group.links" :key="l.to" class="py-2">
+              <nuxt-link class="block text-gray-700 hover:text-nuxt-lightgreen" :class="{'text-nuxt-lightgreen': path === locale + l.to}" :to="locale + l.to" exact>
+                {{ l.name }}
+              </nuxt-link>
             </li>
           </ul>
-        </nav>
-        <nui-search class="lg:block hidden" />
-        <a href="#" class="block flex p-2 -m-2 items-center justify-center text-nuxt-gray hover:text-nuxt-lightgreen z-10 lg:hidden" @click.prevent="openSearch = !openSearch">
-          <nui-search-icon class="block h-5 fill-current" />
-        </a>
-      </div>
-    </nui-container>
-  </header>
+        </transition-group>
+      </nui-container>
+    </div>
+  </div>
 </template>
 
 <script>
 import nuiLogo from '@/components/svg/Nuxtjs'
-import nuiBarsIcon from '@/components/svg/Bars'
+import nuiExamplesIcon from '@/components/svg/Code'
+import nuiGuideIcon from '@/components/svg/Books'
+import nuiApiIcon from '@/components/svg/List'
+import nuiFaqIcon from '@/components/svg/Faq'
 import nuiTimesIcon from '@/components/svg/Times'
 import nuiSearchIcon from '@/components/svg/Search'
 import nuiSearch from '@/components/partials/Search'
@@ -60,32 +82,84 @@ import nuiArrowLeft from '@/components/svg/ArrowLeft'
 
 export default {
   components: {
-    nuiBarsIcon,
     nuiTimesIcon,
+    nuiExamplesIcon,
+    nuiGuideIcon,
+    nuiApiIcon,
+    nuiFaqIcon,
     nuiLogo,
     nuiSearchIcon,
     nuiSearch,
     nuiArrowLeft
   },
   model: {
-    prop: 'mobileNav',
-    event: 'toggle'
+    prop: 'action',
+    event: 'change'
   },
   props: {
-    mobileNav: {
-      type: Boolean,
+    action: {
+      type: String,
       required: true
     }
   },
   data () {
     return {
-      currentSection: '',
-      links: ['guide', 'api', 'examples', 'faq'],
-      openSearch: false
+      links: ['guide', 'api', 'examples', 'faq']
     }
   },
   computed: {
-    menu () { return (this.$i18n && this.$i18n.locale !== 'en' ? `/${this.$i18n.locale}/` : '/') + this.$route.params.section }
+    path () { return this.$route.path.slice(-1) === '/' ? this.$route.path.slice(0, -1) : this.$route.path },
+    locale () { return (this.$i18n && this.$i18n.locale !== 'en' ? `/${this.$i18n.locale}/` : '/') + this.action },
+    sublinks () { return this.$store.state.menu[this.action] || [] }
+  },
+  methods: {
+    nav (section) {
+      this.currentSection = (this.currentSection === section ? '' : section)
+      this.mobileNav = !this.mobileNav
+    }
   }
 }
 </script>
+
+<style>
+.header {
+  @apply fixed bg-white top-0 left-0 right-0 z-30;
+}
+  .header_mobile_nav {
+    @apply fixed bg-white border-t border-gray-300 left-0 bottom-0 right-0 z-30;
+  }
+  .header_mobile_aside {
+    @apply fixed left-0 bg-gray-100 pt-6 z-20 w-full overflow-y-auto;
+    top: 72px;
+    bottom: 60px;
+    transform: translateX(-100%);
+    transition-property: transform;
+    transition-duration: 1s;
+    transition-timing-function: cubic-bezier(0.23, 1, 0.32, 1);
+    /* transition-delay: .5s; */
+    & .header_mobile_aside_group {
+      transform: translateX(-100%);
+      transition-property: transform;
+      transition-duration: 0.3s;
+      transition-timing-function: ease-in-out;
+      &:nth-child(1) { transition-delay: 0.2s; }
+      &:nth-child(2) { transition-delay: 0.3s; }
+      &:nth-child(3) { transition-delay: 0.4s; }
+      &:nth-child(4) { transition-delay: 0.5s; }
+    }
+  }
+  .header_mobile_aside--open {
+    transform: translateX(0px);
+    transition-delay: 0s;
+    & .header_mobile_aside_group {
+      transform: translateX(0px);
+    }
+  }
+.list-enter-active, .list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
+</style>

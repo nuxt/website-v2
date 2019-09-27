aa3b67c83497c77145af523c1a8542a6329fea1f
Sébastien Chopin <seb@nuxtjs.com>
2019-09-27 12:38:13 +0200

    fix: paddings

--- components/partials/home/modes.vue
+++ components/partials/home/modes.vue
@@ -7,21 +7,21 @@
       <div class="flex flex-wrap">
         <div class="lg:w-1/2 p-4 lg:p-8">
           <i-fire class="text-nuxt-green mx-auto my-8 w-32"/>
-          <h4 class="text-center uppercase text-2xl py-8">
+          <h4 class="text-center uppercase text-2xl pt-8 pb-4">
             {{ $store.state.homepage.modes_server_side_rendering.attrs.content_title }}
           </h4>
           <p class="leading-loose text-justify" v-html="$store.state.homepage.modes_server_side_rendering.body" />
         </div>
         <div class="lg:w-1/2 p-4 lg:p-8">
           <i-snow class="mx-auto my-8 w-32"/>
-          <h4 class="text-center uppercase text-2xl py-8">
+          <h4 class="text-center uppercase text-2xl pt-8 pb-4">
             {{ $store.state.homepage.modes_statically_generated.attrs.content_title }}
           </h4>
           <p class="leading-loose text-justify" v-html="$store.state.homepage.modes_statically_generated.body" />
         </div>
       </div>
       <div class="px-4 lg:p-8">
-        <h4 class="uppercase text-2xl py-8">
+        <h4 class="uppercase text-2xl pt-8 pb-4">
           {{ $store.state.homepage.modes_single_page_app.attrs.content_title }}
         </h4>
         <p class="leading-loose text-justify" v-html="$store.state.homepage.modes_single_page_app.body" />

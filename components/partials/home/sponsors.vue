b60afb55219f7d02c104fc600901c10a7f370c26
mornir <jeromepott@hotmail.com>
2019-10-01 17:06:54 -0700

    Align text on the left + add scss style

--- components/partials/home/sponsors.vue
+++ components/partials/home/sponsors.vue
@@ -5,7 +5,7 @@
         Sponsors
       </h1>
       <i-sponsoring class="inline-block float-right ml-4" />
-      <div class="pt-6 pb-12 leading-loose">
+      <div class="pt-6 pb-12 leading-loose text-left">
         <p>
           NuxtJS is an MIT licensed open source project and completely free to use.
           However, the amount of effort needed to maintain and develop new features for the project is not sustainable without proper financial backing.
@@ -66,7 +66,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 .sponsor {
   & img {
     opacity: 0.75;

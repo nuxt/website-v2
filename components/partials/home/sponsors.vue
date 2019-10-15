9b2d57cf9b653d00c7dcf6d901c1c839686c7f7d
Sébastien Chopin <seb@nuxtjs.com>
2019-10-15 17:17:06 +0200

    chore: add OC sponsors

--- components/partials/home/sponsors.vue
+++ components/partials/home/sponsors.vue
@@ -59,6 +59,9 @@ export default {
           { name: 'VueSchool', img: 'vueschool.png', url: 'https://vueschool.io/?friend=nuxt&utm_source=Nuxtjs.org&utm_medium=banner&utm_campaign=Open%20Collective', class: 'h-12' },
           { name: 'ShipShape', img: 'shipshape-logo.svg', url: 'https://shipshape.io/?ref=nuxt', class: 'h-12' },
           { name: 'Hapi', img: 'hapi-logo.svg', url: 'https://hapijs.com/?ref=nuxt', class: 'h-12' }
+        ],
+        special: [
+          { name: 'Google Chrome', img: 'google-chrome.svg', url: 'https://www.google.com/chrome/?ref=nuxt', class: 'h-12' }
         ]
       }
     }

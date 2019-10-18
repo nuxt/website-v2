053299a32bc7b9b146d9f7a4f2a6833f200ecfe8
Sébastien Chopin <seb@nuxtjs.com>
2019-10-18 16:10:16 +0200

    feat: add contributors

--- components/commons/Contribute.global.vue
+++ components/commons/Contribute.global.vue
@@ -4,9 +4,16 @@
       <span v-if="prevLink">← <nuxt-link :to="baseLink + prevLink.to">{{ prevLink.name }}</nuxt-link></span>
       <span v-if="nextLink" class="inline-block float-right"><nuxt-link :to="baseLink + nextLink.to">{{ nextLink.name }}</nuxt-link> →</span>
     </div>
-    <div class="pt-6 mt-6 border-t border-gray-300">
+    <div class="pt-3 mt-6 border-t border-gray-300">
+      <h3 v-if="contributors.length" class="my-2">Contributors</h3>
+      <div v-if="contributors.length">
+        <a v-for="contributor of contributors" :key="contributor.author" :href="`https://github.com/${contributor.author}`" rel="noopener" target="_blank" class="text-nuxt-gray rounded overflow-hidden rounded-md inline-flex mb-2 mr-2 border hover:bg-gray-200">
+          <img :alt="contributor.author" :src="`https://github.com/${contributor.author}.png?size=32`" class="h-8">
+          <span class="inline-block px-2 leading-loose">{{ contributor.author }}</span>
+        </a>
+      </div>
+      <p v-if="docLink" class="text-gray-700 pt-1 mb-3">{{ $store.state.lang.guide.contribute }} <a :href="docLink" target="_blank" rel="noopener">{{ $store.state.lang.guide.edit_on_github }}</a></p>
       <carbon-ads-text :key="$route.path" />
-      <p v-if="docLink" class="text-gray-700 p-0">{{ $store.state.lang.guide.contribute }} <a :href="docLink" target="_blank" rel="noopener">{{ $store.state.lang.guide.edit_on_github }}</a></p>
     </div>
   </div>
 </template>
@@ -23,6 +30,11 @@ export default {
       type: String,
       required: false,
       default: ''
+    },
+    contributors: {
+      type: Array,
+      required: false,
+      default: () => []
     }
   },
   computed: {

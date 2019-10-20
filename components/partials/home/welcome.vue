c71dbb626b0b4adb99c304a32ef47d38d17966f1
numa <aytdm251@gmail.com>
2019-10-20 18:09:13 +0900

    refactor welcome section

--- components/partials/home/welcome.vue
+++ components/partials/home/welcome.vue
@@ -2,22 +2,22 @@
   <nui-container>
     <div class="flex justify-between py-16 sm:py-24">
       <div class="lg:w-1/2 xl:w-6/12 text-center lg:text-left p-4 sm:p-0">
-        <h1 class="text-3xl xl:text-4xl text-nuxt-gray font-medium leading-normal mb-6" v-html="$store.state.homepage.welcome.attrs.title" />
-        <h3 class="xl:text-lg text-gray-600 font-medium leading-relaxed mb-6" v-html="$store.state.homepage.welcome.body" />
+        <h1 class="text-3xl xl:text-4xl text-nuxt-gray font-medium leading-normal mb-6" v-html="wordings.welcome.attrs.title" />
+        <h3 class="xl:text-lg text-gray-600 font-medium leading-relaxed mb-6" v-html="wordings.welcome.body" />
         <div class="flex flex-col sm:block py-4">
           <nui-button :to="{ name: 'section-slug', params: { section: 'guide', slug: 'installation'} }" class="sm:mr-4 py-3 px-6 text-base mb-4">
             <nui-svg-meteor slot="icon" class="h-5 -mb-1 mr-1" />
-            {{ $store.state.lang.homepage.welcome.get_started }}
+            {{ buttonWordings.get_started }}
           </nui-button>
           <nui-button href="https://github.com/nuxt/nuxt.js" variant="gray" class="sm:mr-4 py-3 px-6 text-base">
             <nui-svg-gh slot="icon" class="h-6 -mt-1 mr-1" />
-            {{ $store.state.lang.homepage.welcome.github_stars }}
+            {{ buttonWordings.github_stars }}
           </nui-button>
         </div>
       </div>
       <figure class="hidden lg:block lg:w-5/12">
         <nui-media src="https://player.vimeo.com/video/311756540" />
-        <p class="font-medium py-2 text-xs xl:text-sm text-center text-gray-600" v-html="$store.state.homepage.welcome_figure.body" />
+        <p class="font-medium py-2 text-xs xl:text-sm text-center text-gray-600" v-html="wordings.welcome_figure.body" />
       </figure>
     </div>
   </nui-container>
@@ -34,22 +34,9 @@ export default {
   },
   data () {
     return {
-      appTypes: this.$store.state.lang.homepage.welcome.app_types,
-      current: 0
+      wordings: this.$store.state.homepage,
+      buttonWordings: this.$store.state.lang.homepage.welcome
     }
-  },
-  computed: {
-    appType () {
-      return this.appTypes[this.current]
-    }
-  },
-  mounted () {
-    this._timer = setInterval(() => {
-      this.current = (this.current + 1) % this.appTypes.length
-    }, 2500)
-  },
-  destroyed () {
-    clearInterval(this._timer)
   }
 }
 </script>

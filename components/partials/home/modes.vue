3ae694114f0fd650e49ea0eb41c2bb9b750b3bf2
numa <aytdm251@gmail.com>
2019-10-20 18:06:44 +0900

    use store instead of hard-coded wordings in mode section

--- components/partials/home/modes.vue
+++ components/partials/home/modes.vue
@@ -1,30 +1,28 @@
 <template>
   <nui-container class="pb-12 bg-gray-100 lg:bg-transparent rounded">
     <section class="lg:bg-gray-100 rounded py-6 sm:p-6">
-      <h1 class="text-3xl uppercase text-center pt-10">
-        NUXT<span class="text-nuxt-lightgreen">JS</span> rendering
-      </h1>
+      <h1 class="text-3xl uppercase text-center pt-10" v-html="wordings.modes.attrs.title" />
       <div class="flex flex-wrap">
         <div class="lg:w-1/2 p-4 lg:p-8">
           <i-fire class="text-nuxt-green mx-auto my-8 w-32"/>
           <h4 class="text-center uppercase text-2xl pt-8 pb-4">
-            {{ $store.state.homepage.modes_server_side_rendering.attrs.content_title }}
+            {{ wordings.modes_server_side_rendering.attrs.content_title }}
           </h4>
-          <p class="leading-loose text-left sm:text-justify" v-html="$store.state.homepage.modes_server_side_rendering.body" />
+          <p class="leading-loose text-left sm:text-justify" v-html="wordings.modes_server_side_rendering.body" />
         </div>
         <div class="lg:w-1/2 p-4 lg:p-8">
           <i-snow class="mx-auto my-8 w-32"/>
           <h4 class="text-center uppercase text-2xl pt-8 pb-4">
-            {{ $store.state.homepage.modes_statically_generated.attrs.content_title }}
+            {{ wordings.modes_statically_generated.attrs.content_title }}
           </h4>
-          <p class="leading-loose text-left sm:text-justify" v-html="$store.state.homepage.modes_statically_generated.body" />
+          <p class="leading-loose text-left sm:text-justify" v-html="wordings.modes_statically_generated.body" />
         </div>
       </div>
       <div class="px-4 lg:p-8">
         <h4 class="uppercase text-2xl pt-8 pb-4">
-          {{ $store.state.homepage.modes_single_page_app.attrs.content_title }}
+          {{ wordings.modes_single_page_app.attrs.content_title }}
         </h4>
-        <p class="leading-loose text-left sm:text-justify" v-html="$store.state.homepage.modes_single_page_app.body" />
+        <p class="leading-loose text-left sm:text-justify" v-html="wordings.modes_single_page_app.body" />
       </div>
     </section>
   </nui-container>
@@ -38,6 +36,11 @@ export default {
   components: {
     iSnow,
     iFire
+  },
+  data () {
+    return {
+      wordings: this.$store.state.homepage
+    }
   }
 }
 </script>

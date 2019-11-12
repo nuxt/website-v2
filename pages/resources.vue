5f0d0ded0bf25f7946a7cbfbab89621c4fae46fe
Alex <alex@orion.sh>
2019-11-12 15:27:08 +0100

    Update meta description

--- pages/resources.vue
+++ pages/resources.vue
@@ -42,7 +42,7 @@ export default {
   head: {
     title: 'NuxtJS Resources',
     meta: [
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'Discover a panel of resources made by our partners. By using thoses affiliate resources links, you are helping us to maintain and develop the Open Source Framework.' }
     ]
   }
 }

f3e5a2fcb855c7c48e47763c0124fa948744565b
flrnt <florent@nuxtjs.com>
2020-03-12 15:02:04 +0100

    refactor(blog-slug): refactor into master component : postItem

---
+++ components/commons/blog/postItem/postItem.global.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div class="mt-12">
+      <h1 class="text-3xl font-semibold mb-6">{{ post.attrs.title }}</h1>
+      <div class="text-sm">
+        <p class="text-nuxt-lightgreen">{{ post.attrs.author }}</p>
+        <div class="">
+          {{ formatDateByLocale(post.attrs.date) }}
+          <span class="text-xs mx-1">&bullet;</span>
+          {{ post.readtime.text }}
+        </div>
+      </div>
+    </div>
+    <div class="mt-12">
+      <article>
+        <html-parser :content="post.body" />
+      </article>
+    </div>
+    <div class="border-t border-gray-300 my-10">
+        <div class="flex flex-row justify-start my-10">
+          <span v-for="(tag, id) in post.attrs.tags" :key="id" class="uppercase text-xs px-1 py-1 rounded-sm mr-2 bg-gray-300">{{ tag }}</span>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BlogPostItem',
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    formatDateByLocale (d) {
+      console.log('d', d)
+      const currentLocale = this.$store.state.locale || 'en'
+      const options = { year: 'numeric', month: 'long', day: 'numeric' }
+      return new Date(d).toLocaleDateString(currentLocale, options)
+    }
+  }
+}
+</script>
+
+<style></style>
--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,27 +1,6 @@
 <template>
   <div class="max-w-4xl mx-auto">
-    <nui-article class="mt-12 mx-6">
-      <div class="blog-post-header">
-        <div class="flex justify-center">
-          <h1 class="blog-post-title">{{ page.attrs.title }}</h1>
-        </div>
-        <div class="flex items-baseline justify-between text-sm text-gray-600 font-semibold mb-8">
-          <div class="blog-post-author">{{ page.attrs.author }}</div>
-          <div class="blog-post-readtime">{{ page.readtime.text }}</div>
-        </div>
-        <div class="blog-post-description">{{ page.attrs.description }}</div>
-      </div>
-      <div class="blog-post-content">
-        <html-parser :content="page.body" />
-      </div>
-      <div v-if="hasTags" class="blog-post-footer">
-        <ul class="blog-post-tags">
-          <li v-for="(tag, i) in page.attrs.tags" :key="i" class="blog-post-tag">
-            {{ tag }}
-          </li>
-        </ul>
-      </div>
-    </nui-article>
+    <blogPostItem :post="post"/>
   </div>
 </template>
 
@@ -32,13 +11,13 @@ export default {
     const path = `/${store.state.locale}/blog/${slug}`
     const data = {
       path,
-      page: {}
+      post: {}
     }
     try {
-      const page = await $docs.get(path)
-      if (!page.attrs.title) { console.error(`[/${path}] ${store.state.lang.text.please_define_title}.`) } // eslint-disable-line no-console
-      if (!page.attrs.description) { console.error(`[/${path}] ${store.state.lang.text.please_define_description}.`) } // eslint-disable-line no-console
-      data.page = page
+      const post = await $docs.get(path)
+      if (!post.attrs.title) { console.error(`[/${path}] ${store.state.lang.text.please_define_title}.`) } // eslint-disable-line no-console
+      if (!post.attrs.description) { console.error(`[/${path}] ${store.state.lang.text.please_define_description}.`) } // eslint-disable-line no-console
+      data.post = post
     } catch (err) {
       if (!err.response || err.response.status !== 404) {
         return error({ statusCode: 500, message: store.state.lang.text.an_error_occurred })
@@ -46,39 +25,8 @@ export default {
       return error({ statusCode: 404, message: store.state.lang.text.api_page_not_found })
     }
     return data
-  },
-  computed: {
-    hasTags () {
-      return this.page.attrs.tags
-    }
   }
 }
 </script>
 
-<style lang="postcss">
-.blog-post-header {
-  @apply flex flex-col justify-center
-}
-.blog-post-title {
-  @apply font-bold text-4xl
-}
-.blog-post-author {}
-.blog-post-description {
-  @apply mb-8 text-gray-600 italic
-}
-.blog-post-content h1, h2, h3, h4, h5, h6 {
-  @apply font-semibold
-}
-.blog-post-footer {
-  @apply mt-4 border-t border-gray-300 pt-2
-}
-.blog-post-tags {
-  @apply flex list-none p-0 mt-4
-}
-.blog-post-tag {
-  @apply text-nuxt-gray text-xs py-1 px-3 bg-gray-200 rounded-sm mt-0 mb-0 ml-1 mr-1
-}
-.blog-post-tag:first-of-type {
-  @apply ml-0
-}
-</style>
+<style></style>

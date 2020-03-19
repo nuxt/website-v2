58512979094cf6d45786819af5f4fb0d915b0fe6
flrnt <florent@nuxtjs.com>
2020-03-19 18:17:38 +0100

    style(blog-post-preview-item): truncate text and display img

--- components/commons/blog/postPreviewItem/postPreviewItem.global.vue
+++ components/commons/blog/postPreviewItem/postPreviewItem.global.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="bg-light-surface dark:bg-dark-surface flex flex-col-reverse lg:flex-row mb-8 rounded py-4 px-6 hover:bg-gray-200 transition-colors duration-300 ease-linear">
-    <div class="flex flex-col justify-between pr-4">
+    <div class="w-full lg:w-3/4 flex flex-col justify-between pr-4">
       <div class="">
         <div class="font-semibold text-xl mb-3">
           <nuxt-link :to="`/blog/${post.slug}`" tag="a">{{ post.title }}</nuxt-link>
         </div>
         <div class="mb-3">
-          <p class="">{{ post.description }}</p>
+          <p class="truncate-multiline-3">{{ post.description }}</p>
         </div>
         <div class="flex flex-row justify-start mb-3">
           <span v-for="(tag, id) in post.tags" :key="id" class="uppercase text-xs px-1 py-1 rounded-sm mr-2 border">{{ tag }}</span>
@@ -21,7 +21,7 @@
       </div>
     </div>
     <div
-      class="w-auto h-48 lg:h-auto lg:w-3/4 mb-4 lg:mb-0 bg-cover bg-center"
+      class="w-full lg:w-1/4 h-48 lg:h-auto mb-4 lg:mb-0 bg-cover bg-center rounded"
       :style="{backgroundImage: `url(${post.imgUrl})` }"
     >
     </div>
@@ -48,4 +48,10 @@ export default {
 </script>
 
 <style>
+.truncate-multiline-3 {
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+}
 </style>

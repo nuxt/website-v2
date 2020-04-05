ed4e82344d5e2637dd28469e93323f30a2fddae8
Sergey Bedritsky <sergey.bedritsky@gmail.com>
2020-04-05 19:03:58 +0300

    fix dark mode error page svg (#328)

--- layouts/error.vue
+++ layouts/error.vue
@@ -2,7 +2,7 @@
   <div class="relative">
     <nui-container v-if="error.statusCode === 404" class="pt-20">
       <div class="flex flex-wrap items-center justify-between">
-        <h1 class="w-full text-3xl xl:text-4xl text-nuxt-gray text-center font-medium leading-normal -mb-6 z-10">
+        <h1 class="w-full text-3xl xl:text-4xl light:text-light-onSurfacePrimary dark:text-dark-onSurfacePrimary text-center font-medium leading-normal -mb-6 z-10">
           {{ error.message }}
         </h1>
         <div class="w-full lg:w-2/3 mx-auto">
@@ -15,7 +15,7 @@
         <h1 class="text-6xl font-medium leading-normal text-nuxt-lightgreen">
           {{ error.statusCode }}
         </h1>
-        <h3 class="text-4xl text-nuxt-gray font-medium leading-relaxed mb-6">
+        <h3 class="text-4xl light:text-light-onSurfacePrimary dark:text-dark-onSurfacePrimary font-medium leading-relaxed mb-6">
           {{ error.message }}
         </h3>
       </div>

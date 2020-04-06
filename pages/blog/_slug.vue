8af9d6f975db3a8fb4408a2d39611b3159e47e69
flrnt <flrnt@users.noreply.github.com>
2020-04-06 14:39:21 +0200

    feat: Add /blog section (#307)
    * add blog navigation links
    
    * blog: add index and slug pages
    
    * doc: add blog logic to module
    
    * run yarn add reading-time
    
    * add readtime Object to blog post payload
    
    * display readtime
    
    * style blog post footer
    
    * style(blog): remove class
    
    * fix(docs-server): add english blog posts
    
    * style(blog-pages): add responsive and minor style
    
    * refactor(blog-index): refactor into master component: blogPostPreviewItem
    
    * refactor(post-preview-item): rename global component
    
    * refactor(blog-slug): refactor into master component : postItem
    
    * style(post-item): add minor style
    
    * style(post-item): improve style
    
    * --wip-- [skip ci]
    
    * fix(header): fix route name
    
    * style(blog): apply changes for dark mode theming
    
    * improvement(docs): add dev:api:docs command
    
    * refactor(docs-server): rewrite blog logic
    
    * fix(post): change post keys according to new backend
    
    * fix(post-navigation-links): change post props to links
    
    * improvement(post-navigation-links): change nui-link to nuxt-link
    
    * fix(lang-fallback): handle dark mode switch
    
    * style(blog-post-preview-item): truncate text and display img
    
    * style(blog-post): fix according to review
    
    * fix: text-right for responsive
    
    * improvement(post-author): wrap authors into master component
    
    * style(post-link): add hover state
    
    * improvement(post-head): add missing head
    
    * refactor(post-head): extract head part from component
    
    * improvement(blog-head): add missing head to index page
    
    * postPreview update
    
    * blog updates completed
    
    * add Blog icon, blog images in static, cleanup
    
    * hotfix: filter out blog link
    
    * add Cloudinary env check
    
    * fix /guide issue, add USE_CLOUDINARY check
    
    * Blog main image preview for local, Netlify test and deploy envs
    
    * fix social meta tags
    
    * tiny typography fix
    
    * fix image host for diff env
    
    * fix
    
    * fix :(
    
    * console log on Netlify ;(
    
    * cleanup
    
    * chore: add main image for articles
    
    * fix: use span for author on listing
    
    * chore: update dependencies
    
    * add krutie image
    
    Co-authored-by: flrnt <flrnt@nuxtjs.local>
    Co-authored-by: Sébastien Chopin <seb@nuxtjs.com>
    Co-authored-by: Sergey Bedritsky <sergey.bedritsky@gmail.com>

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="bg-light-elevatedSurface dark:bg-dark-elevatedSurface transition-colors duration-300 ease-linear">
+    <div class="container lg:max-w-4xl mx-auto p-4 pb-8">
+      <div v-if="post.langFallback" class="p-4 mb-6 rounded bg-orange-200 dark:text-light-onSurfacePrimary">
+        ⚠️ You are looking at the english version of the page. Help us translate it <a :href="docLink" class="text-orange-600">here</a>.
+      </div>
+      <nuxt-link :to="{ name: 'blog' }" class="inline-flex items-center dark:hover:text-nuxt-lightgreen light:hover:text-nuxt-lightgreen dark:text-dark-onSurfaceSecondary light:text-light-onSurfaceSecondary">
+        <nuiSvgArrowLeft class="h-5 mr-2"/>
+        back to blog list
+      </nuxt-link>
+      <blogPostItem :post="post" />
+      <blogPostNavigationLinks :links="post.links" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import nuiSvgArrowLeft from '@/components/svg/ArrowLeft.vue'
+
+export default {
+  components: {
+    nuiSvgArrowLeft
+  },
+  async asyncData ({ $docs, params, store, error }) {
+    const slug = params.slug
+    const path = `/${store.state.locale}/blog/${slug}`
+    const data = {
+      path,
+      post: {}
+    }
+    try {
+      const post = await $docs.get(path)
+      if (!post.title) { console.error(`[/${path}] ${store.state.lang.text.please_define_title}.`) } // eslint-disable-line no-console
+      if (!post.description) { console.error(`[/${path}] ${store.state.lang.text.please_define_description}.`) } // eslint-disable-line no-console
+      data.post = post
+    } catch (err) {
+      if (!err.response || err.response.status !== 404) {
+        return error({ statusCode: 500, message: store.state.lang.text.an_error_occurred })
+      }
+      return error({ statusCode: 404, message: store.state.lang.text.api_page_not_found })
+    }
+    return data
+  },
+  computed: {
+    ...mapState({
+      host: state => state.host,
+      isDev: state => state.isDev,
+      isTest: state => state.isTest,
+      isProd: state => state.isProd
+    }),
+    docLink () {
+      let docLink = `https://github.com/nuxt/docs/blob/master/${this.post.path}`
+      if (this.$store.state.locale === 'ru') {
+        docLink = `https://github.com/translation-gang/ru.docs.nuxtjs/blob/translation-ru/${this.post.path}`
+      } else if (this.$store.state.locale === 'cn') {
+        docLink = `https://github.com/o2team/i18n-cn-nuxtjs-docs/blob/dev/${this.post.path}`
+      }
+      return docLink
+    },
+    socialImage () {
+      const image = this.post.imgUrl ? this.post.imgUrl : 'meta_640.png'
+      if (this.isTest || this.isDev) {
+        return `${this.host}/${image}`
+      } else {
+        return `https://res.cloudinary.com/nuxt/image/upload/w_1200,h_628,c_fill,f_auto/remote/nuxt-org/${this.post.imgUrl}`
+      }
+    }
+  },
+  head () {
+    return {
+      title: this.post.title,
+      titleTemplate: '%s - NuxtJS',
+      meta: [
+        { name: 'description', hid: 'description', content: this.post.description },
+        // Open Graph
+        { hid: 'og:site_name', property: 'og:site_name', content: process.env.DEPLOY_PRIME_URL || process.env.URL || 'http://localhost:3000' },
+        { name: 'og:title', content: this.post.title },
+        { name: 'og:description', content: this.post.description },
+        { name: 'og:type', content: 'article' },
+        { name: 'og:url', content: `https://nuxtjs.org/blog/${this.post.slug}` },
+        { name: 'og:image', content: this.socialImage },
+        // Twitter Card
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:site', content: '@nuxt_js' },
+        { name: 'twitter:title', content: this.post.title },
+        { name: 'twitter:description', content: this.post.description },
+        { name: 'twitter:image', content: this.socialImage },
+        { name: 'twitter:image:alt', content: this.post.imgUrl ? 'Blog post image' : 'NuxtJS Logo' }
+      ]
+    }
+  }
+}
+</script>
+
+<style></style>

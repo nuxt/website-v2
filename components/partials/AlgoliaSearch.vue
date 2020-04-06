b0dc4609fce045184d608051d5c43ce4eb659b52
flrnt <flrnt@users.noreply.github.com>
2020-04-06 14:39:21 +0200

    feat: Add /blog section (#307)
    * add blog navigation links
    
    * blog: add index and slug pages
    
    * doc: add blog logic to module
    
    * run yarn add reading-time
    
    * add readtime Object to blog post payload
    
    * display readtime
    
    * style blog post footer
    
    * style(blog): remove class
    
    * fix(docs-server): add english blog posts
    
    * style(blog-pages): add responsive and minor style
    
    * refactor(blog-index): refactor into master component: blogPostPreviewItem
    
    * refactor(post-preview-item): rename global component
    
    * refactor(blog-slug): refactor into master component : postItem
    
    * style(post-item): add minor style
    
    * style(post-item): improve style
    
    * --wip-- [skip ci]
    
    * fix(header): fix route name
    
    * style(blog): apply changes for dark mode theming
    
    * improvement(docs): add dev:api:docs command
    
    * refactor(docs-server): rewrite blog logic
    
    * fix(post): change post keys according to new backend
    
    * fix(post-navigation-links): change post props to links
    
    * improvement(post-navigation-links): change nui-link to nuxt-link
    
    * fix(lang-fallback): handle dark mode switch
    
    * style(blog-post-preview-item): truncate text and display img
    
    * style(blog-post): fix according to review
    
    * fix: text-right for responsive
    
    * improvement(post-author): wrap authors into master component
    
    * style(post-link): add hover state
    
    * improvement(post-head): add missing head
    
    * refactor(post-head): extract head part from component
    
    * improvement(blog-head): add missing head to index page
    
    * postPreview update
    
    * blog updates completed
    
    * add Blog icon, blog images in static, cleanup
    
    * hotfix: filter out blog link
    
    * add Cloudinary env check
    
    * fix /guide issue, add USE_CLOUDINARY check
    
    * Blog main image preview for local, Netlify test and deploy envs
    
    * fix social meta tags
    
    * tiny typography fix
    
    * fix image host for diff env
    
    * fix
    
    * fix :(
    
    * console log on Netlify ;(
    
    * cleanup
    
    * chore: add main image for articles
    
    * fix: use span for author on listing
    
    * chore: update dependencies
    
    * add krutie image
    
    Co-authored-by: flrnt <flrnt@nuxtjs.local>
    Co-authored-by: Sébastien Chopin <seb@nuxtjs.com>
    Co-authored-by: Sergey Bedritsky <sergey.bedritsky@gmail.com>

--- components/partials/AlgoliaSearch.vue
+++ components/partials/AlgoliaSearch.vue
@@ -3,7 +3,7 @@
     <button v-if="!showMobile" class="block md:hidden flex items-center p-2 -m-2 mr-4 justify-center text-light-onSurfaceSecondary dark:text-dark-onSurfaceSecondary hover:text-nuxt-lightgreen dark:hover:text-nuxt-lightgreen z-10 lg:hidden fill-current transition-colors duration-300 ease-linear" @click="showMobile = true">
       <nui-search-icon class="block h-5 fill-current" />
     </button>
-    <nui-search class="hidden md:inline-block align-middle mx-2" appearance="main" />
+    <nui-search class="hidden md:inline-block align-middle" appearance="main" />
     <div v-if="showMobile" class="mobile-search flex items-center absolute inset-0 w-full h-full md:hidden z-20">
       <div class="scrim absolute inset-0 bg-light-elevatedSurface dark:bg-dark-elevatedSurface w-full h-full transition-colors duration-300 ease-linear" />
       <nui-search class="md:inline-block w-full align-middle mx-4" :class="[showMobile && 'absolute z-20 inset-0']" appearance="mobile" @close-search="showMobile = false" />

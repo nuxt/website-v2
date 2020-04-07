268ac61b329b521695bee12bd93eabc08ba98195
Sergey Bedritsky <sergey.bedritsky@gmail.com>
2020-04-07 19:13:18 +0300

    newsletter anchor

--- components/partials/NewsletterForm.vue
+++ components/partials/NewsletterForm.vue
@@ -1,6 +1,6 @@
 <template>
   <nui-container class="px-4">
-    <section class="py-8 px-3 -mx-4 lg:mx-0 text-center rounded-t-lg">
+    <section id="subscribe-to-newsletter" class="py-8 px-3 -mx-4 lg:mx-0 text-center rounded-t-lg">
       <h2 class="text-3xl font-medium text-light-onSurfacePrimary dark:text-dark-onSurfacePrimary mb-2 transition-colors duration-300 ease-linear" v-html="headerWordings.attrs.title" />
       <p class="mb-6 text-light-onSurfaceSecondary dark:text-dark-onSurfaceSecondary transition-colors duration-300 ease-linear" v-html="headerWordings.body" />
       <client-only>

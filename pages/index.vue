9c0867d275e2bf9ba11bae6fe7f63c46eaced57e
Sébastien Chopin <seb@nuxtjs.com>
2020-05-01 17:48:48 +0200

    chore: update preview to large

--- pages/index.vue
+++ pages/index.vue
@@ -33,15 +33,16 @@ export default {
     return {
       title: this.$store.state.lang.homepage.meta.title,
       meta: [
-        { name: 'description', hid: 'description', content: this.$store.state.lang.homepage.meta.description },
+        { hid: 'description', name: 'description', content: this.$store.state.lang.homepage.meta.description },
         // Open Graph
-        { hid: 'og:title', name: 'og:title', content: this.$store.state.lang.homepage.meta.title },
-        { hid: 'og:description', name: 'og:description', content: this.$store.state.lang.homepage.meta.description },
-        { hid: 'og:type', name: 'og:type', content: 'website' },
-        { hid: 'og:url', name: 'og:url', content: 'https://nuxtjs.org' },
-        { hid: 'og:image', name: 'og:image', content: 'https://nuxtjs.org/nuxt-card.png' },
+        { hid: 'og:site_name', property: 'og:site_name', content: 'NuxtJS' },
+        { hid: 'og:title', property: 'og:title', content: this.$store.state.lang.homepage.meta.title },
+        { hid: 'og:description', property: 'og:description', content: this.$store.state.lang.homepage.meta.description },
+        { hid: 'og:type', property: 'og:type', content: 'website' },
+        { hid: 'og:url', property: 'og:url', content: 'https://nuxtjs.org' },
+        { hid: 'og:image', property: 'og:image', content: 'https://nuxtjs.org/nuxt-card.png' },
         // Twitter Card
-        { hid: 'twitter:card', name: 'twitter:card', content: 'summary' },
+        { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
         { hid: 'twitter:site', name: 'twitter:site', content: '@nuxt_js' },
         { hid: 'twitter:title', name: 'twitter:title', content: this.$store.state.lang.homepage.meta.title },
         { hid: 'twitter:description', name: 'twitter:description', content: this.$store.state.lang.homepage.meta.description },

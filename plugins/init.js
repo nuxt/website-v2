f59f7741b843edf25f3e9ada759d9ff097217370
Sébastien Chopin <seb@nuxtjs.com>
2020-05-18 11:40:45 +0200

    chore: update locale usage

--- plugins/init.js
+++ plugins/init.js
@@ -1,23 +1,9 @@
-export default async function ({ $docs, isDev, env, req, app, store: { commit, state, dispatch }, redirect }) {
+export default async function ({ $docs, env, req, store: { commit, state, dispatch }, redirect }) {
   // If state filled on server-side (to support spa fallback)
   if (state.filled) {
     return
   }
-  // If SSR
-  if (req) {
-    const hostParts = (req.headers.host || '').replace('.org', '').split('.')
-    // If url like ja.nuxtjs.org
-    if (hostParts.length === 2) {
-      if (hostParts[0] === 'www') { return redirect(301, 'https://nuxtjs.org' + req.url) }
-
-      if (isDev) {
-        commit('setLocale', hostParts[0])
-      }
-    }
-  } else {
-    // Used with nuxt generate
-    commit('setLocale', env.LOCALE)
-  }
+  commit('setLocale', env.LOCALE)
   try {
     const releases = await $docs.get('/releases')
     commit('setGhVersion', releases[0] ? releases[0].name : 'vX.Y.Z')

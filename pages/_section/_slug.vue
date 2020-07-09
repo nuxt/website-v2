60d3a4bcacdab98b42e5af40b051ebffb397d83d
debs-obrien <debs_obrien@yahoo.co.uk>
2020-07-09 11:15:18 +0200

    feat: fallback component

--- pages/_section/_slug.vue
+++ pages/_section/_slug.vue
@@ -3,17 +3,8 @@
     <div
       class="lg:min-h-screen w-full py-8 px-4 lg:static lg:overflow-visible lg:max-h-full lg:w-3/4"
     >
-      <div
-        v-if="langFallback"
-        class="p-4 mb-6 rounded bg-orange-200 dark:text-light-onSurfacePrimary"
-      >
-        ⚠️ You are looking at the english version of the page. Help us translate it
-        <a
-          :href="docLink"
-          class="text-orange-600"
-          target="_blank"
-        >here</a>.
-      </div>
+      <LangFallback :doc-link="docLink" :lang-fallback="langFallback" />
+
       <AppToc v-if="page.toc && page.toc.length" :toc="page.toc" class="mb-8 hidden lg:block" />
       <article v-if="section === 'examples'">
         <h1

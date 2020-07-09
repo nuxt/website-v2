dc38a2b364944c27fe50ac1233adca4f49d66b82
debs-obrien <debs_obrien@yahoo.co.uk>
2020-07-09 13:24:35 +0200

    fix: clean store

--- store/index.js
+++ store/index.js
@@ -7,13 +7,7 @@ export const state = () => ({
   isTest,
   isDev: !isProd && !isTest,
   filled: false,
-  // docVersion: '',
-  // ghVersion: '',
   visibleAffix: false,
-  // locale: 'en',
-  // lang: {},
-  // menu: {},
-  // homepage: {},
   adBlocked: false,
   focusMode: false
 })
@@ -22,58 +16,10 @@ export const mutations = {
   toggle (state, key) {
     state[key] = !state[key]
   },
-  // setDocVersion (state, docVersion) {
-  //   state.docVersion = docVersion
-  // },
-  // setGhVersion (state, ghVersion) {
-  //   state.ghVersion = ghVersion
-  // },
-  // setLocale (state, locale) {
-  //   state.locale = locale
-  // },
-  // setLang (state, lang) {
-  //   state.lang = lang
-  // },
-  // setMenu (state, menu) {
-  //   state.menu = menu
-  // },
-  // setHomepage (state, homepage) {
-  //   state.homepage = homepage
-  // },
   setFilled (state) {
     state.filled = true
   },
   setAdBlocked (state, value) {
     state.adBlocked = value
   }
-  // setFocusMode (state, value) {
-  //   state.focusMode = value
-  // }
 }
-
-// let _focusTimeout = null
-
-// export const actions = {
-//   // async getLangData ({ commit }, locale) {
-//   //   const lang = await this.$docs.get('/lang/' + locale)
-//   //   commit('setLang', lang)
-//   //   commit('setDocVersion', lang.docVersion)
-//   //   const menu = await this.$docs.get('/menu/' + locale)
-//   //   commit('setMenu', menu)
-//   //   const homepage = await this.$docs.get('/homepage/' + locale)
-//   //   commit('setHomepage', homepage)
-//   // },
-//   focusMode ({ commit }) {
-//     if (_focusTimeout) {
-//       return
-//     }
-//     _focusTimeout = setTimeout(() => commit('setFocusMode', true), 1300)
-//   },
-//   clearFocusMode ({ commit }) {
-//     if (_focusTimeout) {
-//       clearTimeout(_focusTimeout)
-//       _focusTimeout = null
-//     }
-//     commit('setFocusMode', false)
-//   }
-// }

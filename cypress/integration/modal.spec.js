448e5e1e936aa6037c982100b406d76a9bdfcbcd
debs-obrien <debs_obrien@yahoo.co.uk>
2020-07-18 13:24:01 +0200

    test: modify modal test to not be multiple

--- cypress/integration/modal.spec.js
+++ cypress/integration/modal.spec.js
@@ -1,23 +1,23 @@
-describe('It checks to see if people can sign up for the newsletter', () => {
+describe('It checks to see if the modals works', () => {
   beforeEach(() => {
     cy.visit(Cypress.config().baseUrl + '/guides/concepts/context-helpers')
   })
 
   it('successfully opens, checks for iframe and closes the codeSandbox modal', () => {
-    cy.get('[data-cy="modal-button"]').click({multiple:true, force:true})
+    cy.get('[data-cy="modal-button"]').first().click()
     cy.get('[data-cy="modal-open"]')
       .should('be.visible')
-    cy.get('[data-cy="modal-close"]').click({multiple:true, force:true})
+    cy.get('[data-cy="modal-close"]').first().click()
     cy.get('[data-cy="modal-button"]').should('be.visible')
   })
 
   it('successfully opens checks for image and closes the image modal', () => {
-    cy.get('[data-cy="modal-image"]').click({multiple:true, force:true})
+    cy.get('[data-cy="modal-image"]').first().click()
     cy.get('[data-cy="modal-open"]')
       .should('be.visible').within(() => {
         cy.get('img').should('be.visible')
       })
-    cy.get('[data-cy="modal-close"]').click({multiple:true, force:true})
+    cy.get('[data-cy="modal-close"]').first().click()
     cy.get('[data-cy="modal-image"]').should('be.visible')
   })
 })

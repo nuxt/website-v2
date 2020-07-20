93274f0923c2a363dbecafe10284a0ab1681cd3c
debs-obrien <debs_obrien@yahoo.co.uk>
2020-07-20 13:41:14 +0200

    test: fix modal tests

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -12,9 +12,9 @@ describe('The Home Page links', () => {
     cy.url().should('include', '/sponsor-nuxtjs')
     cy.visit(Cypress.config().baseUrl + '/sponsor-nuxtjs')
   })
-  it.skip('checks to see if the try Nuxtjs button links to ', () => {
+  it.only('checks to see if the try Nuxtjs button links to ', () => {
     cy.get('a[data-cy="why"]').click()
-    cy.url().should('eq', 'https://codesandbox.io/s/github/nuxt/codesandbox-nuxt/tree/master/')
+    .its('url').should('eq', 'https://codesandbox.io/s/github/nuxt/codesandbox-nuxt/tree/master/')
   })
   it.skip('github stars links to the right page', () => {
     cy.get('a[data-cy="github-stars"]').click()

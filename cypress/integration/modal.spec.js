a654ac7489f431038ba07b5b5e18c163fbb4b77a
debs-obrien <debs_obrien@yahoo.co.uk>
2020-07-20 13:09:11 +0200

    test: add describe block test modal

--- cypress/integration/modal.spec.js
+++ cypress/integration/modal.spec.js
@@ -1,10 +1,24 @@
-describe('testing the modal with basic test', () => {
+describe('It checks to see if the modals works', () => {
   beforeEach(() => {
-    cy.visit(Cypress.config().baseUrl + '/')
+    cy.visit(Cypress.config().baseUrl + '/guides/concepts/context-helpers')
   })
-  it('get started button links to guides', () => {
-    cy.get('a[data-cy="get-started"]').click()
-    cy.url().should('include', '/guides/get-started/installation')
-    cy.visit(Cypress.config().baseUrl + '/guides/get-started/installation')
+
+
+  it.skip('successfully opens, checks for iframe and closes the codeSandbox modal', () => {
+    cy.get('[data-cy="modal-button"]').first().click()
+    cy.get('[data-cy="modal-open"]')
+      .should('be.visible')
+    cy.get('[data-cy="modal-close"]').first().click()
+  })
+
+  it.skip('successfully opens checks for image and closes the image modal', () => {
+    cy.get('[data-cy="modal-image"]').first().click()
+    cy.get('[data-cy="modal-open"]')
+      .should('be.visible').within(() => {
+        cy.get('img').should('be.visible')
+      })
+    cy.get('[data-cy="modal-close"]').first().click()
+    cy.get('[data-cy="modal-image"]').should('be.visible')
   })
 })
+

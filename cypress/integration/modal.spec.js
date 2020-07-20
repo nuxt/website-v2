98a7db13d85cb048725eb6f00d8c8b2619f46df0
debs-obrien <debs_obrien@yahoo.co.uk>
2020-07-20 12:20:08 +0200

    test: add modal test to test netlify plugin

--- cypress/integration/modal.spec.js
+++ cypress/integration/modal.spec.js
@@ -3,7 +3,7 @@ describe('It checks to see if the modals works', () => {
     cy.visit(Cypress.config().baseUrl + '/guides/concepts/context-helpers')
   })
 
-  it.skip('successfully opens, checks for iframe and closes the codeSandbox modal', () => {
+  it('successfully opens, checks for iframe and closes the codeSandbox modal', () => {
     cy.visit(Cypress.config().baseUrl + '/guides/concepts/context-helpers')
     cy.get('[data-cy="modal-button"]').first().click()
     cy.get('[data-cy="modal-open"]')
@@ -11,7 +11,7 @@ describe('It checks to see if the modals works', () => {
     cy.get('[data-cy="modal-close"]').first().click()
   })
 
-  it.skip('successfully opens checks for image and closes the image modal', () => {
+  it('successfully opens checks for image and closes the image modal', () => {
     cy.get('[data-cy="modal-image"]').first().click()
     cy.get('[data-cy="modal-open"]')
       .should('be.visible').within(() => {

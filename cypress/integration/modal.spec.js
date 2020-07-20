1c2db4797edfa8c8e418866887cef4cc39b1324b
debs-obrien <debs_obrien@yahoo.co.uk>
2020-07-20 12:45:57 +0200

    test: test memory size for netlify build

--- cypress/integration/modal.spec.js
+++ cypress/integration/modal.spec.js
@@ -3,15 +3,14 @@ describe('It checks to see if the modals works', () => {
     cy.visit(Cypress.config().baseUrl + '/guides/concepts/context-helpers')
   })
 
-  it.skip('successfully opens, checks for iframe and closes the codeSandbox modal', () => {
-    cy.visit(Cypress.config().baseUrl + '/guides/concepts/context-helpers')
+  it('successfully opens, checks for iframe and closes the codeSandbox modal', () => {
     cy.get('[data-cy="modal-button"]').first().click()
     cy.get('[data-cy="modal-open"]')
       .should('be.visible')
     cy.get('[data-cy="modal-close"]').first().click()
   })
 
-  it.skip('successfully opens checks for image and closes the image modal', () => {
+  it('successfully opens checks for image and closes the image modal', () => {
     cy.get('[data-cy="modal-image"]').first().click()
     cy.get('[data-cy="modal-open"]')
       .should('be.visible').within(() => {

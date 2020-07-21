6bfc5da7f8d6fac635d88f349728b231ea59d7d1
debs-obrien <debs_obrien@yahoo.co.uk>
2020-07-21 15:48:43 +0200

    fix: full with home page on mobile

--- components/partials/home/HomeWelcome.vue
+++ components/partials/home/HomeWelcome.vue
@@ -4,7 +4,7 @@
   >
     <div class="container mx-auto px-4">
       <div class="flex justify-between py-16 sm:py-24">
-        <div class="lg:w-1/2 xl:w-6/12 text-center lg:text-left py-4 sm:p-0">
+        <div class="w-full lg:w-1/2 xl:w-6/12 text-center lg:text-left py-4 sm:p-0">
           <h1
             class="text-4xl xl:text-5xl text-light-onSurfacePrimary dark:text-dark-onSurfacePrimary font-medium tracking-normal leading-tight mb-6"
             v-html="$t('homepage.welcome.title')"

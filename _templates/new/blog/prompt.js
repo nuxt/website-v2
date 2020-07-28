b9937861276d340426a7229491ed5f9684d9cb05
Debbie O'Brien <debs-obrien@users.noreply.github.com>
2020-07-28 18:24:34 +0200

    feat: blog generate (#433)

--- _templates/new/blog/prompt.js
+++ _templates/new/blog/prompt.js
@@ -1,62 +1,13 @@
 module.exports = [
   {
     type: 'input',
-    name: 'title',
-    message: 'Title:',
+    name: 'filename',
+    message: 'filename:',
     validate(value) {
       if (!value.length) {
-        return 'Blog must have a title.'
+        return 'Blog must have a filename.'
       }
       return true
     }
-  },
-  {
-    type: 'input',
-    name: 'description',
-    message: 'Description:',
-    validate(value) {
-      if (!value.length) {
-        return 'Blog must have a description.'
-      }
-      return true
-    }
-  },
-  {
-    type: 'input',
-    name: 'date',
-    message: 'Date in format- 2020-06-18:',
-    validate(value) {
-      if (!value.length) {
-        return 'Blog must have a date.'
-      }
-      return true
-    }
-  },
-
-  {
-    type: 'input',
-    name: 'authorName',
-    message: 'What is the full name of the author?:',
-    validate(value) {
-      if (!value.length) {
-        return 'Blog must have an author.'
-      }
-      return true
-    }
-  },
-  {
-    type: 'input',
-    name: 'authorAvatar',
-    message: 'What is the full avatar url of the author?:'
-  },
-  {
-    type: 'input',
-    name: 'authorLink',
-    message: 'What is the twitter link of the author?:'
-  },
-  {
-    type: 'input',
-    name: 'tags',
-    message: 'Add your blog post tags'
   }
 ]

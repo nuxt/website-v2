d77b4502f07aec94e233c72f54ea4c46e7913a27
Guillaume Douceron <guillaume.douceron@free.fr>
2020-07-29 08:50:34 +0200

    add: i18n to pages (#434)

--- pages/resources.vue
+++ pages/resources.vue
@@ -6,15 +6,12 @@
           <h1
             class="text-3xl xl:text-4xl text-light-onSurfacePrimary dark:text-dark-onSurfacePrimary font-medium leading-normal mb-6 lg:pt-4"
           >
-            <AppTitle />
-            Resources<br />
+            <AppTitle /> {{ $t('resources.title') }}<br />
           </h1>
           <h3
             class="xl:text-lg text-light-onSurfaceSecondary dark:text-dark-onSurfaceSecondary font-medium leading-relaxed mb-6"
           >
-            Discover a panel of resources made by our partners. By using thoses
-            affiliate resources links, you are helping us to maintain and
-            develop the Open Source Framework.
+            {{ $t('resources.description') }}
           </h3>
         </div>
         <ThemesIllustration
@@ -29,7 +26,7 @@
             <h2
               class="text-2xl text-light-onSurfacePrimary dark:text-dark-onSurfacePrimary font-medium uppercase mb-8 transition-colors duration-300 ease-linear"
             >
-              Themes
+              {{ $t('resources.themes.title') }}
             </h2>
             <ThemingIllustration
               class="text-light-onSurfacePrimary dark:text-dark-onSurfacePrimary inline-block w-auto h-40 sm:h-56 xl:h-64 transition-colors duration-300 ease-linear"
@@ -43,7 +40,7 @@
             <h2
               class="text-2xl text-light-onSurfacePrimary dark:text-dark-onSurfacePrimary font-medium uppercase mb-8 transition-colors duration-300 ease-linear"
             >
-              Video Courses
+              {{ $t('resources.videos.title') }}
             </h2>
             <VideosIllustration
               class="text-light-onSurfacePrimary dark:text-dark-onSurfacePrimary inline-block w-auto h-40 sm:h-56 xl:h-64 transition-colors duration-300 ease-linear"

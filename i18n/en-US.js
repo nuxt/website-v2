661265d5e63d3748f186f82adf5081bb39957bf0
Zachary Burk <zachburk7@gmail.com>
2020-07-29 13:06:32 -0400

    Update en-US.js with missing space (#442)

--- i18n/en-US.js
+++ i18n/en-US.js
@@ -93,7 +93,7 @@ module.exports = {
       performant: {
         title: 'Performant',
         description:
-          'With Nuxt.js, your application will be optimized out of the box.We do our best to build performant applications by utilizing Vue.js and Node.js best practices. To squeeze every unnecessary bit out of your app Nuxt includes a bundle analyzer and lots of opportunities to fine-tune your app.'
+          'With Nuxt.js, your application will be optimized out of the box. We do our best to build performant applications by utilizing Vue.js and Node.js best practices. To squeeze every unnecessary bit out of your app Nuxt includes a bundle analyzer and lots of opportunities to fine-tune your app.'
       }
     },
     companies: {

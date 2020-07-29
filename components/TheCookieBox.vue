e42bf17e5b5a71f4bf96843313d25105533482a2
Debbie O'Brien <debs-obrien@users.noreply.github.com>
2020-07-29 13:00:32 +0200

    feat: add cookie banner (#439)

---
+++ components/TheCookieBox.vue
@@ -0,0 +1,35 @@
+<template>
+  <VueIfBot>
+    <CookieConsent
+      class="flex justify-center items-center text-light-onSurfaceSecondary dark:text-dark-onSurfaceSecondary border border-top border-light-border dark:border-dark-border xs:flex-col xs:pt-2 md:flex-row xs:text-center"
+    >
+      <template slot="message">
+        <p>
+          {{ $t('cookies.consent') }}
+          <a class="underline" href="https://www.cookiesandyou.com/">
+            {{ $t('cookies.linkLabel') }}
+          </a>
+        </p>
+      </template>
+      <template slot="button">
+        <button
+          class="ml-4 no-underline mt-4 font-medium text-sm px-4 py-2 shadow uppercase rounded hover:shadow-md sm:mr-4 py-3 px-6 text-base mb-4 primary bg-primary-base text-white hover:bg-primary-light"
+        >
+          {{ $t('cookies.button') }}
+        </button>
+      </template>
+    </CookieConsent>
+  </VueIfBot>
+</template>
+
+<script>
+import VueIfBot from 'vue-if-bot/dist/vue-if-bot.es'
+import CookieConsent from 'vue-cookieconsent-component/src/components/CookieConsent.vue'
+
+export default {
+  components: {
+    VueIfBot,
+    CookieConsent
+  }
+}
+</script>

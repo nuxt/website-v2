a89b44bad2bffd48c2feed5531cef108f1cf19ce
Debbie O'Brien <debs-obrien@users.noreply.github.com>
2020-08-04 15:21:14 +0200

    fix: newsletter test (#462)

--- cypress/integration/newsletter.spec.js
+++ cypress/integration/newsletter.spec.js
@@ -9,7 +9,7 @@ describe('It checks to see if people can sign up for the newsletter', () => {
       cy.get('[type="email"]')
         .type(this.user.email)
         .should('have.value', this.user.email)
-      cy.route('POST', '/', 'fixture:newsletter-signup/user').as('user')
+      cy.route('POST', '/', 'fixture:newsletter-signup').as('user')
       cy.get('[type="submit"]').click()
     })
     cy.get('form').next().should('contain', 'already registered')

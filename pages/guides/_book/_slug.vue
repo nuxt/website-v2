857b1c09c0e76244971d60166e7b1a6b6a4e8a4d
Debbie O'Brien <debs-obrien@users.noreply.github.com>
2020-08-24 12:56:27 +0200

    fix: add 404 page (#540)

--- pages/guides/_book/_slug.vue
+++ pages/guides/_book/_slug.vue
@@ -53,7 +53,7 @@ export default {
 
       return error({
         statusCode: 404,
-        message: app.i18n.t('common.api_page_not_found')
+        message: app.i18n.t('common.page_not_found')
       })
     }
 

a80548d9aff9669ce10db256ab6375f6534faff6
Debbie O'Brien <debs-obrien@users.noreply.github.com>
2020-08-27 12:21:11 +0200

    fix: uncomment lang until subdomain is ready

--- nuxt.config.js
+++ nuxt.config.js
@@ -197,7 +197,8 @@ export default {
         file: 'pt-BR.js',
         name: 'Português',
         domain: 'https://pt.nuxtjs.org'
-      },
+      }
+      // please leave this commented out until subdomain is live
       // {
       // code: 'tr',
       // iso: 'tr-TR',
@@ -205,13 +206,14 @@ export default {
       // name: 'Türkçe',
       // domain: 'https://tr.nuxtjs.org'
       // }
-      {
-        code: 'es',
-        iso: 'es-ES',
-        file: 'es-ES.js',
-        name: 'Español',
-        domain: 'https://es.nuxtjs.org'
-      }
+      // please leave this commented out until subdomain is live
+      // {
+      //   code: 'es',
+      //   iso: 'es-ES',
+      //   file: 'es-ES.js',
+      //   name: 'Español',
+      //   domain: 'https://es.nuxtjs.org'
+      // }
     ],
     vueI18n: {
       fallbackLocale: 'en'

0a986ecf4ced022be00b3da401008698a1f3d450
Matias Arabolaza <marab@dolby.com>
2020-09-02 07:37:02 -0300

    Translated(es-ES) es/guides/features
    deployment-targets.md
    file-system-routing.md
    rendering-modes.md
    
    Working on:
    configuration.md
    data-fetching.md
    live-preview.md
    loading.md
    meta-tags-seo.md
    nuxt-components.md
    transitions.md

--- nuxt.config.js
+++ nuxt.config.js
@@ -201,7 +201,7 @@ export default {
         file: 'pt-BR.js',
         name: 'Português',
         domain: 'https://pt.nuxtjs.org'
-      }
+      },
       // please leave this commented out until subdomain is live
       // {
       //   code: 'fa',
@@ -219,13 +219,13 @@ export default {
       // domain: 'https://tr.nuxtjs.org'
       // }
       // please leave this commented out until subdomain is live
-      // {
-      //   code: 'es',
-      //   iso: 'es-ES',
-      //   file: 'es-ES.js',
-      //   name: 'Español',
-      //   domain: 'https://es.nuxtjs.org'
-      // }
+      {
+        code: 'es',
+        iso: 'es-ES',
+        file: 'es-ES.js',
+        name: 'Español',
+        domain: 'https://es.nuxtjs.org'
+      }
     ],
     vueI18n: {
       fallbackLocale: 'en'

f83afa2d5fcabbd7e3f29f62fe2d0814f0b08312
roiLeo <medina.leo42@gmail.com>
2020-09-08 11:36:59 +0200

    fix text trad on sidenav

--- pages/guides/_book.vue
+++ pages/guides/_book.vue
@@ -20,31 +20,29 @@ export default {
     let pages = []
 
     try {
-      const locale = ['pt'].includes(app.i18n.locale)
-        ? app.i18n.locale
-        : app.i18n.defaultLocale
-
-      pages = await $content(locale, 'guides', { deep: true })
+      pages = await $content(app.i18n.defaultLocale, 'guides', { deep: true })
         .only(['slug', 'title', 'menu', 'category', 'position'])
         .sortBy('position')
         .sortBy('title')
         .sortBy('menu')
         .fetch()
 
-      // if (app.i18n.locale !== app.i18n.defaultLocale) {
-      //   const newPages = await $content(app.i18n.locale, 'guides', { deep: true })
-      //     .only(['slug', 'title', 'menu', 'category', 'position'])
-      //     .sortBy('position')
-      //     .sortBy('title')
-      //     .sortBy('menu')
-      //     .fetch()
+      if (app.i18n.locale !== app.i18n.defaultLocale) {
+        const newPages = await $content(app.i18n.locale, 'guides', {
+          deep: true
+        })
+          .only(['slug', 'title', 'menu', 'category', 'position'])
+          .sortBy('position')
+          .sortBy('title')
+          .sortBy('menu')
+          .fetch()
 
-      //   pages = pages.map((page) => {
-      //     const newPage = newPages.find(newPage => newPage.slug === page.slug)
+        pages = pages.map(page => {
+          const newPage = newPages.find(newPage => newPage.slug === page.slug)
 
-      //     return newPage || page
-      //   })
-      // }
+          return newPage || page
+        })
+      }
     } catch (e) {}
 
     return {

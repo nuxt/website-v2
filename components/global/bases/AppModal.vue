eb618987596ba201e57d286020a1bdf36bb6abc5
Azri Kahar <42867097+azrikahar@users.noreply.github.com>
2020-09-21 17:54:05 +0800

    refactor: tailwind config & CarbonAds.vue css (#739)
    * refactor: tailwind config & CarbonAds.vue css
    
    * fix "xs:" usage in templates
    
    * fix AppModal close button margin

--- components/global/bases/AppModal.vue
+++ components/global/bases/AppModal.vue
@@ -23,7 +23,7 @@
           <div class="modal-container flex flex-col">
             <div class="w-full flex justify-end">
               <button
-                class="xs:m-2 mb-4 w-auto"
+                class="m-2 w-auto"
                 data-cy="modal-close"
                 @click="showModal = false"
               >

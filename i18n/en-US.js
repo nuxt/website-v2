e9bf2a8d203f470a9f4c6503dbe6c9cb6dbfe338
Alexandre Chopin <alex@orion.sh>
2020-10-30 19:54:54 +0100

    fix docs link

--- i18n/en-US.js
+++ i18n/en-US.js
@@ -19,7 +19,7 @@ module.exports = {
       {
         name: 'Docs',
         icon: 'books',
-        slug: 'guides/get-started/installation'
+        slug: 'guides-book-slug'
       },
       {
         name: 'Resources',

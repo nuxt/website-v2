94679d85e4612e08beb200ce7202b0ac2e8e8428
Florent <flo@nuxtjs.com>
2020-11-03 18:06:21 +0100

    chore: add banner

---
+++ components/organisms/MasteringNuxtBanner.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-surface dark:bg-surface inline-shadow">
+    <div class="container mx-auto px-4 py-4 sm:px-6 lg:px-6">
+      <div
+        class="flex items-center justify-between font-medium text-white text-xs md:text-sm lg:text-lg text-light-onSurfacePrimary dark:text-dark-onSurfacePrimary"
+      >
+        <span> LEARN NUXT.JS - ADVANCED COURSE </span>
+        <div class="flex items-center">
+          <span class="font-bold hidden md:block">
+            REGISTER TODAY FOR EXCLUSIVE OFFER
+          </span>
+          <AppButton class="ml-4">
+SIGN UP &rarr;
+</AppButton>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- components/templates/TheHeader.vue
+++ components/templates/TheHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <header
-    class="fixed top-0 left-0 right-0 z-30 bg-light-elevatedSurface dark:bg-dark-elevatedSurface border-b border-light-border dark:border-dark-border lg:border-0 h-16 lg:h-24 transition-colors duration-300 ease-linear"
+    class="sticky top-0 left-0 right-0 z-30 bg-light-elevatedSurface dark:bg-dark-elevatedSurface border-b border-light-border dark:border-dark-border lg:border-0 h-16 lg:h-24 transition-colors duration-300 ease-linear"
   >
     <div
       class="container relative mx-auto px-4 flex items-center lg:py-6 h-full"

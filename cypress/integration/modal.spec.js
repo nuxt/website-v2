46c71b3d15b54ae825a6965513f58f3168e555c3
Debbie O'Brien <debs-obrien@users.noreply.github.com>
2020-11-04 11:52:13 +0100

    fix: add components to global folder for markdown components and fix broken tests due to banner (#944)

--- cypress/integration/modal.spec.js
+++
@@ -1,8 +0,0 @@
-describe('It checks to see if the modals works', () => {
-  it('successfully opens, checks for iframe and closes the codeSandbox modal', () => {
-    cy.visit('/docs/2.x/get-started/installation')
-    cy.get('[data-cy="modal-button"]').click()
-    cy.get('[data-cy="modal-open"]').should('be.visible')
-    cy.get('[data-cy="modal-close"]').click()
-  })
-})

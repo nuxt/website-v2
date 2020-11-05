fd0736352727da7b4f0ed5b25deaa85f452ada97
Alexandre Chopin <alex@nuxtjs.com>
2020-11-05 09:56:29 +0100

    update banner colors

--- components/organisms/MasteringNuxtBanner.vue
+++ components/organisms/MasteringNuxtBanner.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="bg-surface dark:bg-surface">
-    <div class="container mx-auto px-4 py-2 sm:px-6 lg:px-6 lg:py-4">
+  <div class="bg-nuxt-lightgreen">
+    <div class="container mx-auto px-4 py-2 sm:px-6 lg:px-6 lg:py-3">
       <div
-        class="flex items-center justify-between font-medium text-xs md:text-sm lg:text-base text-light-onSurfacePrimary dark:text-dark-onSurfacePrimary"
+        class="flex items-center justify-between font-medium text-xs md:text-sm lg:text-base text-white"
       >
-        <p><span class="font-bold"> LEARN NUXT.JS</span> - Register today for an Exclusive Offer.</p>
+        <p><span class="font-bold">MASTERING NUXT</span> - Register today for an Exclusive Offer.</p>
         <div class="flex items-center">
           <a
             href="https://masteringnuxt.com/?utm_source=nuxt&utm_medium=link&utm_campaign=pre_launch"
             target="_blank"
             rel="noopener"
-            class="bg-orange-500 hover:bg-nuxt-lightgreen text-white no-underline inline-block font-medium text-xs px-3 py-1 shadow uppercase rounded hover:shadow-md"
+            class="bg-orange-500 hover:bg-orange-600 text-white no-underline inline-block font-medium text-xs px-3 py-1 lg:py-2 shadow uppercase rounded hover:shadow-md"
           >
             SIGN UP &rarr;
           </a>

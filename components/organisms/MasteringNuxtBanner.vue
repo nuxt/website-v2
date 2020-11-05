21bc490f4f87ccb3ffd11036254152acf6c731fb
Alexandre Chopin <alex@nuxtjs.com>
2020-11-05 10:15:31 +0100

    Banner clickable

--- components/organisms/MasteringNuxtBanner.vue
+++ components/organisms/MasteringNuxtBanner.vue
@@ -1,21 +1,18 @@
 <template>
-  <div class="bg-nuxt-lightgreen">
+  <a href="https://masteringnuxt.com/?utm_source=nuxt&utm_medium=link&utm_campaign=pre_launch" target="_blank" rel="noopener" class="block bg-nuxt-lightgreen">
     <div class="container mx-auto px-4 py-2 sm:px-6 lg:px-6 lg:py-3">
       <div
         class="flex items-center justify-between font-medium text-xs md:text-sm lg:text-base text-white"
       >
         <p><span class="font-bold">MASTERING NUXT</span> - Register today for an Exclusive Offer.</p>
         <div class="flex items-center">
-          <a
-            href="https://masteringnuxt.com/?utm_source=nuxt&utm_medium=link&utm_campaign=pre_launch"
-            target="_blank"
-            rel="noopener"
+          <button
             class="bg-orange-500 hover:bg-orange-600 text-white no-underline inline-block font-medium text-xs px-3 py-1 lg:py-2 shadow uppercase rounded hover:shadow-md"
           >
             SIGN UP &rarr;
-          </a>
+          </button>
         </div>
       </div>
     </div>
-  </div>
+  </a>
 </template>

fc88095e66834dbcbbc1aa5f9049dd71000654be
Rolf Haug <rolf@rah.no>
2020-11-10 12:50:14 +0100

    chore: change text (#967)

--- components/organisms/MasteringNuxtBanner.vue
+++ components/organisms/MasteringNuxtBanner.vue
@@ -1,15 +1,23 @@
 <template>
-  <a href="https://masteringnuxt.com/?utm_source=nuxt&utm_medium=link&utm_campaign=pre_launch" target="_blank" rel="noopener" class="block bg-nuxt-lightgreen">
+  <a
+    href="https://masteringnuxt.com/?utm_source=nuxt&utm_medium=link&utm_campaign=pre_launch"
+    target="_blank"
+    rel="noopener"
+    class="block bg-nuxt-lightgreen"
+  >
     <div class="container mx-auto px-4 py-2 sm:px-6 lg:px-6 lg:py-3">
       <div
         class="flex items-center justify-between font-medium text-xs md:text-sm lg:text-base text-white"
       >
-        <p><span class="font-bold">MASTERING NUXT</span> - Register today for an Exclusive Offer.</p>
+        <p>
+          <span class="font-bold">Start Learning Nuxt today!</span> Get 30% off
+          your Mastering Nuxt license - Offer ends soon!
+        </p>
         <div class="flex items-center">
           <button
             class="bg-orange-500 hover:bg-orange-600 text-white no-underline inline-block font-medium text-xs px-3 py-1 lg:py-2 shadow uppercase rounded hover:shadow-md"
           >
-            SIGN UP &rarr;
+            Get 30% off &rarr;
           </button>
         </div>
       </div>

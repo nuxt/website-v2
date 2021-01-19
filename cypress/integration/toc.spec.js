15907613bfb56eec613f7ca720ef150bffda9c72
Daniel Roe <daniel@roe.dev>
2021-01-19 15:25:29 +0000

    ci: fix cypress tests (add trailing slash) (#1172)

--- cypress/integration/toc.spec.js
+++ cypress/integration/toc.spec.js
@@ -1,6 +1,6 @@
 describe('It checks to see if the toc at top of guides works ', () => {
   beforeEach(() => {
-    cy.visit('/docs/2.x/get-started/installation')
+    cy.visit('/docs/2.x/get-started/installation/')
   })
   it('checks the table of contents anchor', () => {
     cy.get('[data-cy="toc"]').first().click()

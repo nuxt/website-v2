9f80ba32c8b128249287aa74e184751d861e5bdb
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2021-01-21 19:52:30 +0100

    fix: styling

--- components/templates/TheAsideNavNewDocs.vue
+++ components/templates/TheAsideNavNewDocs.vue
@@ -38,7 +38,7 @@
               class="text-light-onSurfacePrimary dark:text-dark-onSurfacePrimary"
             >
               <NuxtLink
-                class="p-2 pl-4 flex rounded hover:text-nuxt-lightgreen dark:hover:text-nuxt-lightgreen transition-colors duration-300 ease-linear"
+                class="p-2 pl-4 flex rounded hover:text-nuxt-lightgreen dark:hover:text-nuxt-lightgreen transition-colors duration-300 ease-linear justify-between"
                 exact-active-class="text-nuxt-lightgreen bg-green-100 dark:bg-green-800 dark:text-white"
                 :to="toLink(group, link)"
               >

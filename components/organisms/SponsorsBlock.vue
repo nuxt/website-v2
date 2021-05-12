25303b117347a20336d89696a75bbad262d8720f
Sébastien Chopin <seb@nuxtjs.com>
2021-05-12 12:36:57 +0200

    chore: improvements
    - Update Tailwind
    - Add Tailwind breakpoint in dev
    - Improves partners dimensions and style

--- components/organisms/SponsorsBlock.vue
+++ components/organisms/SponsorsBlock.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
     <p
-      class="text-center lg:text-sm text-light-onSurfacePrimary dark:text-dark-onSurfacePrimary"
+      class="
+        pb-3
+        text-sm
+        font-bold
+        tracking-wide
+        text-gray-500
+        uppercase
+        border-b border-dashed
+        lg:mb-2
+        dark:text-gray-600
+        lg:text-xs
+      "
     >
       Partners
     </p>
@@ -9,18 +20,27 @@
       v-for="sponsor in sponsors"
       :key="sponsor.name"
       :href="sponsor.url"
-      class="block w-48 my-4 lg:inline-block lg:w-full"
+      class="
+        block
+        py-2
+        border-b border-dashed
+        opacity-75
+        hover:opacity-100
+        lg:inline-block
+        lg:w-full
+      "
       rel="noopener sponsored"
       target="_blank"
     >
       <img
         :src="`/img/sponsors/${$colorMode.value}/${sponsor.img}`"
         :alt="sponsor.name"
+        class="w-32"
       />
     </a>
     <AppButton
       :to="localePath({ name: 'sponsor-nuxtjs' })"
-      class="justify-center mb-8"
+      class="justify-center mt-2 mb-8"
     >
       Support Us
     </AppButton>
@@ -34,7 +54,7 @@ export default {
       sponsors: [
         {
           name: 'Strapi',
-          link: 'https://strapi.io/',
+          url: 'https://strapi.io/?ref=nuxt',
           img: 'strapi-logo.svg',
           class: 'h-12'
         },
@@ -46,7 +66,7 @@ export default {
         {
           name: 'Google Chrome',
           img: 'google-chrome.svg',
-          url: 'https://www.google.com/chrome/',
+          url: 'https://www.google.com/chrome/?ref=nuxt',
           class: 'h-12'
         }
       ]

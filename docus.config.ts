ec4c40138b7cb47ed70eec57405ed910b47edc09
Sergey Bedritsky <sergey.bedritsky@gmail.com>
2021-06-24 13:25:27 +0300

    fix: add more tokens (#10)

--- docus.config.ts
+++ docus.config.ts
@@ -35,21 +35,31 @@ export default {
       'd-primary-text-hover': 'text-gray-600 dark:text-cloud-lighter',
       'd-prose-code-inline-bg': 'bg-gray-100 dark:bg-secondary-darkest',
       'd-prose-code-bg': 'bg-gray-100 dark:bg-secondary-darkest',
-      'd-bg-header': 'bg-white bg-opacity-80 dark:bg-secondary-black dark:bg-opacity-80',
-      'd-border': 'light:border-gray-200 light:border-opacity-50 dark:border-secondary-darker',
+      'd-bg-header':
+        'bg-white bg-opacity-80 dark:bg-secondary-black dark:bg-opacity-80',
+      'd-border':
+        'light:border-gray-200 light:border-opacity-50 dark:border-secondary-darker',
       'd-border-header': 'border-none',
       'd-page-mobile-toc-bg': 'd-body-bg bg-opacity-80 dark:bg-opacity-80',
       'd-aside-header-bg': 'bg-gray-50 dark:bg-secondary-darkest',
-      'd-active-aside-navigation-item-bg': 'bg-primary-50 dark:bg-secondary-darkest',
-      'd-active-aside-navigation-item-text': 'text-primary-500 dark:text-primary-400 ',
+      'd-active-aside-navigation-item-bg':
+        'bg-primary-50 dark:bg-secondary-darkest',
+      'd-active-aside-navigation-item-text':
+        'text-primary-500 dark:text-primary-400 ',
       'd-code-group-header-bg': 'bg-gray-200 dark:bg-secondary-darker',
       'd-code-group-tab': 'bg-gray-300 dark:bg-secondary-dark',
       'd-prose-code-filename-bg': 'bg-gray-100 dark:bg-secondary-darker',
       'd-prose-code-filename-text': 'text-gray-600 dark:text-secondary-light',
-      'd-prose-thead-border': 'border-b border-gray-200 dark:border-secondary-dark',
-      'd-prose-tr-border': 'border-b border-gray-100 dark:border-secondary-darker',
-      'd-prose-blockquote-border': 'border-l-2 border-gray-200 dark:border-secondary-darker',
-      'd-tertiary-text': 'text-gray-400 dark:text-cloud-dark'
+      'd-prose-thead-border':
+        'border-b border-gray-200 dark:border-secondary-dark',
+      'd-prose-tr-border':
+        'border-b border-gray-100 dark:border-secondary-darker',
+      'd-prose-blockquote-border':
+        'border-l-2 border-gray-200 dark:border-secondary-darker',
+      'd-tertiary-text': 'text-gray-400 dark:text-cloud-dark',
+      'd-prose-hr-border':
+        'border-t border-gray-100 dark:border-secondary-darker',
+      'd-prose-ul-li-bullet': 'd-secondary-bg'
     }
   }
 }

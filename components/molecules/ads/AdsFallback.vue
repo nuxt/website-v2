a26813c1c4c22e0b9cf536a0c6b599faaeeba23c
DespauxGuillaume <64960186+DespauxGuillaume@users.noreply.github.com>
2021-06-28 17:07:05 +0200

    remove: ads img (#30)

--- components/molecules/ads/AdsFallback.vue
+++ components/molecules/ads/AdsFallback.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="nui-support-nuxt">
-    <div class="pr-4 sm:pr-0">
-      <BlockedIcon alt="Support NuxtJS" width="125" height="125" class="block mr-4" />
-    </div>
     <div>
       <p class="m-0 font-bold sm:text-sm d-text-primary">NuxtJS needs you!</p>
       <p class="p-0 m-0 leading-normal text-gray-600 dark:text-white sm:text-xs">
@@ -11,16 +8,6 @@
     </div>
   </div>
 </template>
-
-<script>
-import BlockedIcon from '~/assets/img/blocked.svg?inline'
-
-export default {
-  components: {
-    BlockedIcon
-  }
-}
-</script>
 
 <style lang="postcss">
 .nui-support-nuxt {

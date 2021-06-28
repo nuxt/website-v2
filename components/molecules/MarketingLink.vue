6523737cebf1f6c7aed5f039092e0f26a716d7f7
Florent Delerue <flo@nuxtjs.com>
2021-06-28 11:34:18 +0200

    feat: use marking links on resources page (#25)
    * :sparkles: (MarketingLink) add size variant
    
    * :sparkles: (ResourcesList) update links to MarketingLink

--- components/molecules/MarketingLink.vue
+++ components/molecules/MarketingLink.vue
@@ -1,14 +1,19 @@
 <template>
   <Link
     :key="name"
-    class="relative inline-flex items-center text-body-base lg:text-base-xl font-bold group flex-nowrap max-w-max"
+    class="relative inline-flex items-center font-bold group flex-nowrap max-w-max"
     :class="'text-' + color"
     :to="`${to}`"
     :blank="blank"
   >
-    {{ name }}
+    <span :class="[
+      { 'text-md 2xl:text-base ': size === 'sm' },
+      { 'text-lg': size === 'md' },
+      { 'text-xl': size === 'lg' }
+    ]">{{ name }}</span>
 
-    <Component :is="icon" class="ml-2 h-5 w-5" />
+    <Component :is="icon" class="ml-2"
+    :class="size === 'sm' ? 'w-4 h-4' : 'w-5 h-5'" />
 
     <span class="absolute -bottom-2 h-0.5 w-8 group-hover:w-full transition-all" :class="'bg-' + color" />
   </Link>
@@ -38,6 +43,10 @@ export default defineComponent({
       type: Boolean,
       default: true,
       required: false
+    },
+    size: {
+      type: String,
+      default: 'md'
     }
   }
 })

894805c4a035865ea631b5bcfe92c73069f100b6
Florent Delerue <flo@nuxtjs.com>
2021-06-29 16:09:42 +0200

    :bug: (SustainabilitySection) add missing button (#33)

--- components/organisms/resources/SustainabilitySection.vue
+++ components/organisms/resources/SustainabilitySection.vue
@@ -1,30 +1,37 @@
 <template>
   <div class="py-4 d-container-content">
-    <div class="flex flex-col light:text-secondary-dark dark:text-white pt-8 pb-24 space-y-4">
+    <div class="flex flex-col items-center light:text-sky-darker dark:text-white pt-8 space-y-4">
       <h2 class="font-semibold text-display-6 text-center">
         <Markdown use="titleSection" unwrap="p" />
       </h2>
 
-      <div class="text-lg w-full flex justify-center">
+      <div class="text-lg w-full flex justify-center pb-16">
         <p class="text-center md:w-2/3">
           <Markdown use="descriptionSection" unwrap="p" />
         </p>
       </div>
-    </div>
 
-    <div v-for="(sponsors, index) in sustainability" :key="index" class="flex flex-col items-center">
-      <h2 class="text-display-6 font-semibold">{{ sponsors.tier }}</h2>
-      <div class="flex flex-wrap justify-center -mx-8 pt-8 pb-24">
-        <NuxtHref
-          v-for="logo in sponsors.logos"
-          :key="logo.title"
-          :href="logo.url"
-          :aria-label="logo.title"
-          class="mx-8 my-4"
-        >
-          <img :src="`/img/sponsors/${$colorMode.value}/${logo.img}.png`" :alt="logo.title" :class="logo.size" />
-        </NuxtHref>
+      <div v-for="(sponsors, index) in sustainability" :key="index" class="text-center">
+        <h2 class="text-display-6 font-semibold">{{ sponsors.tier }}</h2>
+        <div class="flex flex-wrap justify-center -mx-8 pt-8 pb-16">
+          <NuxtHref
+            v-for="logo in sponsors.logos"
+            :key="logo.title"
+            :href="logo.url"
+            :aria-label="logo.title"
+            class="mx-8 my-4"
+          >
+            <img :src="`/img/sponsors/${$colorMode.value}/${logo.img}.png`" :alt="logo.title" :class="logo.size" />
+          </NuxtHref>
+        </div>
       </div>
+      <SectionButton
+        to="https://github.com/sponsors/nuxt"
+        :aria-label="buttonText"
+        size="md"
+        class="bg-primary text-gray-800 hover:bg-green-300 focus:bg-green-300">
+          {{ buttonText }}
+      </SectionButton>
     </div>
   </div>
 </template>
@@ -33,6 +40,13 @@
 import { defineComponent, useContext, ref, useFetch } from '@nuxtjs/composition-api'
 
 export default defineComponent({
+  props: {
+    buttonText: {
+      type: String,
+      default: '',
+      required: true
+    }
+  },
   setup() {
     const { $docus } = useContext()
     const sustainability = ref()
--- content/en/sponsors-nuxtjs.md
+++ content/en/sponsors-nuxtjs.md
@@ -14,6 +14,7 @@ You can support NuxtJS development via the following methods:
 ::
 
 ::sustainability-section
+buttonText: Become a sponsor
 ---titleSection
 Recurring pledges
 ---descriptionSection

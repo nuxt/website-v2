b84dead8375fd3bf2e4eea9007dc5d6f830a9273
Clément Ollivier <clement.o2p@gmail.com>
2021-07-02 12:17:28 +0200

    content: add movavi sponsor

--- content/sponsors.js
+++ content/sponsors.js
@@ -105,6 +105,12 @@ export default {
       img: 'remotely-logo.svg',
       url: 'https://remotely.so/',
       class: 'h-8'
+    },
+    {
+      name: 'Movavi',
+      img: 'movavi.svg',
+      url: 'https://www.movavi.com/',
+      class: 'h-8'
     }
   ]
 }

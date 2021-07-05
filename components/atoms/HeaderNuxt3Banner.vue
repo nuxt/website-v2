979267ef58e1ec61a3fba5002fa58846f710d6c1
Florent Delerue <flo@nuxtjs.com>
2021-07-05 16:50:16 +0200

    feat: nuxt 3 header banner (#43)

---
+++ components/atoms/HeaderNuxt3Banner.vue
@@ -0,0 +1,6 @@
+<template>
+    <Link to='#'>
+        <img :src="`/img/banner/banner-nuxt3.png`" class="w-full h-8 object-right object-cover"/>
+        <span class="absolute h-10 text-white top-1.5 w-full pl-4 sm:text-center font-semibold text-sm">Is Nuxt 3 ready ?</span>
+    </Link>
+</template>
--- components/organisms/app/AppHeader.vue
+++ components/organisms/app/AppHeader.vue
@@ -1,26 +1,29 @@
 <template>
-  <header class="d-header">
-    <div class="flex flex-none h-full d-container-content">
-      <NavigationButton v-if="aside" />
+  <header class="sticky w-full top-0 z-50 ">
+    <HeaderNuxt3Banner />
+    <div class="d-header -mt-0.5 lg:-mt-0">
+      <div class="flex flex-none d-container-content pt-1">
+        <NavigationButton v-if="aside" />
 
-      <div
-        :class="[aside ? 'justify-center' : 'justify-start']"
-        class="flex items-center flex-1 lg:flex-none"
-      >
-        <HeaderLogo />
-      </div>
+        <div
+          :class="[aside ? 'justify-center' : 'justify-start']"
+          class="flex items-center flex-1 lg:flex-none"
+        >
+          <Logo :settings="settings" />
+        </div>
 
-      <div class="items-center hidden lg:flex lg:flex-1">
-        <HeaderNavigation />
-      </div>
+        <div class="items-center hidden lg:flex lg:flex-1">
+          <HeaderNavigation />
+        </div>
 
-      <div class="flex items-center justify-end lg:flex-none lg:w-60">
-        <AlgoliaSearchBox
-          v-if="settings.algolia"
-          :options="settings.algolia"
-          :settings="settings"
-          class="w-14 lg:px-2 ml-auto"
-        />
+        <div class="flex items-center justify-end lg:flex-none lg:w-60">
+          <AlgoliaSearchBox
+            v-if="settings.algolia"
+            :options="settings.algolia"
+            :settings="settings"
+            class="w-14 lg:px-2 ml-auto"
+          />
+        </div>
       </div>
     </div>
   </header>

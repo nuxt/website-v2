93bf8da5d91a922491458ffd01183604f781ad60
Florent Delerue <flo@nuxtjs.com>
2021-07-06 11:14:01 +0200

    fix: releases page container (#61)
    * :sparkles: (AppPage) hide x overflow
    
    * :bug: (releases) fix aside nav

---
+++ components/organisms/app/AppPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col w-full pt-0 xl:flex-row xl:pt-10 overflow-x-hidden">
+    <article class="flex-auto order-last min-w-0 mt-0 xl:order-first xl:mt-0">
+      <slot />
+      <div v-if="$scopedSlots['prev-next']" class="px-4 sm:px-6">
+        <hr class="mt-10 mb-8 border-t d-border" />
+        <slot name="prev-next" />
+      </div>
+    </article>
+
+    <slot name="toc" />
+  </div>
+</template>

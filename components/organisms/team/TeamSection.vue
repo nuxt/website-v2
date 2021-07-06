b95f70f8aeaa53580061bdccd57c351bee9f6d7f
Florent Delerue <flo@nuxtjs.com>
2021-07-06 18:03:42 +0200

    fix: refacto team section for title styling (works for all language)  (#66)
    * :bug: (PartnersBanner) add missing width
    
    * :bug: (HomeDiscoverBanner) remove duplicate partners
    
    * :construction: (Teams) wip
    
    * :bug: (docus) change approach for content
    
    * :sparkles: (Team) refacto team section
    
    Co-authored-by: Yaël GUILLOUX <yael.guilloux@gmail.com>

--- components/organisms/team/TeamSection.vue
+++ components/organisms/team/TeamSection.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="py-16 d-container-content">
-    <div v-for="team in teams" :key="team.slug">
-      <TeamTitle :slug="team.slug" :name="team.name" class="text-center pb-4" />
-      <div class="text-center text-lg pb-16"><DocusContent :document="team" /></div>
-      <div class="flex justify-center">
-        <div class="flex flex-wrap justify-center pb-16">
-          <div v-for="member in team.members" :key="member.name">
-            <TeamMember :member="member" />
-          </div>
+    <h2 class="text-center pb-4 text-display-4 font-serif">
+      <Markdown use="title" unwrap="p" />
+    </h2>
+    <div class="text-center text-lg pb-16 d-heading-description px-18">
+      <Markdown use="description" unwrap="p" />
+    </div>
+    <div class="flex justify-center">
+      <div class="flex flex-wrap justify-center">
+        <div v-for="member in team.members" :key="member.name">
+          <TeamMember :member="member" />
         </div>
       </div>
     </div>
@@ -15,22 +17,36 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, useContext, useFetch } from '@nuxtjs/composition-api'
+import {
+  defineComponent,
+  ref,
+  useContext,
+  useFetch
+} from '@nuxtjs/composition-api'
 
 export default defineComponent({
-  setup() {
+  props: {
+    teamName: {
+      type: String,
+      required: true
+    }
+  },
+  setup(props) {
     const { $docus, i18n } = useContext()
     const teams = ref()
+    const team = ref([])
+
     useFetch(async () => {
       teams.value = await $docus
-        .search('/teams', { deep: true })
-        .where({ 'language': i18n.locale })
+        .search(`/teams/${props.teamName}`, { deep: true })
+        .where({ language: i18n.locale })
         .sortBy('position', 'asc')
         .fetch()
-      teams.value = teams.value.filter(team => team.name)
+
+      team.value = teams.value[0]
     })
     return {
-      teams
+      team
     }
   }
 })

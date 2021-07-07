6451653b0580b9a977c78a1a2cf915846b54999f
Sergey Bedritsky <sergey.bedritsky@gmail.com>
2021-07-07 17:25:30 +0300

    fix: center AppHeader items (#72)
    * fix: center AppHeader items
    
    * fix: desktop centering

--- components/organisms/app/AppHeader.vue
+++ components/organisms/app/AppHeader.vue
@@ -1,15 +1,16 @@
 <template>
-  <header class="sticky w-full top-0 z-50 ">
-    <div class="d-header -mt-0.5 lg:-mt-0">
-      <div class="flex flex-none d-container-content pt-1 md:pt-2.5">
-        <NavigationButton v-if="aside" />
+  <header class="sticky w-full top-0 z-50">
+    <div class="d-header">
+      <div class="flex h-full flex-none d-container-content -mx-3 lg:mx-auto">
+        <NavigationButton v-if="aside" class="px-2.5" />
 
         <Link
-          :class="[aside ? 'justify-center' : 'justify-start']"
-          class="flex items-center flex-1 lg:flex-none lg:mr-28"
+          :class="[aside ? 'justify-center lg:justify-start' : 'justify-start']"
+          class="flex items-center flex-1 lg:flex-none lg:w-60"
           :to="localePath('/')"
         >
-          <Logo :settings="settings" class="h-5 sm:h-6 md:h-7" />
+          <!-- "mr-4 lg:mr-0" to optically center logo text -->
+          <Logo :settings="settings" class="h-5 sm:h-6 md:h-7 mr-4 lg:mr-0" />
         </Link>
 
         <div class="items-center hidden lg:flex lg:flex-1">
@@ -21,7 +22,7 @@
             v-if="settings.algolia"
             :options="settings.algolia"
             :settings="settings"
-            class="w-14 lg:px-2 ml-auto"
+            class="lg:px-2 ml-auto"
           />
         </div>
       </div>

2be36611a15b4d6aa66b851b932745a694cdcd50
DespauxGuillaume <64960186+DespauxGuillaume@users.noreply.github.com>
2021-07-07 17:12:29 +0200

    fix: Padding banner on mobile (#75)

--- components/organisms/app/AppBanner.vue
+++ components/organisms/app/AppBanner.vue
@@ -1,6 +1,6 @@
 <template>
     <Link to='#'>
-        <img :src="`/img/banner/banner-nuxt3.png`" class="w-full h-8 object-right object-cover"/>
-        <span class="absolute h-10 text-white top-1.5 w-full pl-4 sm:text-center font-semibold text-sm">Is Nuxt 3 ready ?</span>
+        <img :src="`/img/banner/banner-nuxt3.png`" class="object-cover object-right w-full h-12 sm:h-8"/>
+        <span class="absolute h-10 text-white top-3.5 sm:top-1.5 w-full pl-4 sm:text-center font-semibold text-sm">Is Nuxt 3 ready ?</span>
     </Link>
 </template>

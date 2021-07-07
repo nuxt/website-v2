633c4383528d3592d6132a870a4e5289fe30f210
Sébastien Chopin <seb@nuxtjs.com>
2021-07-08 00:06:47 +0200

    docs: start nuxt 3 page

--- components/organisms/app/AppBanner.vue
+++ components/organisms/app/AppBanner.vue
@@ -1,6 +1,6 @@
 <template>
-    <Link to='#'>
+    <Link to="/3">
         <img :src="`/img/banner/banner-nuxt3.png`" class="object-cover object-right w-full h-12 sm:h-8"/>
-        <span class="absolute h-10 text-white top-3.5 sm:top-1.5 w-full pl-4 sm:text-center font-semibold text-sm">Is Nuxt 3 ready ?</span>
+        <span class="absolute h-10 text-white top-3.5 sm:top-1.5 w-full pl-4 sm:text-center font-semibold text-sm">Is Nuxt 3 ready?</span>
     </Link>
 </template>

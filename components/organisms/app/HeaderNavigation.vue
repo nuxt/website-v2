cccf7aa3bcf304261be33f46259992145e497e20
DespauxGuillaume <64960186+DespauxGuillaume@users.noreply.github.com>
2021-07-15 17:16:17 +0200

    feat: new component for externals links (#1538)

--- components/organisms/app/HeaderNavigation.vue
+++ components/organisms/app/HeaderNavigation.vue
@@ -1,22 +1,10 @@
 <template>
-  <nav class="relative w-full h-full flex items-center justify-center">
+  <nav class="relative flex items-center justify-center w-full h-full">
     <Link
-      v-for="{ title, href, slug, blank } in links"
+      v-for="{ title, href, slug, blank, icon } in links"
       :key="slug"
       :aria-label="title"
-      class="
-        relative
-        capitalize
-        font-medium
-        flex flex-col
-        items-center
-        justify-center
-        text-center
-        px-4
-        h-full
-        flex
-        items-center
-      "
+      class="relative flex flex-col items-center justify-center h-full px-4 font-medium text-center capitalize group"
       :to="href"
       :blank="blank"
       :class="{
@@ -24,11 +12,13 @@
         'hover:d-primary-text-hover': currentSlug !== slug
       }"
     >
+    <div class="flex items-center">
       {{ $t(`header.${title}`) }}
+      <Component v-if="icon" :is="icon" class="w-4 h-4 ml-2 opacity-0 lg:group-hover:opacity-100" />
+    </div>
     </Link>
   </nav>
 </template>
-
 <script>
 import { computed, defineComponent, useRoute } from '@nuxtjs/composition-api'
 
@@ -62,32 +52,29 @@ export default defineComponent({
           title: 'Video Courses',
           slug: 'video-courses',
           href: 'https://masteringnuxt.com/?utm_source=nuxt&utm_medium=link&utm_campaign=navbar_link',
-          blank: true
+          blank: true,
+          icon: 'IconExternalLink'
         }
       ]
     }
   },
   setup() {
     const route = useRoute()
-
     const currentSlug = computed(() => {
       return route.value.path !== '/' && route?.value?.params?.pathMatch
         ? route.value.params.pathMatch.split('/')[0]
         : null
     })
-
     return {
       currentSlug
     }
   }
 })
 </script>
-
 <style scoped lang="postcss">
 .nuxt-link-active {
   color: rgba(52, 211, 153);
 }
-
 .menu li {
   display: block;
   position: relative;

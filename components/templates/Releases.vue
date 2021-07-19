6832891f098b26b4b2bbd3d9b0715e43f6b5922d
Florent Delerue <flo@nuxtjs.com>
2021-07-19 09:40:45 +0200

    :bug: (Releases) fix horizontal overflow on mobile (#1551)

--- components/templates/Releases.vue
+++ components/templates/Releases.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="overflow-x-hidden sm:overflow-x-visible">
     <section class="py-20 border-b mx-0 lg:mx-6 border-sky-darker">
       <h1 class="flex-1 text-4xl pl-6 lg:pl-0 font-semibold tracking-tight text-gray-900 dark:text-gray-100">
         {{ page.title }}
@@ -10,14 +10,14 @@
     </section>
 
     <div class="divide-tw-gray-200">
-      <div v-for="release of releases" :key="release.name" class="px-6 border-b dark:border-sky-darker lg:border-transparent">
+      <div v-for="release of releases" :key="release.name" class="px-6 border-b dark:border-sky-darker dark:lg:border-transparent lg:border-transparent">
         <div class="relative lg:flex py-12 lg:py-16 lg:border-b lg:dark:border-sky-darker">
           <div class="self-start w-full lg:w-1/4 lg:sticky lg:top-0 lg:pt-38 lg:-mt-32 lg:mb-6">
             <h2 class="mb-3 text-3xl font-light text-tw-gray-900">Version <span class="font-bold">{{ release.name }}</span></h2>
             <p class="text-xs font-semibold uppercase text-tw-gray-400">Released on {{ new Date(release.date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' }) }}</p>
           </div>
 
-          <DocusContent :document="release" class="px-8 lg:px-0 pt-8 lg:pt-0" />
+          <DocusContent :document="release" class="lg:w-3/4 px-8 lg:px-0 pt-8 lg:pt-0" />
         </div>
       </div>
     </div>

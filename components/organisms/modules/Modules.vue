ec2fa3d779226c20ee9df8341849a1c34e81ca29
Florent <flo@nuxtjs.com>
2021-07-23 16:57:03 +0200

    :construction: (Modules) get modules from api

--- components/organisms/modules/Modules.vue
+++ components/organisms/modules/Modules.vue
@@ -1,21 +1,43 @@
 <template>
-  <div>
-    {{ modulesList }}
+
+  <div v-if="!pending">
+    {{ modules }}
+  </div>
+  <div v-else>
+    Loading...
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api'
+import { defineComponent, ref, useContext, useFetch } from '@nuxtjs/composition-api'
 import { useModules } from '../../../plugins/modulesList'
 
 export default defineComponent({
   setup() {
-    const { modules } = useModules()
+    const { i18n } = useContext()
+    const { getModules } = useModules()
+    const modules = ref(null)
+    const error = ref(null)
+    const pending = ref(false)
 
-    console.log('modulesList', modules)
+    useFetch(async () => {
+
+      pending.value = true
+
+      await getModules().then((res) => {
+          pending.value = false
+          modules.value = res
+        })
+        .catch(() => {
+          pending.value = false
+          error.value = i18n.t('modules.error')
+        })
+    })
 
     return {
-      modules
+      modules,
+      error,
+      pending
     }
   },
 })

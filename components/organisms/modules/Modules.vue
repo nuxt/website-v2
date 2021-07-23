9c8dbb5944af8cf88f9a49a27d98de967d395911
Florent <flo@nuxtjs.com>
2021-07-23 11:35:00 +0200

    :construction: (Modules) modules page

---
+++ components/organisms/modules/Modules.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    {{ modulesList }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+import { useModules } from '../../../plugins/modulesList'
+
+export default defineComponent({
+  setup() {
+    const { modules } = useModules()
+
+    console.log('modulesList', modules)
+
+    return {
+      modules
+    }
+  },
+})
+</script>
---
+++ content/en/modules.md
@@ -0,0 +1,16 @@
+---
+template: page
+layout:
+  fluid: true
+navigation: false
+---
+
+::page-hero
+title: Modules
+description: "Discover a panel of resources made by our partners. By using thoses affiliate resources links, you are helping us to maintain and develop the Open Source Framework.
+"
+---
+::
+
+::modules
+::

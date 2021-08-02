d63c384ec5c51127a7f7a7a28f546b410e4b0aa3
Florent <flo@nuxtjs.com>
2021-08-02 18:08:22 +0200

    :sparkles: (AsideModules) update

--- components/organisms/modules/AsideModules.vue
+++ components/organisms/modules/AsideModules.vue
@@ -1,24 +1,24 @@
 <template>
   <div
     class="
-      fixed
+      lg:sticky
       top-0
-      left-0
+      lg:left-0
       h-full
-      overflow-auto
       pointer-events-auto
       min-h-fill-available
-      lg:h-screen lg:sticky lg:top-header
+      lg:h-screen lg:top-header
       lg:w-1/5
     "
   >
-    <div class="w-auto h-full overflow-auto d-bg-header dark:lg:bg-transparent lg:bg-transparent">
+    <div class="w-auto h-full d-bg-header dark:lg:bg-transparent lg:bg-transparent">
       <nav
         class="
-          flex flex-col
+          flex lg:flex-col
           justify-between
           lg:justify-start
-          max-w-sm
+          lg:max-w-sm
+          w-full
           overflow-y-auto
           text-sm
           font-medium
@@ -27,12 +27,12 @@
           d-scrollbar
         "
       >
-        <div class="py-4 pl-4 pr-24 sm:pl-6 lg:pr-0 lg:pt-10">
-          <ul>
+        <div class="py-4 pr-0 lg:pt-10">
+          <ul class="flex flex-wrap lg:flex-col">
             <li v-for="category in categories" :key="category.name">
-              <button @click="selectCategory(category.name)" class="py-2 px-4 flex justify-between w-full" :class="{ 'rounded-md bg-gray-100 dark:bg-white dark:bg-opacity-10': category.name === selectedCategory }">
+              <button @click="selectCategory(category.name)" class="py-2 px-4 flex justify-between w-full focus:outline-none focus:ring-transparent" :class="{ 'rounded-md bg-gray-100 dark:bg-white dark:bg-opacity-10': category.name === selected }">
                 <div class="truncate max-w-48 font">{{ category.name }}</div>
-                <span v-if="category.name === selectedCategory" class="rounded-xl bg-white dark:bg-sky-darkest ml-2 px-2 py-0.5 text-xs">{{ category.count }}</span>
+                <span v-if="category.name === selected" class="rounded-xl bg-white dark:bg-sky-darkest ml-2 px-2 py-0.5 text-xs">{{ category.count }}</span>
               </button>
             </li>
           </ul>
@@ -42,32 +42,50 @@
   </div>
 </template>
 <script lang="ts">
-import { defineComponent, ref, } from '@nuxtjs/composition-api'
+import { defineComponent, ref, onMounted, useFetch } from '@nuxtjs/composition-api'
+import { useModules } from '../../../plugins/modulesList'
 
 export default defineComponent({
   props: {
-    categories: {
+    modules: {
       type: Array,
       required: true
-    },
+    }
   },
   setup(props) {
     // @ts-ignore
-    let selectedCategory = ref('')
+    let selected = ref('')
+    let categories = ref([])
+    const { getModulesCategories } = useModules()
 
-    function selectCategory(category: any) {
-      if (category === selectedCategory.value) {
-        category = null
-        selectedCategory.value = ''
+    //fetch modules
+    useFetch(async () => {
+      await getModulesCategories(props.modules).then((categs) => {
+        categories.value = categs
+      })
+    })
+
+    onMounted(() => {
+      const selectedCateg = (window.location.hash || '').substr(1)
+      if (selectedCateg) {
+        selectCategory(selectedCateg)
+      }
+    })
+
+    function selectCategory (category: string) {
+
+      if (selected.value === category) {
+        selected.value = null
       } else {
-        selectedCategory.value = category
+        selected.value = category
       }
 
-      this.$emit('category', category)
+      this.$emit('category', selected.value)
     }
 
     return {
-      selectedCategory,
+      categories,
+      selected,
       selectCategory
     }
   },

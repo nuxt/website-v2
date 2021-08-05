9abcd10a614509f837d9a443053d031c6c994e05
Florent <flo@nuxtjs.com>
2021-08-05 14:39:39 +0200

    :sparkles: (SustainabilityCard) update

--- components/molecules/SustainabilityCard.vue
+++ components/molecules/SustainabilityCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="p-4 w-full dark:bg-sky-darker rounded-md shadow-lg flex space-x-4 items-center dark:text-white">
     <!-- img :src="`/img/sponsors/${$colorMode.value}/${sponsor.img}`" :alt="sponsor.title" :class="sponsor.size" / -->
-    <img :src="`/img/sponsors/sponsors-square/${sponsor.img}`" :alt="sponsor.title" :class="sponsor.size" class="w-1/4 object-contain" />
+    <img :src="`/img/sponsors/sponsors-square/${sponsor.img_square}`" :alt="sponsor.title" :class="sponsor.size" class="w-1/4 object-contain" />
     <div class="flex w-3/4" :class="{ 'justify-between items-center': sponsor.link }">
       <div class="flex flex-col items-start">
         <span class="font-medium">

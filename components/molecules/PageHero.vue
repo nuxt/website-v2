a67807f279520ddd87d00efa9345dc7aa6fae4e1
Florent <flo@nuxtjs.com>
2021-08-05 16:25:45 +0200

    :bug: (PageHero) overflow ellipsis on title

--- components/molecules/PageHero.vue
+++ components/molecules/PageHero.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="relative pt-24 pb-32 bg-gray-100 dark:bg-secondary-darkest">
     <div class="relative d-container-content">
-      <h1 class="text-display-3 font-serif mb-4">{{ title }}</h1>
+      <h1 class="text-display-3 font-serif mb-4 overflow-ellipsis overflow-hidden">{{ title }}</h1>
        <p :class="{ 'text-body-xl max-w-3xl': !descriptionFullWidth }">{{ description }}</p>
       <Markdown use="bottom" />
     </div>

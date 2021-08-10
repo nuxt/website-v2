b184451f55e6b1ff628f88f2873adf3f337d1878
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-08-10 19:36:15 +0200

    :sparkles: (plugins) ssr compatible modules plugins

--- plugins/fuse.js
+++ plugins/fuse.js
@@ -1,8 +1,8 @@
 import Fuse from 'fuse.js/dist/fuse.basic.esm'
-import { ref, computed, watch, useContext } from '@nuxtjs/composition-api'
+import { ssrRef, computed, watch, useContext } from '@nuxtjs/composition-api'
 
 // Sorting function
-const sort = (a, b, asc) => asc ? a - b : b - a
+const sort = (a, b, asc) => (asc ? a - b : b - a)
 // Default ordering
 const ORDERS = {
   DESC: 'desc',
@@ -11,23 +11,24 @@ const ORDERS = {
 // Incrementing interval
 const MODULE_INCREMENT_LOADING = 12
 // Sorting fields
-const sortFields = [{ text: 'Downloads' , value: 'downloads' }, { text: 'Stars', value: 'stars' }]
+const sortFields = [
+  { text: 'Downloads', value: 'downloads' },
+  { text: 'Stars', value: 'stars' }
+]
 // Fuse instance
-const fuse = ref()
+const fuse = ssrRef(undefined, 'fuseInstanceRef')
 // Query reference
-const query = ref()
+const query = ssrRef(undefined, 'fuseQueryRef')
 // Ordered by
-const orderedBy = ref(ORDERS.DESC)
+const orderedBy = ssrRef(ORDERS.DESC, 'fuseOrderRef')
 // Sorted by
-const sortedBy = ref(sortFields[0].value)
+const sortedBy = ssrRef(sortFields[0].value, 'fuseSortedByRef')
 // Sort by menu
-const sortByMenuVisible = ref(false)
+const sortByMenuVisible = ssrRef(false, 'fuseSortByMenuVisible')
 // Currently selected category
-const selectedCategory = ref('')
-// Page filtered modules
-const pageFilteredModulesList = ref()
+const selectedCategory = ssrRef('', 'fuseSortByCategoryRef')
 // Amount of modules loaded
-const modulesLoaded = ref(MODULE_INCREMENT_LOADING)
+const modulesLoaded = ssrRef(MODULE_INCREMENT_LOADING, 'fuseModulesLoadedRef')
 
 /**
  * Modules helpers
@@ -35,185 +36,153 @@ const modulesLoaded = ref(MODULE_INCREMENT_LOADING)
  *       as this composable could be used with other kind of datas afterwards.
  */
 export function useFuse(modules) {
-    // Context
-    const { route } = useContext()
+  // Context
+  const { route } = useContext()
 
-    // Filtered modules list
-    const filteredModules = computed(() => {
-      let filteredModulesList = modules.value
+  // Filtered modules list
+  const filteredModules = computed(() => {
+    let filteredModulesList = modules.value
 
-      // Filter modules w/ Fuse
-      if (filteredModulesList) {
-        if (query.value) {
-          filteredModulesList = fuse.value.search(query.value).map((r) => r.item)
-        } else {
-          filteredModulesList = filteredModulesList.sort(
-            (a, b) => sort(a[sortedBy.value], b[sortedBy.value], orderedBy.value === ORDERS.ASC)
-          )
-        }
-
-        if (selectedCategory.value) {
-          filteredModulesList = filteredModulesList.filter((module) => module.category === selectedCategory.value)
-        }
-      }
-
-      return filteredModulesList
-    })
-
-    // Sorting options
-    const sortByComp = computed(() => sortFields[sortedBy.value])
-
-    // Sorting options
-    const sortByOptions = computed(() => {
-      const options = {}
-
-      for (const field in sortFields) {
-        if (field === sortedBy.value) {continue}
-
-        options[field] = {
-          ...sortFields[field]
-        }
-      }
-
-      return options
-    })
-
-    // Watch local references and sync URL on changes
-    watch([selectedCategory, query, orderedBy, sortedBy], syncURL)
-
-    // Initialize Fuse
-    function init() {
-      const fuseOptions = {
-        threshold: 0.1,
-        keys: [
-          'name',
-          'npm',
-          'category',
-          'maintainers.name',
-          'maintainers.github',
-          'description',
-          'repo'
-        ]
-      }
-
-      const index = Fuse.createIndex(fuseOptions.keys, modules.value)
-
-      fuse.value = new Fuse(modules.value, fuseOptions, index)
-
-      selectedCategory.value = (window.location.hash || '').substr(1)
-
-      const { q, sortBy, orderBy } = route.value.query
-
-      if (q) {
-        query.value = q
-      }
-
-      if (sortBy) {
-        sortedBy.value = sortBy
-      }
-
-      if (orderBy) {
-        orderedBy.value = orderBy
-      }
-
-      setPageFilteredModules(MODULE_INCREMENT_LOADING)
-    }
-
-    // Sync page URL with current page state
-    function syncURL () {
-      const url = route.value.path
-      let q = ''
-      resetModulesLoaded()
-      setPageFilteredModules(MODULE_INCREMENT_LOADING)
-
+    // Filter modules w/ Fuse
+    if (filteredModulesList) {
       if (query.value) {
-        q += `?q=${query.value}`
-      }
-
-      if (orderedBy.value !== ORDERS.DESC) {
-        q += `${q ? '&' : '?'}orderBy=${orderedBy.value}`
-      }
-
-      if (sortedBy.value !== sortFields[0].value) {
-        q += `${q ? '&' : '?'}sortBy=${sortedBy.value}`
+        filteredModulesList = fuse.value.search(query.value).map(r => r.item)
+      } else {
+        filteredModulesList = filteredModulesList.sort((a, b) =>
+          sort(a[sortedBy.value], b[sortedBy.value], orderedBy.value === ORDERS.ASC)
+        )
       }
 
       if (selectedCategory.value) {
-        q += `#${selectedCategory.value}`
+        filteredModulesList = filteredModulesList.filter(module => module.category === selectedCategory.value)
       }
-
-      window.history.pushState('', '', `${url}${q}`)
     }
 
-    // Select current category
-    function getCategory(category) {
-      selectedCategory.value = category
+    return (filteredModulesList || []).splice(0, modulesLoaded.value)
+  })
+
+  // Sorting options
+  const sortByComp = computed(() => sortFields[sortedBy.value])
+
+  // Watch local references and sync URL on changes
+  watch([selectedCategory, query, orderedBy, sortedBy], syncURL)
+
+  // Initialize Fuse
+  function init() {
+    const fuseOptions = {
+      threshold: 0.1,
+      keys: ['name', 'npm', 'category', 'maintainers.name', 'maintainers.github', 'description', 'repo']
     }
 
-    // Reset filters
-    function clearFilters () {
-      selectedCategory.value = null
-      query.value = null
-      resetModulesLoaded()
+    const index = Fuse.createIndex(fuseOptions.keys, modules.value)
+
+    fuse.value = new Fuse(modules.value, fuseOptions, index)
+
+    selectedCategory.value = (window.location.hash || '').substr(1)
+
+    const { q, sortBy, orderBy } = route.value.query
+
+    if (q) {
+      query.value = q
     }
 
-    // Reset modules list incrementing
-    function resetModulesLoaded () {
-      modulesLoaded.value = MODULE_INCREMENT_LOADING
+    if (sortBy) {
+      sortedBy.value = sortBy
     }
 
-    // Toggle sorting type
-    function toggleOrderBy () {
-      orderedBy.value = (orderedBy.value === ORDERS.ASC) ? ORDERS.DESC : ORDERS.ASC
+    if (orderBy) {
+      orderedBy.value = orderBy
     }
 
-    // Select sorting type
-    function selectSortBy (field) {
-      sortedBy.value = field
-      sortByMenuVisible.value = false
+    modulesLoaded.value = MODULE_INCREMENT_LOADING
+  }
+
+  // Sync page URL with current page state
+  function syncURL() {
+    const url = route.value.path
+    let q = ''
+    resetModulesLoaded()
+    modulesLoaded.value = MODULE_INCREMENT_LOADING
+
+    if (query.value) {
+      q += `?q=${query.value}`
     }
 
-    // Intersection observer callback
-    function intersectedModulesLoading () {
-      modulesLoaded.value += MODULE_INCREMENT_LOADING
-      setPageFilteredModules(modulesLoaded.value)
+    if (orderedBy.value !== ORDERS.DESC) {
+      q += `${q ? '&' : '?'}orderBy=${orderedBy.value}`
     }
 
-    // Push new modules to displayed modules list
-    function setPageFilteredModules (count) {
-      pageFilteredModulesList.value = Object.assign([], filteredModules.value).splice(0, count)
+    if (sortedBy.value !== sortFields[0].value) {
+      q += `${q ? '&' : '?'}sortBy=${sortedBy.value}`
     }
 
-    // Update selected category
-    function selectCategory (category) {
-      if (selectedCategory.value === category) selectedCategory.value = null
-      else selectedCategory.value = category
+    if (selectedCategory.value) {
+      q += `#${selectedCategory.value}`
     }
 
-    return {
-      // References
-      fuse,
-      query,
-      orderedBy,
-      sortedBy,
-      sortByMenuVisible,
-      selectedCategory,
-      pageFilteredModulesList,
-      modulesLoaded,
-      // Computed
-      filteredModules,
-      sortByComp,
-      sortByOptions,
-      // Functions
-      init,
-      syncURL,
-      getCategory,
-      clearFilters,
-      resetModulesLoaded,
-      toggleOrderBy,
-      selectSortBy,
-      intersectedModulesLoading,
-      setPageFilteredModules,
-      selectCategory
-    }
+    window.history.pushState('', '', `${url}${q}`)
+  }
+
+  // Select current category
+  function getCategory(category) {
+    selectedCategory.value = category
+  }
+
+  // Reset filters
+  function clearFilters() {
+    selectedCategory.value = null
+    query.value = null
+    resetModulesLoaded()
+  }
+
+  // Reset modules list incrementing
+  function resetModulesLoaded() {
+    modulesLoaded.value = MODULE_INCREMENT_LOADING
+  }
+
+  // Toggle sorting type
+  function toggleOrderBy() {
+    orderedBy.value = orderedBy.value === ORDERS.ASC ? ORDERS.DESC : ORDERS.ASC
+  }
+
+  // Select sorting type
+  function selectSortBy(field) {
+    sortedBy.value = field
+    sortByMenuVisible.value = false
+  }
+
+  // Update selected category
+  function selectCategory(category) {
+    if (selectedCategory.value === category) selectedCategory.value = null
+    else selectedCategory.value = category
+  }
+
+  // Intersection observer callback
+  function loadModules() {
+    modulesLoaded.value += MODULE_INCREMENT_LOADING
+  }
+
+  return {
+    // References
+    fuse,
+    query,
+    orderedBy,
+    sortedBy,
+    sortByMenuVisible,
+    selectedCategory,
+    modulesLoaded,
+    // Computed
+    filteredModules,
+    sortByComp,
+    // Functions
+    init,
+    syncURL,
+    getCategory,
+    clearFilters,
+    resetModulesLoaded,
+    toggleOrderBy,
+    selectSortBy,
+    selectCategory,
+    loadModules
+  }
 }
--- plugins/modules.js
+++ plugins/modules.js
@@ -1,20 +1,17 @@
-import { ref, useContext, watch } from '@nuxtjs/composition-api'
+import { ssrRef } from '@nuxtjs/composition-api'
 import { $fetch } from 'ohmyfetch'
 
 // Nuxt API URL
 const apiURL = 'https://api.nuxtjs.org/api'
 
 // Universal $fetch workaround (server/client side)
-const _fetch = typeof fetch === "undefined" ? require('ohmyfetch/node').$fetch : $fetch
-
-// Errored reference
-const error = ref(false)
+const _fetch = typeof fetch === 'undefined' ? require('ohmyfetch/node').$fetch : $fetch
 
 // Modules reference
-const modules = ref()
+const modules = ssrRef([], 'modulesRef')
 
 // Categories reference
-const categories = ref()
+const categories = ssrRef([], 'categoriesRef')
 
 /**
  * Modules helpers
@@ -26,18 +23,18 @@ export function useModules() {
     modules.value = await _fetch(`${apiURL}/modules`)
 
     // Extract categories out of modules
-    categories.value = Object.entries(modules.value
-      .reduce((result, current) => {
+    categories.value = Object.entries(
+      modules.value.reduce((result, current) => {
         result[current.category] = (result[current.category] || 0) + 1
         return result
-      }, {}))
+      }, {})
+    )
       .map(categ => ({ name: categ[0], count: categ[1] }))
       .sort((a, b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase()))
   }
 
   return {
     fetch,
-    error,
     modules,
     categories
   }

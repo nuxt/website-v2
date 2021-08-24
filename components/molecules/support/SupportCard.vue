c599710c0286ec2c0ebddcf857ebaf229b6f6a43
Florent <flo@nuxtjs.com>
2021-08-24 14:49:17 +0200

    :sparkles: (support) responsive

--- components/molecules/support/SupportCard.vue
+++ components/molecules/support/SupportCard.vue
@@ -1,14 +1,14 @@
 <template>
   <li class="w-full p-4 rounded-md shadow-lg dark:bg-sky-darker">
-    <div class="flex items-center justify-between xl:h-24">
-      <div class="flex flex-col xl:flex-row justify-start items-start xl:items-center space-x-4">
+    <div class="flex flex-col space-y-4 sm:flex-row sm:items-center justify-between xl:h-24">
+      <div class="flex flex-col sm:w-2/3 xl:flex-row justify-start items-start xl:items-center space-x-4">
         <img :src="`/img/support/${imgName}.svg`" class="h-20" />
         <div class="flex flex-col">
           <h2 class="text-xl lg:text-2xl font-medium">{{ title }}</h2>
-          <span class="text-sm md:text-base max-w-3/5 xl:w-full">{{ description }}</span>
+          <span class="text-sm md:text-base sm:w-full">{{ description }}</span>
         </div>
       </div>
-      <div class="pr-4">
+      <div class="pr-4 ml-4 sm:ml-0 py-2 sm:py-0">
         <SectionButton
           :to="button.to"
           @click.native="form ? show() : showForm = false"

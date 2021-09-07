cab1502e30295e5267f99e03f819df180b8e6c06
Florent <flo@nuxtjs.com>
2021-09-07 15:54:42 +0200

    :sparkles: (Blog) custom blog template

---
+++ components/templates/Blog.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <PageHero
+      :title="page.heroTitle || page.title"
+      :description="page.heroDescription || page.description"
+      :description-full-width="page.heroDescriptionFullWidth"
+    />
+    <div class="d-container">
+      <DocusContent :document="page" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  props: {
+    page: {
+      type: Object,
+      required: true
+    }
+  },
+  templateOptions: {
+    aside: false,
+    fluid: true
+  }
+})
+</script>

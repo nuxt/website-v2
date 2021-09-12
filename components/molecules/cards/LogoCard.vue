4c5085e29d620be5f75606d28b8394c519280d91
Alexandre Chopin <alex@nuxtjs.com>
2021-09-12 19:53:39 +0200

    updates

--- components/molecules/cards/LogoCard.vue
+++ components/molecules/cards/LogoCard.vue
@@ -1,13 +1,9 @@
 <template>
   <div class="ListCard group">
-    <Link :to="item.link" :aria-label="item.title" target="_blank" class="absolute inset-0" />
-    <nuxt-img
-      class="ListCard-external group-hover:opacity-100"
-      alt="external_link"
-      src="/img/icons/ext.svg"
-      width="24"
-      height="24"
-    />
+    <template v-if="item.link">
+      <Link :to="item.link" :aria-label="item.title" target="_blank" class="absolute inset-0" />
+      <nuxt-img class="ListCard-external group-hover:opacity-100" alt="external_link" src="/img/icons/ext.svg" />
+    </template>
     <div class="ListCard-body">
       <nuxt-img :alt="`${item.title} logo`" :src="item.logo" class="w-12 h-12 mr-4 rounded-md" />
       <h3 class="font-bold text-xl py-2">{{ item.title || 'Title' }}</h3>
@@ -34,6 +30,6 @@ export default defineComponent({
   @apply relative hover:bg-gray-50 hover:dark:bg-opacity-80 dark:bg-sky-darker rounded-md shadow-md py-6 px-4 flex flex-col justify-between;
 }
 .ListCard-external {
-  @apply transition-opacity duration-200 opacity-0 absolute top-4 right-4 cursor-pointer;
+  @apply transition-opacity duration-200 opacity-0 absolute top-4 right-4 cursor-pointer h-6 w-6;
 }
 </style>

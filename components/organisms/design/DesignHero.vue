4a8b8170c364c6a2e062e7c08eac12f4f13f22df
Alexandre Chopin <alex@nuxtjs.com>
2021-09-14 18:01:01 +0200

    fix: design kit link to figma

--- components/organisms/design/DesignHero.vue
+++ components/organisms/design/DesignHero.vue
@@ -5,12 +5,13 @@
         <h1 class="text-display-3 font-serif mb-4">{{ title }}</h1>
         <p class="text-body-xl max-w-3xl">{{ description }}</p>
         <div class="mt-8">
-          <a
-            href="/design-kit/DesignKit_Nuxt.zip"
+          <Link
+            to="https://www.figma.com/@nuxtjs"
             :aria-label="downloadText"
+            blank
             class="rounded bg-primary text-gray-800 hover:bg-primary-400 focus:bg-primary-300 py-3 px-4"
             download
-            ><span class="font-medium">{{ downloadText }}</span></a
+            ><span class="font-medium">{{ downloadText }}</span></Link
           >
         </div>
       </div>

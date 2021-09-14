75e085ad475134214a56f800e6231d98dd452e61
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2021-09-14 18:30:52 +0200

    fix(banner): slightly displayed on other pages

--- components/theme/app/AppBanner.vue
+++ components/theme/app/AppBanner.vue
@@ -2,7 +2,7 @@
   <Link
     to="/v3"
     class="group block flex items-center transition-height delay-200 bg-primary hover:bg-primary-400"
-    :class="[$route.path === localePath('/') ? 'h-10' : 'h-0']"
+    :class="[$route.path === localePath('/') ? 'h-10' : 'h-0 overflow-hidden']"
   >
     <i18n
       tag="p"
@@ -18,16 +18,3 @@
     </i18n>
   </Link>
 </template>
-
-<script>
-import { defineComponent, useRoute, computed } from '@nuxtjs/composition-api'
-
-export default defineComponent({
-  setup() {
-    const route = useRoute()
-    const showBanner = computed(() => {
-      return route.value.path === localePath('/')
-    })
-  }
-})
-</script>

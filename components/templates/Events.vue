7a644b2eb6df906ba4daee136e4c66be6c928775
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2021-09-20 18:30:13 +0200

    feat(events): improve UI

--- components/templates/Events.vue
+++ components/templates/Events.vue
@@ -7,7 +7,23 @@
     />
     <div class="d-container px-6 grid sm:grid-cols-2 lg:grid-cols-3 gap-6 pt-8">
       <LogoCard v-for="(item, index) in events" :key="index" :item="item">
-        <template #footer> {{ item.date }}</template>
+        <template #footer>
+          <div class="flex items-center" :class="item.eventLogo ? 'justify-between' : 'justify-end'">
+            <img
+              v-if="item.eventLogo"
+              :src="`/img/events/light/${item.eventLogo}`"
+              loading="lazy"
+              class="h-5 light-img"
+            />
+            <img
+              v-if="item.eventLogo"
+              :src="`/img/events/dark/${item.eventLogo}`"
+              loading="lazy"
+              class="h-5 dark-img"
+            />
+            {{ item.displayDate }}
+          </div>
+        </template>
       </LogoCard>
     </div>
   </div>
@@ -28,6 +44,12 @@ export default defineComponent({
     const { $docus, i18n } = useContext()
     const events = ref([])
 
+    const formatDateByLocale = (locale, d) => {
+      const currentLocale = locale || 'en'
+      const options = { year: 'numeric', month: 'long', day: 'numeric' }
+      return new Date(d).toLocaleDateString(currentLocale, options)
+    }
+
     useFetch(async () => {
       const eventsCollection = await $docus
         .search('/collections/events', { deep: true })
@@ -38,10 +60,14 @@ export default defineComponent({
         .filter(e => e.events)
         .map(e => e.events)
         .flat()
-        .map(event => ({
-          ...event,
-          CTA: isFuture(parse(event.date, 'dd/MM/yyyy', new Date())) ? 'Subscribe' : 'Watch'
-        }))
+        .map(event => {
+          const date = parse(event.date, 'dd/MM/yyyy', new Date())
+          return {
+            ...event,
+            displayDate: formatDateByLocale(i18n.locale, date),
+            CTA: isFuture(date) ? 'Subscribe' : 'Watch'
+          }
+        })
     })
 
     return {

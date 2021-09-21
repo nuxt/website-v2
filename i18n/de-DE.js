601c3800af6d5ed4c7c23ac3c74060d9d8cf19e5
Intevel ツ <59223342+Intevel@users.noreply.github.com>
2021-09-21 19:36:10 +0200

    Create de-DE.js

---
+++ i18n/de-DE.js
@@ -0,0 +1,149 @@
+import de from '~docus-i18n/de-DE'
+
+export default {
+  ...de,
+  banner: {
+    here: 'hier',
+    format: '{nuxt} kommt... Mehr darüber erfahren Sie {here} !'
+  },
+  footer: {
+    titles: {
+      Discover: 'Entdecken',
+      Help: 'Hilfe',
+      Support: 'Support'
+    },
+    'Our team': 'Unser Team',
+    'Design Kit': 'Design Kit',
+    'Contact us': 'Kontakt aufnehmen',
+    Resources: 'Ressourcen',
+    'Chat with us': 'Chatten Sie mit uns',
+    'Contribution guide': 'Contribution Leitfaden',
+    Sustainability: 'Nachhaltigkeit',
+    Training: 'Training',
+    newsletter: {
+      title: 'Newsletter',
+      description: 'Die neuesten Nachrichten, Artikel und Ressourcen, die monatlich an Ihren Posteingang geschickt werden.',
+      form: {
+        email: 'Email',
+        subscribing: 'Abonniere...',
+        subscribe: 'Abonnieren',
+        already_registered: 'Du bist bereits registriert!',
+        invalid_address: 'Ungültige E-Mail Adresse',
+        subscribed_messages: {
+          pre: 'Eine E-Mail zur Bestätigung Ihres Abonnements wurde gesendet an',
+          confirmation: 'E-Mail bestätigt.'
+        }
+      }
+    },
+    license: 'Nuxt ist ein Open-Source-Framework unter der MIT-Lizenz.'
+  },
+  sidebar: {
+    partners: {
+      title: 'Partner',
+      button: 'Unterstützen Sie uns'
+    },
+    ads: {
+      fallback: {
+        title: 'Nuxt braucht dich!',
+        description: 'Wenn Sie nuxtjs.org auf Ihrem Ad-Blocker zulassen, unterstützen Sie unsere Arbeit und helfen uns finanziell.'
+      }
+    }
+  },
+  home: {
+    discover: {
+      pages: 'Seiten',
+      ui: 'UI',
+      data: 'Daten',
+      modules: 'Module',
+      deployment: 'Deployment'
+    },
+    cli: 'Über CLI',
+    scratch: 'Von Grund auf'
+  },
+  modules: {
+    search: 'Suche nach einem Modul (Name, Kategorie, Benutzername, etc.)',
+    sort_by: 'Sortieren',
+    sort_fields: {
+      downloads: 'Downloads',
+      stars: 'Sterne'
+    },
+    loading: 'Lädt...',
+    error: 'Beim Abrufen von den Modulen ist ein Fehler aufgetreten.'
+  },
+  resources: {
+    themes: {
+      get_for_it: 'Kaufen für {price}',
+      video_course: 'Kurs starten'
+    }
+  },
+  sustainability: {
+    tiers: {
+      mvp_sponsors: 'MVP Sponsoren',
+      partners: 'Partner',
+      sponsors: 'Sponsoren',
+      donations: 'Spenden'
+    },
+    mvp_detail: {
+      services: 'Dienstleistungen',
+      location: 'Standort',
+      contact_partner: 'Partner kontaktieren',
+      visit_website: 'Webseite besuchen'
+    }
+  },
+  support: {
+    confirm_sending: 'Eine Nachricht wurde an den technischen Support gesendet.',
+    error_sending: 'Ein Fehler ist beim Senden aufgetreten.',
+    invalid_adress: 'Ungültige E-Mail Adresse'
+  },
+  showcases: {
+    loading: 'Lädt...',
+    chrome_extension: 'Chrome Erweiterung',
+    firefox_extension: 'Firefox Erweiterung',
+    categories: {
+      Featured: 'Featured',
+      'E-Commerce': 'E-Commerce',
+      News: 'News',
+      Government: 'Regierung',
+      Sport: 'Sport',
+      Education: 'Bildung',
+      Entertainment: 'Unterhaltung',
+      Travel: 'Reisen',
+      Finance: 'Finanzen',
+      Tech: 'Technik',
+      Business: 'Business'
+    }
+  },
+  releases: {
+    version: 'Version',
+    released_on: 'Veröffentlicht in' //Veröffentlicht in Version 1.9 (Example)
+  },
+  partners: {
+    become_partner: 'Werden Sie Partner'
+  },
+  theme_mode: {
+    system: 'System',
+    light: 'Hell',
+    dark: 'Dunkel'
+  },
+  common: {
+    an_error_occurred: 'Es ist ein Fehler aufgetreten',
+    page_not_found: "Wir konnten die gesuchte Seite nicht finden.",
+    please_define_title: 'Bitte definieren Sie einen Titel im Vorwort',
+    please_define_description: 'Bitte definieren Sie eine Beschreibung im Vorwort',
+    search: 'Suchen ("/" um zu fokussieren)',
+    settings: 'Einstellungen',
+    version: 'Version',
+    currently_version: 'Aktuell in privater Beta',
+    back: 'Zurück',
+    go_to: 'Gehe zu {title}',
+    go_home: 'Zurück zur Startseite',
+    read_article: 'Artikel lesen',
+    days: 'Tage',
+    hours: 'Stunden',
+    minutes: 'Minuten',
+    seconds: 'Sekunden',
+    copied: 'kopiert',
+    watch_video: 'Video ansehen'
+  },
+  iso: 'de'
+}

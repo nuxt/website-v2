9c174d28dd7fb9e309e1e3ece67df5f90659ca89
Florent <flo@nuxtjs.com>
2021-09-22 16:41:10 +0200

    :sparkles: (CookieBanner) create cookie banner component

---
+++ components/templates/CookieBanner.vue
@@ -0,0 +1,47 @@
+<template>
+  <div
+    class="
+      bg-white
+      dark:bg-sky-black
+      border-t border-t-sky-light
+      text-center
+      md:text-left
+      text-sky-dark
+      dark:text-white
+      font-medium
+    "
+  >
+    <VueIfBot>
+      <CookieConsent
+        class="d-container-content p-4 flex flex-col md:flex-row items-center md:justify-between space-y-4 md:space-y-0"
+      >
+        <template slot="message">
+          <p class="md:w-1/2">
+            {{ $t('cookies.message') }}
+            <a class="underline" rel="noopener" target="_blank" href="https://www.cookiesandyou.com/">
+              {{ $t('cookies.link') }}
+            </a>
+          </p>
+        </template>
+        <template slot="button">
+          <SectionButton size="md" class="bg-primary text-black hover:bg-primary-400 focus:bg-primary-400">
+            {{ $t('cookies.button') }}
+          </SectionButton>
+        </template>
+      </CookieConsent>
+    </VueIfBot>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from '@nuxtjs/composition-api'
+import VueIfBot from 'vue-if-bot/dist/vue-if-bot.es'
+import CookieConsent from 'vue-cookieconsent-component/src/components/CookieConsent.vue'
+
+export default defineComponent({
+  components: {
+    VueIfBot,
+    CookieConsent
+  }
+})
+</script>

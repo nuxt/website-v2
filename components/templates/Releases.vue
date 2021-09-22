f194cc53d0338da46d5ea9ee1e4fc87d8102ae38
Florent <flo@nuxtjs.com>
2021-09-22 10:24:08 +0200

    :lipstick: (Releases) update vertical spacing

--- components/templates/Releases.vue
+++ components/templates/Releases.vue
@@ -14,7 +14,7 @@
         :key="release.name"
         class="border-b dark:border-sky-darker dark:lg:border-transparent lg:border-transparent"
       >
-        <div class="relative lg:flex py-12 lg:py-16 lg:border-b lg:dark:border-sky-darker">
+        <div class="relative lg:flex py-8 lg:py-16 lg:border-b lg:dark:border-sky-darker">
           <div class="self-start w-full lg:w-1/4 lg:sticky lg:top-0 lg:pt-38 lg:-mt-32 lg:mb-6">
             <h2 class="mb-3 text-3xl font-light text-tw-gray-900">
               {{ $t('releases.version') }} <span class="font-bold">{{ release.name }}</span>
@@ -31,7 +31,7 @@
             </p>
           </div>
 
-          <DocusContent :document="release" class="lg:w-3/4 px-8 lg:px-0 pt-8 lg:pt-0" />
+          <DocusContent :document="release" class="lg:w-3/4 px-8 lg:px-0 pt-4 lg:pt-0" />
         </div>
       </div>
     </div>

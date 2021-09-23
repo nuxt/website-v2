93f3d7b132b72e039303986f3ea81f8c4343779b
Florent <flo@nuxtjs.com>
2021-09-23 17:56:41 +0200

    :sparkles: (TweetsCollection) (wip) tweets collection component

---
+++ components/organisms/lists/TweetsCollection.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    {{ collections }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+import { useTweets } from '../../../plugins/tweets'
+
+export default defineComponent({
+  data() {
+    return {
+      collections: null
+    }
+  },
+  async fetch() {
+    const { fetch: fetchCollections } = useTweets({
+      collectionID: '1440983543858044928'
+    })
+    this.collections = (await fetchCollections()).value
+  }
+})
+</script>

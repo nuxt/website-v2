bd80c83ae07a7cb474612caafd1ba6762a200c1e
Florent <flo@nuxtjs.com>
2021-09-23 17:54:28 +0200

    :sparkles: (tweets.js) plugin to get tweets collection from twitter api

---
+++ plugins/tweets.js
@@ -0,0 +1,31 @@
+import { ssrRef } from '@nuxtjs/composition-api'
+import { $fetch } from 'ohmyfetch'
+
+// Universal $fetch workaround (server/client side)
+const _fetch = typeof fetch === 'undefined' ? require('ohmyfetch/node').$fetch : $fetch
+
+const BASE_URL = 'https://api.twitter.com/1.1/collections'
+
+// Tweets reference
+const tweets = ssrRef([], 'tweetsRef')
+
+/**
+ * Tweets helpers
+ */
+export function useTweets({ collectionID }) {
+  // Fetch showcases
+  const fetch = async () => {
+    // Get collection
+    tweets.value = await _fetch(`${BASE_URL}/show.json?id=${collectionID}`, {
+      method: 'GET',
+      headers: { 'Access-Control-Allow-Origin': '*' }
+    })
+
+    return tweets
+  }
+
+  return {
+    fetch,
+    tweets
+  }
+}

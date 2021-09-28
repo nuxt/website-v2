e90c0504866f8cb64274ee49fa102e0e8c620cbd
Florent <flo@nuxtjs.com>
2021-09-28 11:56:17 +0200

    :fire: (nuxt.config) remove tweets plugins

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -67,7 +67,6 @@ export default withDocus({
     '~/plugins/adblock.client',
     '~/plugins/v-tooltip.ts',
     '~/plugins/showcases',
-    '~/plugins/tweets',
     '~/plugins/nav'
   ],
   windicss: {

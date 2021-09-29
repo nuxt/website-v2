4961cb14b8fac57802a1ac550c98f52f02f283ea
Hamza Djennad <djennadhamza@hotmail.com>
2021-09-29 20:30:54 +0100

    Creating ar-AR.js & copied the folder lang

---
+++ i18n/ar-AR.js
@@ -0,0 +1,154 @@
+import en from '~docus-i18n/en-US'
+
+export default {
+  ...en,
+  banner: {
+    here: 'here',
+    format: '{nuxt} is coming... Discover more about it {here} !'
+  },
+  cookies: {
+    message: 'We use Cookies for user analysis and on-page improvements!',
+    link: 'Learn about cookies',
+    button: 'Got it'
+  },
+  footer: {
+    titles: {
+      Discover: 'Discover',
+      Help: 'Help',
+      Support: 'Support'
+    },
+    'Our team': 'Our team',
+    'Design Kit': 'Design Kit',
+    'Contact us': 'Contact us',
+    Resources: 'Resources',
+    'Chat with us': 'Chat with us',
+    'Contribution guide': 'Contribution guide',
+    Sustainability: 'Sustainability',
+    Training: 'Training',
+    newsletter: {
+      title: 'Newsletter',
+      description: 'The latest news, articles, and resources, sent to your inbox monthly.',
+      form: {
+        email: 'Email',
+        subscribing: 'Subscribing...',
+        subscribe: 'Subscribe',
+        already_registered: 'You are already registered',
+        invalid_address: 'Invalid address',
+        subscribed_messages: {
+          pre: 'An email to confirm your subscription has been sent to',
+          confirmation: 'Email confirmed'
+        }
+      }
+    },
+    license: 'Nuxt is an open source framework under MIT license.'
+  },
+  sidebar: {
+    partners: {
+      title: 'Partners',
+      button: 'Support Us'
+    },
+    ads: {
+      fallback: {
+        title: 'Nuxt needs you!',
+        description: 'By allowing nuxtjs.org on your Ad-Blocker, you support our work and help us financially.'
+      }
+    }
+  },
+  home: {
+    discover: {
+      pages: 'Pages',
+      ui: 'UI',
+      data: 'Data',
+      modules: 'Modules',
+      deployment: 'Deployment'
+    },
+    cli: 'From CLI',
+    scratch: 'From Scratch'
+  },
+  modules: {
+    search: 'Search a module (name, category, username, etc.)',
+    sort_by: 'Sort by',
+    sort_fields: {
+      downloads: 'Downloads',
+      stars: 'Stars'
+    },
+    loading: 'Loading...',
+    error: 'An error occured while fetching modules.'
+  },
+  resources: {
+    themes: {
+      get_for_it: 'Get it for {price}',
+      video_course: 'Start course'
+    }
+  },
+  sustainability: {
+    tiers: {
+      mvp_sponsors: 'MVP Sponsors',
+      partners: 'Partners',
+      sponsors: 'Sponsors',
+      donations: 'Donate'
+    },
+    mvp_detail: {
+      services: 'Services',
+      location: 'Location',
+      contact_partner: 'Contact Partner',
+      visit_website: 'Visit Website'
+    }
+  },
+  support: {
+    confirm_sending: 'Message sent to technical support',
+    error_sending: 'Error during sending',
+    invalid_adress: 'Invalid address'
+  },
+  showcases: {
+    loading: 'Loading...',
+    chrome_extension: 'Chrome extension',
+    firefox_extension: 'Firefox extension',
+    categories: {
+      Featured: 'Featured',
+      'E-Commerce': 'E-Commerce',
+      News: 'News',
+      Government: 'Government',
+      Sport: 'Sport',
+      Education: 'Education',
+      Entertainment: 'Entertainment',
+      Travel: 'Travel',
+      Finance: 'Finance',
+      Tech: 'Tech',
+      Business: 'Business'
+    }
+  },
+  releases: {
+    version: 'Version',
+    released_on: 'Released on {datetime}'
+  },
+  partners: {
+    become_partner: 'Become a partner'
+  },
+  theme_mode: {
+    system: 'System',
+    light: 'Light',
+    dark: 'Dark'
+  },
+  common: {
+    an_error_occurred: 'An error occurred',
+    page_not_found: "We couldn't find the page you are looking for.",
+    please_define_title: 'Please define a title in the front matter',
+    please_define_description: 'Please define a description in the front matter',
+    search: 'Search ("/" to focus)',
+    settings: 'Settings',
+    version: 'Version',
+    currently_version: 'Currently in private beta',
+    back: 'Back',
+    go_to: 'Go to {title}',
+    go_home: 'Go home',
+    read_article: 'Read article',
+    days: 'days',
+    hours: 'hours',
+    minutes: 'minutes',
+    seconds: 'seconds',
+    copied: 'Copied',
+    watch_video: 'Watch video'
+  },
+  iso: 'ar'
+}

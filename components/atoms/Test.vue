df870a36e1d3856d41bcfeb2c1c9cd5d49b934b4
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2021-10-12 11:23:24 +0200

    add test file

---
+++ components/atoms/Test.vue
@@ -0,0 +1,20 @@
+<template>
+    <span  class="text-black" >{{  value }}</span>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default:  ''
+    }
+  }
+}
+</script>
+
+<style>
+ .test-class{
+
+ }
+</style>

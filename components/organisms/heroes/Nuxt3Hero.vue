bbb96e96d934eeb37ed7c9917c7cc609eb303f4f
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2021-10-12 14:33:35 +0200

    feat: update v3 page

--- components/organisms/heroes/Nuxt3Hero.vue
+++ components/organisms/heroes/Nuxt3Hero.vue
@@ -2,7 +2,10 @@
   <div class="overflow-hidden relative mx-auto max-w-8xl">
     <Nuxt3HeroParallax />
     <div class="flex flex-wrap justify-center py-0 section">
-      <section class="flex flex-col justify-start w-full px-4 pt-36 pb-52 md:pt-40 lg:pb-56 lg:pt-48 text-center">
+      <section
+        v-if="!isDatePassed"
+        class="flex flex-col justify-start w-full px-4 pt-36 pb-52 md:pt-40 lg:pb-56 lg:pt-48 text-center"
+      >
         <div>
           <span class="nuxt-text-highlight">{{ $t('common.currently_version') }}</span>
         </div>
@@ -41,19 +44,66 @@
           </p>
         </div>
       </section>
+      <section
+        v-else
+        class="
+          d-container-content
+          flex flex-col
+          justify-start
+          w-full
+          px-4
+          pt-36
+          pb-52
+          md:pt-40
+          lg:pb-56 lg:pt-48
+          text-center
+        "
+      >
+        <h1 class="font-normal font-serif text-display-5 xs:text-display-4 md:text-display-3 2xl:text-display-2 my-4">
+          Nuxt 3 is out<span class="text-primary">!</span>
+        </h1>
+        <h2
+          class="
+            font-normal
+            text-body-base
+            xs:text-body-lg
+            md:text-body-xl
+            2xl:text-body-2xl
+            px-8
+            sm:px-0
+            text-secondary-dark
+            dark:text-cloud-lighter
+          "
+        >
+          Build you next application with Vue 3 and experience hybrid rendering, powerful data fetching and new
+          features. Nuxt 3 is an open source framework making web development simple and powerful.
+        </h2>
+        <SectionButton
+          href="https://v3.nuxtjs.org"
+          class="rounded-md bg-primary text-gray-800 hover:bg-primary-300 focus:bg-primary-300 mx-auto mt-8"
+          >Get Started</SectionButton
+        >
+      </section>
     </div>
   </div>
 </template>
 
 <script>
-import { defineComponent } from '@nuxtjs/composition-api'
+import { defineComponent, ref } from '@nuxtjs/composition-api'
+import { parseISO } from 'date-fns'
 import { useNewsletter } from '~/plugins/composables'
 
 export default defineComponent({
   setup() {
     const { email, error, subscribe, pending, subscribed } = useNewsletter()
 
+    const date = ref(parseISO('2021-10-12T14:00:00+02:00'))
+    const now = new Date()
+
+    const isDatePassed = date.value.getTime() - now.getTime() < 0
+
     return {
+      isDatePassed,
       email,
       pending,
       subscribe,

7f6722d33ffe6943836924706814c253d817e9ab
Florent <flo@nuxtjs.com>
2021-10-12 16:28:31 +0200

    :fire: (SustainabilitySection) remove commented code

--- components/organisms/sections/SustainabilitySection.vue
+++ components/organisms/sections/SustainabilitySection.vue
@@ -1,26 +1,8 @@
 <template>
   <div class="py-4 d-container-content">
     <div class="flex flex-col pt-8 space-y-4 light:text-sky-darker dark:text-white">
-      <!-- h2 class="font-semibold text-center text-display-6">
-        <Markdown use="titleSection" unwrap="p" />
-      </h2>
-
-      <div class="flex justify-center w-full pb-16 text-lg">
-        <p class="text-center md:w-2/3">
-          <Markdown use="descriptionSection" unwrap="p" />
-        </p>
-      </div-->
-
       <SectionSponsors :tier="$t('sustainability.tiers.mvp_sponsors')" :sponsors="mvp" class="pb-12" />
       <SectionSponsors :tier="$t('sustainability.tiers.sponsors')" :sponsors="sponsors" />
-
-      <!-- SectionButton
-        to="https://github.com/sponsors/nuxt"
-        :aria-label="buttonText"
-        size="md"
-        class="text-gray-800 bg-primary hover:bg-primary-400 focus:bg-primary-400">
-          {{ buttonText }}
-      </SectionButton -->
     </div>
   </div>
 </template>

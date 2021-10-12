55acbdfddef8d061c40d8cb8016388780c598eda
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2021-10-12 18:18:30 +0200

    feat: create `AppLink`

---
+++ components/atoms/links/AppLink.vue
@@ -0,0 +1,50 @@
+<template>
+  <Link v-bind="attrs">
+    <slot />
+  </Link>
+</template>
+
+<script>
+import { defineComponent, computed } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  props: {
+    to: {
+      type: String,
+      default: ''
+    },
+    href: {
+      type: String,
+      default: ''
+    },
+    blank: {
+      type: Boolean,
+      default: true
+    },
+    rel: {
+      type: String,
+      default: 'noopener nofollow noreferrer'
+    }
+  },
+  setup(props) {
+    const attrs = computed(() => {
+      if (props.href.length) {
+        return {
+          to: props.href,
+          blank: props.blank,
+          static: true,
+          rel: props.rel
+        }
+      } else {
+        return {
+          to: props.to
+        }
+      }
+    })
+
+    return {
+      attrs
+    }
+  }
+})
+</script>

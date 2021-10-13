5ce50c2ea0dfd1e03ec5993871dc895c6cffd22a
Florent <flo@nuxtjs.com>
2021-10-13 16:02:52 +0200

    :bug: (AppButton) update name class

--- components/atoms/buttons/AppButton.vue
+++ components/atoms/buttons/AppButton.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- eslint-disable-next-line vue/require-component-is -->
-  <Component v-bind="buttonProps" class="button-link" :class="[buttonClass, size]">
+  <Component v-bind="buttonProps" class="app-button" :class="[buttonClass, size]">
     <Component :is="icon" v-if="icon" class="mr-2 icon" :class="size" />
 
     <Markdown unwrap="li ul p" />
@@ -65,7 +65,7 @@ export default defineComponent({
 })
 </script>
 <style lang="postcss" scoped>
-a.button-link {
+.app-button {
   @apply inline-flex items-center flex-none rounded-md mb-2 sm:mb-0 px-4 py-2.5 text-sm font-medium leading-4 transition-colors duration-200 border border-transparent focus:ring-2 focus:ring-offset-2 focus:ring-offset-white dark:focus:ring-offset-gray-900 focus:outline-none;
   &.small {
     @apply text-xs leading-2 py-2;

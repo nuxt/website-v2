f26d013ea9dc74d1ea133c4266a62a60a33e5276
Florent <flo@nuxtjs.com>
2021-10-14 16:04:57 +0200

    :recycle: (ShowcasesBottomHero) use AppButton instead AppLink

--- components/organisms/heroes/ShowcasesBottomHero.vue
+++ components/organisms/heroes/ShowcasesBottomHero.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="flex flex-col sm:flex-row sm:space-x-6 space-y-4 sm:space-y-0">
-    <AppLink
+    <AppButton
       href="https://chrome.google.com/webstore/detail/vue-telescope/neaebjphlfplgdhedjdhcnpjkndddbpd"
-      class="inline-flex items-center gap-3 h-12 bg-gray-500 bg-opacity-10 px-4 hover:bg-opacity-20 rounded-md"
+      extra-class="gap-3 app-button-secondary-color"
+      size="large"
     >
       <img loading="lazy" :src="`/img/showcases/chrome.svg`" alt="chrome" class="w-5 h-5" />
-      <span class="font-medium">{{ $t('showcases.chrome_extension') }}</span>
-    </AppLink>
-    <AppLink
+      {{ $t('showcases.chrome_extension') }}
+    </AppButton>
+    <AppButton
       href="https://addons.mozilla.org/en/firefox/addon/vue-telescope/"
-      class="inline-flex items-center gap-3 h-12 bg-gray-500 bg-opacity-10 px-4 hover:bg-opacity-20 rounded-md"
+      extra-class="gap-3 app-button-secondary-color"
+      size="large"
     >
       <img loading="lazy" :src="`/img/showcases/firefox.svg`" alt="firefox" class="w-5 h-5" />
       <span class="font-medium">{{ $t('showcases.firefox_extension') }}</span>
-    </AppLink>
+    </AppButton>
   </div>
 </template>

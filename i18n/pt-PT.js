51dc49c83c9b62e8e1423e968af42d668b5baa07
Rafael Fernandes <rafatexfr@gmail.com>
2021-10-22 11:18:47 +0200

    docs(pt-PT): added translation to pt-PT (#1862)

---
+++ i18n/pt-PT.js
@@ -0,0 +1,156 @@
+import pt from '~docus-i18n/pt-PT'
+
+export default {
+  ...pt,
+  banner: {
+    here: 'v3.nuxtjs.org',
+    format: '{nuxt} beta já está disponível! Descobre mais sobre isso {here}'
+  },
+  cookies: {
+    message: 'Nós usamos Cookies para análise do utilizador e fazer melhorias na página!',
+    link: 'Aprender sobre cookies',
+    button: 'OK'
+  },
+  footer: {
+    titles: {
+      Discover: 'Descobrir',
+      Help: 'Ajuda',
+      Support: 'Suporte'
+    },
+    'Our team': 'A nossa equipa',
+    'Design Kit': 'Kit de Design',
+    'Contact us': 'Contacta-nos',
+    Resources: 'Recursos',
+    'Chat with us': 'Conversa connosco',
+    'Contribution guide': 'Guia de contribuição',
+    Sustainability: 'Sustentabilidade',
+    Training: 'Treino',
+    newsletter: {
+      title: 'Newsletter',
+      description: 'As últimas novidades, artigos, e recursos, enviados para o teu inbox mensalmente',
+      form: {
+        email: 'Email',
+        subscribing: 'Inscrevendo...',
+        subscribe: 'Inscrever',
+        already_registered: 'Já estás inscrito',
+        invalid_address: 'Endereço inválido',
+        subscribed_messages: {
+          pre: 'Um email para confirmar a tua subscrição foi enviado para',
+          confirmation: 'Email confirmado'
+        }
+      }
+    },
+    license: 'Nuxt é uma framework open source sob licença MIT.'
+  },
+  sidebar: {
+    partners: {
+      title: 'Parceiros',
+      button: 'Suporta-nos'
+    },
+    ads: {
+      fallback: {
+        title: 'Nuxt precisa de ti!',
+        description:
+          'Ao permitir nuxtjs.org no teu AdBlocker, estás a apoiar o nosso trabalho e a ajudar-nos financeiramente.'
+      }
+    }
+  },
+  home: {
+    discover: {
+      pages: 'Páginas',
+      ui: 'UI',
+      data: 'Dados',
+      modules: 'Módulos',
+      deployment: 'Desdobramento'
+    },
+    cli: 'Com a CLI',
+    scratch: 'De raiz'
+  },
+  modules: {
+    search: 'Procurar um módulo (nome, categoria, nome de utilizador, etc.)',
+    sort_by: 'Ordenar por',
+    sort_fields: {
+      downloads: 'Downloads',
+      stars: 'Estrelas'
+    },
+    loading: 'Carregando...',
+    error: 'Um error ocorreu durante o carregamento dos módulos.'
+  },
+  resources: {
+    themes: {
+      get_for_it: 'Comprar por {price}',
+      video_course: 'Iniciar tutorial'
+    }
+  },
+  sustainability: {
+    tiers: {
+      mvp_sponsors: 'Patrocinadores MVP',
+      partners: 'Parceiros',
+      sponsors: 'Patrocinadores',
+      donations: 'Doar'
+    },
+    mvp_detail: {
+      services: 'Serviços',
+      location: 'Localização',
+      contact_partner: 'Contactar Parceiro',
+      visit_website: 'Visitar Website'
+    }
+  },
+  support: {
+    confirm_sending: 'Mensagem enviada para o suporte técnico',
+    error_sending: 'Erro durante o envio',
+    invalid_adress: 'Endereço inválido'
+  },
+  showcases: {
+    loading: 'Carregando...',
+    chrome_extension: 'Extensão para Chrome',
+    firefox_extension: 'Extensão para Firefox',
+    categories: {
+      Featured: 'Destaques',
+      'E-Commerce': 'E-Commerce',
+      News: 'Notícias',
+      Government: 'Governo',
+      Sport: 'Desporto',
+      Education: 'Educação',
+      Entertainment: 'Entretenimento',
+      Travel: 'Viagem',
+      Finance: 'Finança',
+      Tech: 'Tecnologia',
+      Business: 'Negócios'
+    }
+  },
+  releases: {
+    version: 'Versão',
+    released_on: 'Lançado em {datetime}'
+  },
+  partners: {
+    become_partner: 'Torne-se um parceiro'
+  },
+  theme_mode: {
+    system: 'Sistema',
+    light: 'Claro',
+    dark: 'Escuro'
+  },
+  common: {
+    an_error_occurred: 'Um erro ocorreu',
+    page_not_found: 'Não conseguimos encontrar a página que procuras.',
+    please_define_title: 'Por favor, defina um título no assunto principal',
+    please_define_description: 'Por favor, defina uma descrição no assunto principal',
+    search: 'Procurar ("/" para focar)',
+    settings: 'Definições',
+    version: 'Versão',
+    currently_version: 'Atualmente em beta privado',
+    back: 'Para trás',
+    go_to: 'Ir para {title}',
+    go_home: 'Ir para página principal',
+    read_article: 'Ler artigo article',
+    days: 'dias',
+    hours: 'horas',
+    minutes: 'minutos',
+    seconds: 'segundos',
+    copied: 'Copiado',
+    watch_video: 'Ver vídeo',
+    multiple_authors: 'Vários Autores'
+  },
+  iso: 'pt-PT'
+}

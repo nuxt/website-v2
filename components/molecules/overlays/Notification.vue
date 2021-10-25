2f494073b251703f86dc46a5f7f0c1d5ded8f12a
Florent <flo@nuxtjs.com>
2021-10-25 16:21:12 +0200

    :recycle: (Notification) refactoring style

--- components/molecules/overlays/Notification.vue
+++ components/molecules/overlays/Notification.vue
@@ -1,60 +1,22 @@
 <template>
-  <transition
-    appear
-    enter-class="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
-    enter-active-class="transition duration-300 ease-out transform"
-    enter-to-class="translate-y-0 opacity-100 sm:translate-x-0"
-    leave-class="opacity-100"
-    leave-active-class="transition duration-100 ease-in"
-    leave-to-class="opacity-0"
-  >
-    <div
-      class="z-50 w-full bg-white rounded-lg shadow-lg pointer-events-auto dark:bg-sky-darkest"
-      @mouseover="onMouseover"
-      @mouseout="onMouseout"
-    >
-      <div class="relative overflow-hidden rounded-lg ring-1 ring-gray-200 dark:ring-sky-dark">
-        <div class="p-4">
-          <div class="flex">
-            <div class="flex-shrink-0">
-              <Component :is="iconName" class="w-6 h-6" :class="iconColorClass" />
-            </div>
-            <div class="ml-3 w-0 flex-1 pt-0.5">
-              <p class="text-sm font-medium leading-5 text-tw-gray-900">{{ title }}</p>
-              <p v-if="description" class="mt-1 text-sm leading-5 text-tw-gray-500">{{ description }}</p>
-              <AppButton v-if="undo" class="mt-2" @click.native.stop="cancel">
-                Undo
-
-                <div class="inline-flex items-center rounded bg-tw-gray-200 ml-1.5">
-                  <span class="w-full px-1 text-center text-tw-gray-600 text-xxs"> Z </span>
-                </div>
-              </AppButton>
-            </div>
-            <div class="flex-shrink-0 ml-4">
-              <button
-                class="
-                  transition
-                  duration-150
-                  ease-in-out
-                  text-tw-gray-400
-                  focus:outline-none
-                  hover:text-tw-gray-500
-                  focus:text-tw-gray-500
-                "
-                @click.stop="close"
-              >
-                <span class="sr-only">Close</span>
-                <IconClose class="w-5 h-5" />
-              </button>
-            </div>
-          </div>
-        </div>
-        <div v-if="timeout" class="absolute bottom-0 left-0 right-0 h-1">
-          <div class="h-1" :class="progressBarColorClass" :style="progressBarStyle" />
-        </div>
+  <div class="notification-container" @mouseover="onMouseover" @mouseout="onMouseout">
+    <div class="p-4 flex">
+      <Component :is="iconName" class="w-6 h-6" :class="iconColorClass" />
+      <div class="ml-3 flex-1 pt-0.5">
+        <p class="text-sm font-medium leading-5 text-sky-black dark:text-white">{{ title }}</p>
+        <p v-if="description" class="mt-1 text-sm leading-5 text-gray-600 dark:text-gray-400">{{ description }}</p>
       </div>
+      <AppButton
+        class="ml-4 text-gray-400 focus:outline-none hover:text-gray-500 focus:text-gray-500"
+        @click.stop="close"
+      >
+        <IconClose class="w-5 h-5" />
+      </AppButton>
     </div>
-  </transition>
+    <div v-if="timeout" class="absolute bottom-0 left-0 right-0">
+      <div class="h-1" :class="progressBarColorClass" :style="progressBarStyle" />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -89,10 +51,6 @@ export default defineComponent({
     timeout: {
       type: Number,
       default: 5000
-    },
-    undo: {
-      type: Function,
-      default: null
     },
     callback: {
       type: Function,
@@ -183,17 +141,6 @@ export default defineComponent({
       }
     }
 
-    function cancel() {
-      if (timer) {
-        timer.value.stop()
-        ticker.value.stop()
-      }
-      if (props.undo) {
-        this.undo()
-      }
-      emit('close')
-    }
-
     function close() {
       if (props.callback) {
         this.callback()
@@ -210,9 +157,23 @@ export default defineComponent({
       progressBarStyle,
       onMouseover,
       onMouseout,
-      cancel,
       close
     }
   }
 })
 </script>
+<style lang="postcss" scoped>
+.notification-container {
+  @apply z-50 w-full relative overflow-hidden pointer-events-auto shadow-lg rounded-lg ring-1 ring-gray-200 dark:ring-sky-dark bg-white dark:bg-sky-darkest;
+  animation: transition-opacity 0.5s ease-in-out;
+}
+
+@keyframes transition-opacity {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>

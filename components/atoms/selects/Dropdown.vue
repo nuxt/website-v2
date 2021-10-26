27f8e9e63e4fe1e955a309cdcd77d9c5430e4eff
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2021-10-26 12:50:48 +0200

    chore(icons): update IconChevronBottom

--- components/atoms/selects/Dropdown.vue
+++ components/atoms/selects/Dropdown.vue
@@ -10,21 +10,11 @@
   >
     <div class="flex items-center w-full" :class="mainLinkClass">
       <slot name="trigger" :toggle="toggle" :open="open" />
-      <svg
+      <IconChevronBottom
         v-if="icon"
         class="h-4 w-4"
         :class="isHome ? 'text-white' : 'dark:text-secondary light:text-gray-300'"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        aria-hidden="true"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-          clip-rule="evenodd"
-        />
-      </svg>
+      />
     </div>
 
     <div
--- components/atoms/selects/NuxtSelectNative.vue
+++ components/atoms/selects/NuxtSelectNative.vue
@@ -31,19 +31,7 @@
     </select>
 
     <div class="pointer-events-none absolute inset-y-0 right-0 px-2 flex items-center">
-      <svg
-        class="h-4 w-4 dark:text-secondary light:text-gray-300"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        aria-hidden="true"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-          clip-rule="evenodd"
-        />
-      </svg>
+      <IconChevronBottom class="h-4 w-4 dark:text-secondary light:text-gray-300" />
     </div>
   </div>
 </template>
